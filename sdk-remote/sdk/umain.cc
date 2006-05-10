61b717bdf04118b08a031b651239b9e4ff47e31a
nottale <nottale@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-05-10 12:10:27 +0000

    updated sdk, it is now standalone
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@96 b8db33d8-8104-0410-b827-8403f563b8eb

---
+++ sdk-remote/sdk/umain.cc
@@ -0,0 +1,29 @@
+/*! \file ballmain.cc
+ *******************************************************************************
+
+ File: ballmain.cc\n
+ Constains the main() function.
+
+ This file if part of the 'ball' soft device\n
+ (c) Jean-Christophe Baillie, 2004-2006.
+
+ Permission to use, copy, modify, and redistribute this software for
+ non-commercial use is hereby granted.
+
+ This software is provided "as is" without warranty of any kind,
+ either expressed or implied, including but not limited to the
+ implied warranties of fitness for a particular purpose.
+
+ For more information, comments, bug reports: http://www.urbiforge.com
+
+ **************************************************************************** */
+
+#include <uobject.h>
+#include <uclient.h>
+
+int
+main(int argc, char *argv[])
+{
+  urbi::main(argc, argv);
+  urbi::execute();
+};
