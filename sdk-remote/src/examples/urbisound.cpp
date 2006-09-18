7f38ecc55cfc33f77cddc87515a5e8c21485f056
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-09-18 09:07:18 +0000

    2006-09-18  Akim Demaille  <akim@lrde.epita.fr>
    	* src/liburbi/Makefile.am, src/liburbi/Doxyfile.in: Move do
    	generation to...
    	* doc/Makefile.am, doc/Doxyfile.in: here.
    	Put it in srcdir.
    	(install-data-local): New.
    	* doc/Doxyfile.in (INPUT, FILE_PATTERNS): Complete.
    
    	* src/examples/urbisound.cpp: Formatting changes.
    
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@128 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/src/examples/urbisound.cpp
+++ sdk-remote/src/examples/urbisound.cpp
@@ -3,7 +3,7 @@
  *
  * Sample demonstration of URBI sound capabilities.
  *
- * Copyright (C) 2004 Jean-Christophe Baillie.  All rights reserved.
+ * Copyright (C) 2004, 2006 Jean-Christophe Baillie.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,13 +20,13 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 **********************************************************************/
 
-/*
- * This simple demonstration program save to a file or send to the speakers sounds coming from an Urbi server.
- * The data written is the raw data as sent by the server (ers7 = 16000Hz stereo 16bit).
+/* This simple demonstration program save to a file or send to the
+ * speakers sounds coming from an Urbi server.  The data written is
+ * the raw data as sent by the server (ers7 = 16000Hz stereo 16bit).
  * You can use the sox program to convert it.
  */
 
-#include <stdio.h>
+#include <cstdio>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -35,26 +35,26 @@
 #include "uclient.h"
 
 #ifndef WIN32
-#include <sys/ioctl.h>
-#include <sys/soundcard.h>
+# include <sys/ioctl.h>
+# include <sys/soundcard.h>
 static const char *device="/dev/dsp";
 #endif
 
 struct wavheader {
-	char riff[4];
-	int length;
-	char wave[4];
-	char fmt[4];
-	int lnginfo;
-	short one;
-	short channels;
-	int freqechant;
-	int bytespersec;
-	short bytesperechant;
-	short bitperchannel;
-	char data[4];
-	int datalength;
-  };
+  char riff[4];
+  int length;
+  char wave[4];
+  char fmt[4];
+  int lnginfo;
+  short one;
+  short channels;
+  int freqechant;
+  int bytespersec;
+  short bytesperechant;
+  short bitperchannel;
+  char data[4];
+  int datalength;
+};
 
 
 
@@ -66,24 +66,26 @@ bool outtodsp;
 bool withheader;
 bool waswithheader;
 int totallength;
+
 UCallbackAction endProgram(const UMessage &msg) {
   if (waswithheader) {
     //fclose(file);
     //file = fopen(fname, "w");
     // seek to set correct size in wav header
-    if (fseek(file,offsetof(wavheader, length),SEEK_SET)==-1) {
-      fprintf(stderr, "warning, can't seek output file (stdout?), wav header will be incorrect!\n");
-      exit(0);
+    if (fseek(file,offsetof(wavheader, length),SEEK_SET)==-1)
+      {
+	fprintf(stderr, "warning, can't seek output file (stdout?), wav header will be incorrect!\n");
+	exit(0);
     }
     int len = totallength  - 8;
     fwrite(&len, 1, 4, file);
     fseek(file,offsetof(wavheader, datalength),SEEK_SET);
-	len = totallength - 44;
+    len = totallength - 44;
     fwrite(&len, 1, 4, file);
     fclose(file);
     exit(0);
   }
-   exit(0);
+  exit(0);
   return URBI_CONTINUE;
 }
 
@@ -100,7 +102,7 @@ UCallbackAction getSound(const UMessage &msg)
       out.channels = 2;
       out.sampleSize = 16;
       out.rate = 16000;
-      out.sampleFormat = urbi::SAMPLE_SIGNED;   
+      out.sampleFormat = urbi::SAMPLE_SIGNED;
     }
     else {
       out.channels = 0;
@@ -109,9 +111,9 @@ UCallbackAction getSound(const UMessage &msg)
       out.sampleFormat = (urbi::USoundSampleFormat)0;
     }
   }
- 
-  if (msg.type != MESSAGE_DATA 
-	  || msg.value->type != urbi::DATA_BINARY 
+
+  if (msg.type != MESSAGE_DATA
+      || msg.value->type != urbi::DATA_BINARY
       || msg.value->binary->type != urbi::BINARY_SOUND)
     return URBI_CONTINUE;
   out.soundFormat = withheader? urbi::SOUND_WAV:urbi::SOUND_RAW;
@@ -125,10 +127,10 @@ UCallbackAction getSound(const UMessage &msg)
 
 int main(int argc, char *argv[])
 {
-  char *usage = "usage:  urbisound robot milisecondtime  :plays sound recorded by the aibo to /dev/dsp\n" 
+  const char *usage = "usage:  urbisound robot milisecondtime  :plays sound recorded by the aibo to /dev/dsp\n"
     "\turbisound robot milisecondtime file [withoutheader] : write recorded sound to a file, with a wav header except if argument withoutheader is set to anything\n";
   //16000 1 16
-  if (argc < 3) {
+  if (argc != 3) {
     printf(usage);
     exit(1);
   }
@@ -136,10 +138,10 @@ int main(int argc, char *argv[])
   int time = strtol(argv[2], NULL, 0);
 
   if (argc >= 4) {
-    outtodsp = false;	 
-    if (!strcmp(argv[3],"-")) 
+    outtodsp = false;
+    if (!strcmp(argv[3],"-"))
       file=stdout;
-    else 
+    else
       file = fopen(argv[3], "wb+");
     fname = argv[3];
     if (file==0) {
@@ -147,41 +149,57 @@ int main(int argc, char *argv[])
       exit(2);
     }
 
-    if (argc==5) 
+    if (argc==5)
       withheader = false;
-    else 
+    else
       withheader = true;
   }
   else {
 #ifdef WIN32
-	printf("output to soundcard not supported under windows\n");
-	exit(2);
+    printf("output to soundcard not supported under windows\n");
+    exit(2);
 #else
-    outtodsp = true;	 
+    outtodsp = true;
     withheader = false;
     file = fopen(device, "wb" );
     if (file==0) {
       printf("error opening device\n"), exit(2);
-    } 
-	int f = fileno(file);
+    }
+    int f = fileno(file);
     int param;
+
     param=16;
-    if (ioctl(f, SNDCTL_DSP_SAMPLESIZE, &param)==-1)  {fprintf(stderr,"failed to set sample size for %s\n", device); exit(1);}
+    if (ioctl(f, SNDCTL_DSP_SAMPLESIZE, &param)==-1)
+      {
+	fprintf(stderr,"failed to set sample size for %s\n", device);
+	exit(1);
+      }
+
     param=1;
-    if (ioctl (f, SNDCTL_DSP_STEREO, &param)==-1) {fprintf(stderr,"failed to set stereo for %s\n", device); exit(1);}
+    if (ioctl (f, SNDCTL_DSP_STEREO, &param)==-1)
+      {
+	fprintf(stderr,"failed to set stereo for %s\n", device);
+	exit(1);
+      }
+
     param=16000;
-    if (ioctl (f, SNDCTL_DSP_SPEED, &param) == -1) {fprintf(stderr,"failed to set speed for %s\n", device); exit(1);}
+    if (ioctl (f, SNDCTL_DSP_SPEED, &param) == -1)
+      {
+	fprintf(stderr,"failed to set speed for %s\n", device);
+	exit(1);
+      }
 #endif
   }
-  
+
   waswithheader = withheader;
   client = new UClient(argv[1]);
   if (client->error() != 0)
     exit(0);
-  
+
   client->setCallback(getSound, "usound");
   client->setCallback(endProgram, "end");
-  
-  client->send("loopsound:loop usound: micro.val ,   { wait(%d); stop loopsound; wait(1000); end:ping }, ", time);
+
+  client->send("loopsound:loop usound: micro.val ,"
+	       "{ wait(%d); stop loopsound; wait(1000); end:ping }, ", time);
   urbi::execute();
 }
