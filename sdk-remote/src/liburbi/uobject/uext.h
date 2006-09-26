efa1ba71c24e98ee354b24a5752e48a0d7c35c58
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-09-26 19:00:15 +0000

    Fixes. 	* src/liburbi/uobject/uext.h: Add a missing "public".
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@162 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/src/liburbi/uobject/uext.h
+++ sdk-remote/src/liburbi/uobject/uext.h
@@ -1,6 +1,8 @@
-#ifndef URBIEXT_H
-#define URBIEXT_H
 // Hash maps, depending on the environment
+
+#ifndef URBIEXT_H
+# define URBIEXT_H
+
 # ifndef _MSC_VER  //GCC
 #  include <hash_map.h>
 # elif (_MSC_VER == 1400)
@@ -9,8 +11,10 @@
 # else
 #  include <hash_map>
 # endif
-#include <string>
-#include <string.h>
+# include <string>
+# include <cstring>
+
+
 // A quick hack to be able to use hash_map with string easily
 
 # ifndef _MSC_VER
@@ -21,9 +25,13 @@
   namespace __gnu_cxx {
 #   endif
 
-template<> struct hash< std::string > {
-   size_t operator()( const std::string& x ) const
-     { return hash< const char* >()( x.c_str() );}
+template<> 
+struct hash< std::string > 
+{
+  size_t operator()( const std::string& x ) const
+  { 
+    return hash< const char* >()( x.c_str() );
+  }
 };
 
 #   if (__GNUC__ == 2)
@@ -35,25 +43,32 @@ template<> struct hash< std::string > {
 
 
 
-  namespace urbi {
-    
-    //! Used in the hash_map object to define equality of two variable names
-    struct eqStr
+namespace urbi 
+{
+
+  //! Used in the hash_map object to define equality of two variable names
+  struct eqStr
+  {
+    bool operator()(const char* s1, const char* s2) const
     {
-      bool operator()(const char* s1, const char* s2) const
-      {
-	return strcmp(s1, s2) == 0;
-      }
-    };
-    template<class K, class V> class hash_map_type {
-	public:
-		  typedef __gnu_cxx::hash_map<K, V> type;
-    };
-    
-    template<class V> class hash_map_type<const char *, V> {
-      typedef __gnu_cxx::hash_map<const char *, V, hash<const char *>, eqStr> type;
-    };
-  }
+      return strcmp(s1, s2) == 0;
+    }
+  };
+
+  template<class K, class V> 
+  class hash_map_type
+  {
+  public:
+    typedef __gnu_cxx::hash_map<K, V> type;
+  };
+
+  template<class V>
+  class hash_map_type<const char *, V>
+  {
+  public:
+    typedef __gnu_cxx::hash_map<const char *, V, hash<const char *>, eqStr> type;
+  };
+}
 
 #else //_MSC_VER
 
@@ -66,42 +81,51 @@ _STD_BEGIN
 #endif
 //msc does not define a hash function for hash_compare
 
-template<> class hash_compare<const char*> {
-public:
-	 enum
-    {	// parameters for hash table
-	bucket_size = 4,	// 0 < bucket_size
-	min_buckets = 8
-    };	// min_buckets = 2 ^^ N, 0 < N
-	 
-	 size_t operator ()(const char *c) const {		 
-		size_t r = 0;
-		while (*c!=0) {
-			r = (*c)+31*r;
-			c++;
-		}
-		return r;
-	}
-	 bool operator()(const char* _Keyval1, const char* _Keyval2) const
-    {	// test if _Keyval1 ordered before _Keyval2
-	return strcmp(_Keyval1, _Keyval2)<0;
-    }
-};	
-template<> class hash_compare<std::string> {
-  public:
-    enum
-    {	// parameters for hash table
-	bucket_size = 4,	// 0 < bucket_size
-	min_buckets = 8
-    };	// min_buckets = 2 ^^ N, 0 < N
+template<>
+class hash_compare<const char*>
+{
+ public:
+  enum
+  {	// parameters for hash table
+    bucket_size = 4,	// 0 < bucket_size
+    min_buckets = 8
+  };	// min_buckets = 2 ^^ N, 0 < N
 
-    size_t operator()( const std::string& x ) const
-    { return hash_compare< const char* >()( x.c_str() );}
+  size_t operator ()(const char *c) const
+  {
+    size_t r = 0;
+    while (*c!=0)
+      {
+	r = (*c)+31*r;
+	c++;
+      }
+    return r;
+  }
+  bool operator()(const char* _Keyval1, const char* _Keyval2) const
+  {	// test if _Keyval1 ordered before _Keyval2
+    return strcmp(_Keyval1, _Keyval2)<0;
+  }
+};
 
-    bool operator()(const std::string& _Keyval1, const std::string& _Keyval2) const
-    {	// test if _Keyval1 ordered before _Keyval2
-	return (_Keyval1< _Keyval2);
-    }
+template<>
+class hash_compare<std::string>
+{
+ public:
+  enum
+  {	// parameters for hash table
+    bucket_size = 4,	// 0 < bucket_size
+    min_buckets = 8
+  };	// min_buckets = 2 ^^ N, 0 < N
+
+  size_t
+  operator()( const std::string& x ) const
+  { return hash_compare< const char* >()( x.c_str() );}
+
+  bool
+  operator()(const std::string& _Keyval1, const std::string& _Keyval2) const
+  {	// test if _Keyval1 ordered before _Keyval2
+    return (_Keyval1< _Keyval2);
+  }
 };
 
 #if (_MSC_VER == 1400)
@@ -109,14 +133,18 @@ _STDEXT_END
 #else
 _STD_END
 #endif
-namespace urbi {
-	template<class K, class V> class hash_map_type {
-	public:
-		typedef ::HASH_NS::hash_map<K, V> type;
-	};
-	
+namespace urbi 
+{
+  template<class K, class V> 
+  class hash_map_type 
+  {
+  public:
+    typedef ::HASH_NS::hash_map<K, V> type;
+  };
+
 }
 #undef HASH_NS
 #endif // _MSC_VER
-#endif //URBIEXT_H
 
+
+#endif //URBIEXT_H
