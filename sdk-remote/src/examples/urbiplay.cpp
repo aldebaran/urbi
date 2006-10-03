1a22849b947b4b06873a3ae944ba9450c0a36124
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-10-03 16:14:53 +0000

    Formatting changes. 	* src/examples/urbiplay.cpp: Do that.
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@184 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/src/examples/urbiplay.cpp
+++ sdk-remote/src/examples/urbiplay.cpp
@@ -32,25 +32,29 @@ UDev* devices;
 int devCount;
 char dumpMode;
 
-bool parseHeader(FILE *f) {
+bool parseHeader(FILE *f)
+{
   char buff[4];
   if (fread(buff,4,1,f)!=1) return false;
   if (strncmp(buff,"URBI",4)) return false;
   if (fread(&devCount,4,1,f)!=1) return false;
   devices=(UDev*)malloc(devCount*sizeof(UDev));
-  for (int i=0;i<devCount;i++) {
-	char device[256];
-	int pos=0;
-	do {
-	  if ((device[pos++]=fgetc(f))==EOF) return false;
-	}
-	while (device[pos-1]);
-	devices[i].name = strdup(device);
-	if (fread(&devices[i].id,2,1,f)!=1) return false;
-	int type;
-	if ( (type=fgetc(f)) ==EOF) return false;
-	devices[i].type=(UType)type;
-  }
+  for (int i=0;i<devCount;i++)
+    {
+      char device[256];
+      int pos=0;
+      do {
+	if ((device[pos++]=fgetc(f)) == EOF) 
+	  return false;
+      } while (device[pos-1]);
+      devices[i].name = strdup(device);
+      if (fread(&devices[i].id,2,1,f)!=1) 
+	return false;
+      int type;
+      if ((type=fgetc(f)) == EOF) 
+	return false;
+      devices[i].type=(UType)type;
+    }
   return true;
 }
 
@@ -63,57 +67,63 @@ void play(urbi::UClient * robot, FILE *f)
   int starttime=0;
   bool commandPending = false;
   int lastCommandTime=0;
-  while (fread(&uc,sizeof(uc),1,f)==1) {
-    int sleeptime=uc.timestamp;
-    if (!starttime) {
-      starttime=robot->getCurrentTime()-sleeptime-1;
-      lastCommandTime = 0;
+  while (fread(&uc,sizeof(uc),1,f)==1)
+    {
+      int sleeptime=uc.timestamp;
+      if (!starttime)
+	{
+	  starttime=robot->getCurrentTime()-sleeptime-1;
+	  lastCommandTime = 0;
+	}
+      int sleepstop=sleeptime+starttime; //when command should be executed in our timeframe
+      //find the device
+      UDev * dev=NULL;
+      for (int i=0;i<devCount;i++)
+	if (devices[i].id==uc.id) 
+	  {
+	    dev=&devices[i];
+	    break;
+	  }
+      if (!dev)
+	{
+	  fprintf(stderr,"device id %d not found\n",(int)uc.id);
+	  continue;
+	}
+      if (robot)
+	{
+	  if (lastCommandTime != uc.timestamp)
+	    {
+	      if (commandPending)
+		robot->send("noop;");
+	      if (sleepstop-robot->getCurrentTime() > 500)
+		//queue no more than 500 ms in advance
+		usleep((sleepstop-robot->getCurrentTime()-500)*1000);
+	    }
+	  commandPending = true;
+	  lastCommandTime = uc.timestamp;
+	  robot->send("%s.val = %f&",dev->name,uc.value.angle);
+	}
+      else
+	{
+	  if (dumpMode=='-')
+	    printf("%d %s.val = %f\n",sleepstop, dev->name,uc.value.angle);
+	  else
+	    {
+	      if ( uc.timestamp!=lastCommandTime)
+		printf("noop;\n");
+	      lastCommandTime = uc.timestamp;
+	      printf("%s.val = %f&\n", dev->name,uc.value.angle);
+	    }
+	}
+      if (!(tick%1000))
+	{
+	  if (tick)
+	    fprintf( stderr,"%f cps\n",
+		     1000000.0/(float)(robot->getCurrentTime()-ttime));
+	  ttime=robot->getCurrentTime();
+	}
+      tick++;
     }
-    int sleepstop=sleeptime+starttime; //when command should be executed in our timeframe
-    //find the device
-    UDev * dev=NULL;
-    for (int i=0;i<devCount;i++)
-      if (devices[i].id==uc.id) {dev=&devices[i];break;}
-    if (!dev)
-      {
-	fprintf(stderr,"device id %d not found\n",(int)uc.id);
-	continue;
-      }
-    if (robot)
-      {
-	if (lastCommandTime != uc.timestamp)
-	  {
-	    if (commandPending)
-	      robot->send("noop;");
-	    if (sleepstop-robot->getCurrentTime() > 500)
-	      //queue no more than 500 ms in advance
-	      usleep((sleepstop-robot->getCurrentTime()-500)*1000);
-	  }
-	commandPending = true;
-	lastCommandTime = uc.timestamp;
-	robot->send("%s.val = %f&",dev->name,uc.value.angle);
-      }
-    else
-      {
-	if (dumpMode=='-')
-	  printf("%d %s.val = %f\n",sleepstop, dev->name,uc.value.angle);
-	else
-	  {
-	    if ( uc.timestamp!=lastCommandTime)
-	      printf("noop;\n");
-	    lastCommandTime = uc.timestamp;
-	    printf("%s.val = %f&\n", dev->name,uc.value.angle);
-	  }
-      }
-    if (!(tick%1000))
-      {
-	if (tick)
-	  fprintf( stderr,"%f cps\n",
-		   1000000.0/(float)(robot->getCurrentTime()-ttime));
-	ttime=robot->getCurrentTime();
-      }
-    tick++;
-  }
 
   if (robot && commandPending)
     {
@@ -135,10 +145,11 @@ int main(int argc, char * argv[])
   }
 
   int loop=0;
-  if (argc>3) {
-    loop=1;
-    loop=strtol(argv[3],NULL,0);
-  }
+  if (argc>3)
+    {
+      loop=1;
+      loop=strtol(argv[3],NULL,0);
+    }
 
   urbi::UClient * robot;
 
@@ -147,13 +158,15 @@ int main(int argc, char * argv[])
     robot=NULL;
     dumpMode=argv[1][0];
   }
-  else {
-    robot=new urbi::UClient(argv[1]);
-    robot->start();
-    if (robot->error()) exit(4);
-  }
-
-  if (robot) robot->send("motoron;");
+  else 
+    {
+      robot=new urbi::UClient(argv[1]);
+      robot->start();
+      if (robot->error()) exit(4);
+    }
+  
+  if (robot) 
+    robot->send("motoron;");
 
   FILE * f;
   do {
