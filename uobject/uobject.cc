c1a14deafe0918d59c29e531ef7e6cdff3183537
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2006-10-10 11:22:34 +0000

    uobject.cc in urbi::. 	* uobject/uobject.cc:  Define in urbi:: the entities declared in it.
    git-svn-id: file:///srv/svn/kernel1/trunk@211 1e37f2df-8004-0410-ba96-adcf012cb36e

--- uobject/uobject.cc
+++ uobject/uobject.cc
@@ -28,8 +28,6 @@ For more information, comments, bug reports: http://www.urbiforge.com
 #include "uobject.h"
 #include "utypes.h"
 
-using namespace urbi;
-
 #ifdef _MSC_VER
 #define snprintf _snprintf
 #define vsnprintf _vsnprintf
@@ -113,312 +111,315 @@ namespace urbi
     ghost->received((const unsigned char *)(buf),size);
     ghost->newDataAdded = true;
   }
-}
 
-void
-urbi::main(int argc, char *argv[])
-{
-  // no effect here
-}
 
-// **************************************************************************
-//! UGenericCallback constructor.
-UGenericCallback::UGenericCallback(const std::string& objname,
-				   const std::string& type,
-				   const std::string& name,
-				   int size,  UTable &t)
-  : storage(0), objname(objname), name(name)
-{
-  nbparam = size;
+  void
+  main(int argc, char *argv[])
+  {
+    // no effect here
+  }
 
-  if (type == "function"
-      || type== "event"
-      || type=="eventend")
+  // **************************************************************************
+  //! UGenericCallback constructor.
+  UGenericCallback::UGenericCallback(const std::string& objname,
+				     const std::string& type,
+				     const std::string& name,
+				     int size,  UTable &t)
+    : storage(0), objname(objname), name(name)
+  {
+    nbparam = size;
+
+    if (type == "function"
+	|| type== "event"
+	|| type=="eventend")
+      t[this->name].push_back(this);
+
+    if (type == "var" || type=="var_onrequest")
+      {
+	HMvariabletab::iterator it = ::urbiserver->variabletab.find(name.c_str());
+	if (it == ::urbiserver->variabletab.end())
+	  {
+	    UVariable *variable = new UVariable(name.c_str(), new ::UValue());
+	    if (variable) variable->internalBinder.push_back(this);
+	  }
+	else
+	  it->second->internalBinder.push_back(this);
+      }
+
+    if (type == "varaccess")
+      {
+	HMvariabletab::iterator it = ::urbiserver->variabletab.find(name.c_str());
+	if (it == ::urbiserver->variabletab.end())
+	  {
+	    UVariable *variable = new UVariable(name.c_str(), new ::UValue());
+	    if (variable)
+	      variable->internalAccessBinder.push_back(this);
+	  }
+	else
+	  it->second->internalAccessBinder.push_back(this);
+      }
+  }
+
+  //! UGenericCallback constructor.
+  UGenericCallback::UGenericCallback(const std::string& objname,
+				     const std::string& type,
+				     const std::string& name, UTable &t)
+    : storage(0), objname(objname), name(name)
+  {
     t[this->name].push_back(this);
+  }
 
-  if (type == "var" || type=="var_onrequest")
-    {
-      HMvariabletab::iterator it = ::urbiserver->variabletab.find(name.c_str());
-      if (it == ::urbiserver->variabletab.end())
-	{
-	  UVariable *variable = new UVariable(name.c_str(), new ::UValue());
-	  if (variable) variable->internalBinder.push_back(this);
-	}
-      else
-	it->second->internalBinder.push_back(this);
-    }
-
-  if (type == "varaccess")
-    {
-      HMvariabletab::iterator it = ::urbiserver->variabletab.find(name.c_str());
-      if (it == ::urbiserver->variabletab.end())
-	{
-	  UVariable *variable = new UVariable(name.c_str(), new ::UValue());
-	  if (variable)
-	    variable->internalAccessBinder.push_back(this);
-	}
-      else
-	it->second->internalAccessBinder.push_back(this);
-    }
-}
-
-//! UGenericCallback constructor.
-UGenericCallback::UGenericCallback(const std::string& objname,
-				   const std::string& type,
-				   const std::string& name, UTable &t)
-  : storage(0), objname(objname), name(name)
-{
-  t[this->name].push_back(this);
-}
-
-UGenericCallback::~UGenericCallback()
-{
-}
+  UGenericCallback::~UGenericCallback()
+  {
+  }
 
 
-// **************************************************************************
-//! UTimerCallbacl constructor.
+  // **************************************************************************
+  //! UTimerCallbacl constructor.
 
-UTimerCallback::UTimerCallback(const std::string& objname,
-			       ufloat period, UTimerTable &tt)
-  : period(period),
-    objname(objname)
-{
-  tt.push_back(this);
-  lastTimeCalled = -9999999;
-}
+  UTimerCallback::UTimerCallback(const std::string& objname,
+				 ufloat period, UTimerTable &tt)
+    : period(period),
+      objname(objname)
+  {
+    tt.push_back(this);
+    lastTimeCalled = -9999999;
+  }
 
-UTimerCallback::~UTimerCallback()
-{
-}
-
-
-// **************************************************************************
-//  Monitoring functions
-
-//! Generic UVar monitoring without callback
-void
-UObject::USync(UVar &v)
-{
-  UNotifyChange(v,&UObject::voidfun);
-}
-
-// **************************************************************************
-//! UObject constructor.
-UObject::UObject(const std::string &s)
-  : __name(s)
-{
-  objecthub = 0;
-  autogroup = false;
-  lastUObject = this;
-  UString tmps(__name.c_str()); // quelle merde ces UString!!!!
-  UObj* tmpobj = new UObj(&tmps);
-
-  for (urbi::UStartlist::iterator retr = urbi::objectlist->begin();
-       retr != urbi::objectlist->end();
-       retr++)
-    if ((*retr)->name == __name)
-      tmpobj->internalBinder = (*retr);
-
-  // default
-  derived = false;
-  classname = __name;
-
-  UBindVar(UObject,load);
-  load = 1;
-}
+  UTimerCallback::~UTimerCallback()
+  {
+  }
 
 
-//! Clean a callback UTable from all callbacks linked to the object whose name is 'name'
-void
-cleanTable(UTable &t, std::string name)
-{
-  std::list<UTable::iterator> todelete;
-  for (UTable::iterator it = t.begin();
-       it != t.end();
-       ++it)
-    {
-      std::list<UGenericCallback*>& tocheck = (*it).second;
-      for (std::list<UGenericCallback*>::iterator it2 = tocheck.begin();
-	   it2 != tocheck.end();
-	   )
-	{
-	  if ((*it2)->objname == name)
-	    {
-	      delete *it2;
-	      it2 = tocheck.erase(it2);
-	    }
-	  else
-	    ++it2;
-	}//for
+  // **************************************************************************
+  //  Monitoring functions
 
-      if (tocheck.empty())
-	todelete.push_back(it);
-    }//for
+  //! Generic UVar monitoring without callback
+  void
+  UObject::USync(UVar &v)
+  {
+    UNotifyChange(v,&UObject::voidfun);
+  }
 
-  for (std::list<UTable::iterator>::iterator dit = todelete.begin();
-       dit != todelete.end();
-       ++dit)
-    {
-      t.erase(*dit);
-    }
-}//function
+  // **************************************************************************
+  //! UObject constructor.
+  UObject::UObject(const std::string &s)
+    : __name(s)
+  {
+    objecthub = 0;
+    autogroup = false;
+    lastUObject = this;
+    UString tmps(__name.c_str()); // quelle merde ces UString!!!!
+    UObj* tmpobj = new UObj(&tmps);
 
+    for (UStartlist::iterator retr = urbi::objectlist->begin();
+	 retr != objectlist->end();
+	 retr++)
+      if ((*retr)->name == __name)
+	tmpobj->internalBinder = (*retr);
 
-//! Clean a callback UTimerTable from all callbacks linked to the object whose name is 'name'
-void
-cleanTimerTable(UTimerTable &t, std::string name)
-{
-  for (UTimerTable::iterator it = t.begin();
-       it != t.end();
-       )
-    {
-      if ((*it)->objname == name)
-	{
-	  delete *it;
-	  it = t.erase(it);
-	}
-      else
-	++it;
-    }//for
-}//function
+    // default
+    derived = false;
+    classname = __name;
+
+    UBindVar(UObject,load);
+    load = 1;
+  }
 
 
-//! UObject cleaner
-void
-UObject::clean()
-{
-  cleanTable(functionmap, __name);
-  cleanTable(eventmap, __name);
-  cleanTable(eventendmap, __name);
+  //! Clean a callback UTable from all callbacks linked to the object whose name is 'name'
+  void
+  cleanTable(UTable &t, std::string name)
+  {
+    std::list<UTable::iterator> todelete;
+    for (UTable::iterator it = t.begin();
+	 it != t.end();
+	 ++it)
+      {
+	std::list<UGenericCallback*>& tocheck = (*it).second;
+	for (std::list<UGenericCallback*>::iterator it2 = tocheck.begin();
+	     it2 != tocheck.end();
+	     )
+	  {
+	    if ((*it2)->objname == name)
+	      {
+		delete *it2;
+		it2 = tocheck.erase(it2);
+	      }
+	    else
+	      ++it2;
+	  }//for
 
-  cleanTimerTable(timermap, __name);
-  cleanTimerTable(updatemap, __name);
+	if (tocheck.empty())
+	  todelete.push_back(it);
+      }//for
 
-  if (objecthub)
-    objecthub->members.remove(this);
-}
+    for (std::list<UTable::iterator>::iterator dit = todelete.begin();
+	 dit != todelete.end();
+	 ++dit)
+      {
+	t.erase(*dit);
+      }
+  }//function
+
+
+  //! Clean a callback UTimerTable from all callbacks linked to the object whose name is 'name'
+  void
+  cleanTimerTable(UTimerTable &t, std::string name)
+  {
+    for (UTimerTable::iterator it = t.begin();
+	 it != t.end();
+	 )
+      {
+	if ((*it)->objname == name)
+	  {
+	    delete *it;
+	    it = t.erase(it);
+	  }
+	else
+	  ++it;
+      }//for
+  }//function
+
+
+  //! UObject cleaner
+  void
+  UObject::clean()
+  {
+    cleanTable(functionmap, __name);
+    cleanTable(eventmap, __name);
+    cleanTable(eventendmap, __name);
+
+    cleanTimerTable(timermap, __name);
+    cleanTimerTable(updatemap, __name);
+
+    if (objecthub)
+      objecthub->members.remove(this);
+  }
 
 
 
-//! UObject destructor.
-UObject::~UObject()
-{
-  clean();
-}
+  //! UObject destructor.
+  UObject::~UObject()
+  {
+    clean();
+  }
 
-void
-UObject::UJoinGroup(const std::string& gpname)
-{
-  HMgrouptab::iterator hma;
-  UGroup *g;
+  void
+  UObject::UJoinGroup(const std::string& gpname)
+  {
+    HMgrouptab::iterator hma;
+    UGroup *g;
 
-  hma = ::urbiserver->grouptab.find(gpname.c_str());
-  if (hma != ::urbiserver->grouptab.end())
-    g = hma->second;
-  else
-    {
-      g = new UGroup(new UString(gpname.c_str()));
-      ::urbiserver->grouptab[g->name->str()] = g;
-    }
+    hma = ::urbiserver->grouptab.find(gpname.c_str());
+    if (hma != ::urbiserver->grouptab.end())
+      g = hma->second;
+    else
+      {
+	g = new UGroup(new UString(gpname.c_str()));
+	::urbiserver->grouptab[g->name->str()] = g;
+      }
 
-  g->members.push_back(new UString(__name.c_str()));
-}
+    g->members.push_back(new UString(__name.c_str()));
+  }
 
-void
-UObject::USetUpdate(ufloat t)
-{
-  period = t;
-  new UTimerCallbackobj<UObject>(__name, t, this, &UObject::update, updatemap);
-}
+  void
+  UObject::USetUpdate(ufloat t)
+  {
+    period = t;
+    new UTimerCallbackobj<UObject>(__name, t, this,
+				   &UObject::update, updatemap);
+  }
 
-// **************************************************************************
-//! UObjectHub constructor.
+  // **************************************************************************
+  //! UObjectHub constructor.
 
-UObjectHub::UObjectHub(const std::string& s) : name(s)
-{
-}
+  UObjectHub::UObjectHub(const std::string& s) : name(s)
+  {
+  }
 
-//! UObjectHub destructor.
-UObjectHub::~UObjectHub()
-{
-}
+  //! UObjectHub destructor.
+  UObjectHub::~UObjectHub()
+  {
+  }
 
-void
-UObjectHub::USetUpdate(ufloat t)
-{
-  period = t;
-  new UTimerCallbackobj<UObjectHub>(name, t, this, &UObjectHub::updateGlobal, updatemap);
-}
+  void
+  UObjectHub::USetUpdate(ufloat t)
+  {
+    period = t;
+    new UTimerCallbackobj<UObjectHub>(name, t, this,
+				      &UObjectHub::updateGlobal, updatemap);
+  }
 
-int
-UObjectHub::updateGlobal()
-{
-  for (UObjectList::iterator it = members.begin();
-       it != members.end();
-       it++)
-    (*it)->update();
-  update();
-  return 0;
-}
+  int
+  UObjectHub::updateGlobal()
+  {
+    for (UObjectList::iterator it = members.begin();
+	 it != members.end();
+	 it++)
+      (*it)->update();
+    update();
+    return 0;
+  }
 
-void
-UObjectHub::addMember(UObject* obj)
-{
-  members.push_back(obj);
-}
+  void
+  UObjectHub::addMember(UObject* obj)
+  {
+    members.push_back(obj);
+  }
 
-UObjectList*
-UObjectHub::getSubClass(const std::string& subclass)
-{
-  UObjectList* res = new UObjectList();
-  for (UObjectList::iterator it = members.begin();
-       it != members.end();
-       it++)
-    if ((*it)->classname == subclass)
-      res->push_back(*it);
+  UObjectList*
+  UObjectHub::getSubClass(const std::string& subclass)
+  {
+    UObjectList* res = new UObjectList();
+    for (UObjectList::iterator it = members.begin();
+	 it != members.end();
+	 it++)
+      if ((*it)->classname == subclass)
+	res->push_back(*it);
 
-  return(res);
-}
-
-
-//! retrieve a UObjectHub based on its name
-urbi::UObjectHub*
-urbi::getUObjectHub(const std::string& name)
-{
-  for (urbi::UStartlistHub::iterator retr = urbi::objecthublist->begin();
-       retr != urbi::objecthublist->end();
-       retr++)
-    if ((*retr)->name == name)
-      return (*retr)->getUObjectHub();
-
-  return 0;
-}
-
-//! retrieve a UObject based on its name
-urbi::UObject*
-urbi::getUObject(const std::string& name)
-{
-  for (urbi::UStartlist::iterator retr = urbi::objectlist->begin();
-       retr != urbi::objectlist->end();
-       retr++)
-    if ((*retr)->name == name)
-      return (*retr)->getUObject();
-
-  return 0;
-}
+    return(res);
+  }
 
 
-//! echo method
-void
-urbi::echo(const char* format, ... ) {
+  //! retrieve a UObjectHub based on its name
+  UObjectHub*
+  getUObjectHub(const std::string& name)
+  {
+    for (UStartlistHub::iterator retr = objecthublist->begin();
+	 retr != objecthublist->end();
+	 retr++)
+      if ((*retr)->name == name)
+	return (*retr)->getUObjectHub();
 
-  char tmpoutput[1024];
+    return 0;
+  }
 
-  va_list arg;
-  va_start(arg, format);
-  vsnprintf(tmpoutput, 1024, format, arg);
-  va_end(arg);
+  //! retrieve a UObject based on its name
+  UObject*
+  getUObject(const std::string& name)
+  {
+    for (UStartlist::iterator retr = objectlist->begin();
+	 retr != objectlist->end();
+	 retr++)
+      if ((*retr)->name == name)
+	return (*retr)->getUObject();
 
-  ::urbiserver->debug(tmpoutput);
+    return 0;
+  }
+
+  //! echo method
+  void
+  echo(const char* format, ... )
+  {
+    char tmpoutput[1024];
+
+    va_list arg;
+    va_start(arg, format);
+    vsnprintf(tmpoutput, 1024, format, arg);
+    va_end(arg);
+
+    ::urbiserver->debug(tmpoutput);
+  }
+
 }
