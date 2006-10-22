a8384efe29336eaaf90fc1f0a944716cd58ad651
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2006-10-22 11:21:02 +0000

    Fix parser compilation under Aibo 	* src/parser/bison/bison.mk: Include an explicit compilation 	rule for ugrammar.cc which uses PARSER_CXXFLAGS. 	This rule was copied from Automake 1.9.6's output. 	* src/Makefile.am: Don't compile console for aibo. 	(libkernel_la_CPPFLAGS): Don't define it, it makes the object 	file names longer for no good reason. 	Just let AM_CPPFLAGS do its job. 	* configure.ac: Don't define NETWORK_OPENR, OPENR suffices.
    git-svn-id: file:///srv/svn/kernel1/trunk@246 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/parser/bison/bison.mk
+++ src/parser/bison/bison.mk
@@ -18,9 +18,14 @@ ugrammar.cc
 
 BUILT_SOURCES += $(FROM_UGRAMMAR_Y)
 CLEANFILES += $(FROM_UGRAMMAR_Y) ugrammar.stamp ugrammar.output
-# We use a small library just to emulate Per-Object Flags.
-# See the Automake documentation.  We need to pass -O0 when
-# compiling ugrammar.cc with mipsel-linux-c++, to avoid this:
+nodist_libkernel_la_SOURCES = $(FROM_UGRAMMAR_Y)
+
+## ------------- ##
+## ugrammar.lo.  ##
+## ------------- ##
+
+# We need to pass -O0 when compiling ugrammar.cc with mipsel-linux-c++,
+# to avoid this:
 #
 #  {standard input}: Assembler messages:
 #  {standard input}:4842: Error: Branch out of range
@@ -32,10 +37,16 @@ CLEANFILES += $(FROM_UGRAMMAR_Y) ugrammar.stamp ugrammar.output
 #  {standard input}:5569: Error: Branch out of range
 #  {standard input}:5595: Error: Branch out of range
 #  
-noinst_LTLIBRARIES = libparser.la
-nodist_libparser_la_SOURCES = $(FROM_UGRAMMAR_Y)
-libparser_la_CXXFLAGS = $(AM_CXXFLAGS) $(PARSER_CXXFLAGS)
-libkernel_la_LIBADD = libparser.la
+# This is an edited copy of Automake's compilation rule so that we can
+# insert $(PARSER_CXXFLAGS) *after* $(CXXFLAGS) to override -O2.
+# Using foo_CXXFLAGS does not suffice, since CXXFLAGS, a user
+# variable, appears last.
+ugrammar.lo: ugrammar.cc
+@am__fastdepCXX_TRUE@	if $(LTCXXCOMPILE) $(PARSER_CXXFLAGS) -MT ugrammar.lo -MD -MP -MF "$(DEPDIR)/ugrammar.Tpo" -c -o ugrammar.lo `test -f 'ugrammar.cc' || echo '$(srcdir)/'`ugrammar.cc; \
+@am__fastdepCXX_TRUE@	then mv -f "$(DEPDIR)/ugrammar.Tpo" "$(DEPDIR)/ugrammar.Plo"; else rm -f "$(DEPDIR)/ugrammar.Tpo"; exit 1; fi
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	source='ugrammar.cc' object='ugrammar.lo' libtool=yes @AMDEPBACKSLASH@
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	DEPDIR=$(DEPDIR) $(CXXDEPMODE) $(depcomp) @AMDEPBACKSLASH@
+@am__fastdepCXX_FALSE@	$(LTCXXCOMPILE) $(PARSER_CXXFLAGS) -c -o ugrammar.lo `test -f 'ugrammar.cc' || echo '$(srcdir)/'`ugrammar.cc
 
 # Compile the parser and save cycles.
 # This code comes from "Handling Tools that Produce Many Outputs",
@@ -68,7 +79,7 @@ utoken.cc
 BUILT_SOURCES += $(FROM_UTOKEN_L)
 CLEANFILES += $(FROM_UTOKEN_L) utoken.stamp
 dist_libkernel_la_SOURCES += $(parsedir)/flex-lexer.hh
-nodist_libkernel_la_SOURCES = $(FROM_UTOKEN_L)
+nodist_libkernel_la_SOURCES += $(FROM_UTOKEN_L)
 
 EXTRA_DIST += $(parsedir)/utoken.l
 utoken.stamp: $(parsedir)/utoken.l $(parsedir)/bison.mk
