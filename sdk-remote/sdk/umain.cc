7779eba3ce67489adccc32badf34329a99115d87
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-11-01 11:19:29 +0000

    encore un... 	* sdk/umain.cc: uobject.hh.
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@242 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/sdk/umain.cc
+++ sdk-remote/sdk/umain.cc
@@ -18,7 +18,7 @@
 
  **************************************************************************** */
 
-#include <uobject/uobject.h>
+#include <uobject/uobject.hh>
 #ifdef WIN32
 # include <windows.h>
 # define usleep(a) Sleep(a/1000)
