df29280c851d48d22c3475926f77e14e9d650fbb
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-11-07 13:33:13 +0000

    liburbi installs libport 	* flags.mk: Use LIBPORT_CPPFLAGS. 	* configure.ac: nostdinc. 	* Makefile.am: Don't include libport.mk, we use its Makefile.am.
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@255 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/flags.mk
+++ sdk-remote/flags.mk
@@ -1,11 +1,11 @@
 include $(top_srcdir)/build-aux/init.mk
 
-# Find libjpeg's src (jpeglib.h) and build (jconfig.h) files.
-AM_CPPFLAGS += -I$(top_srcdir) -I$(top_builddir)/jpeg-6b
+# Find libport's sources and config.h.
+AM_CPPFLAGS += $(LIBPORT_CPPFLAGS)
+# Find libjpeg's build (jconfig.h) files.
+AM_CPPFLAGS += -I$(top_builddir)/jpeg-6b
 # Find urbi/ headers.
 AM_CPPFLAGS += -I$(top_srcdir)/include
-# Find libport's config.h.
-AM_CPPFLAGS += -I$(top_builddir)
 # Find uobject.hh.
 AM_CPPFLAGS += -I$(top_srcdir)/src/liburbi
 
