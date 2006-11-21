89df9366638c02ec79d0dbb3dedaef97f9054170
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2006-11-21 14:56:59 +0000

    Formatting changes 	* src/memorymanager/memorymanager.cc: here.
    git-svn-id: file:///srv/svn/kernel1/trunk@357 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/memorymanager/memorymanager.cc
+++ src/memorymanager/memorymanager.cc
@@ -14,7 +14,7 @@ BlockPool::BlockPool ()
 
 #ifndef DISABLE_BLOCKMEMMNGR
 void
-block_operator_delete(BlockPool* mempool, void * ptr)
+block_operator_delete(BlockPool* mempool, void* ptr)
 {
   mempool->cptr++;
   *mempool->cptr = ptr;
@@ -25,37 +25,37 @@ block_operator_delete(BlockPool* mempool, void * ptr)
 void* block_operator_new(BlockPool* &mempool, int sz)
 {
   if (!mempool)
+  {
+    mempool = new BlockPool;
+    mempool->cptr--;
+    const int align = sizeof(void*);
+    int asz = sz;
+    if (asz%align)
+      asz = asz+align-(asz%align);
+    mempool->itemSize = asz;
+  }
+
+  if (!mempool->ptr || mempool->cptr < mempool->ptr)
+  {
+    int newsize = (mempool->size*3)/2 + DEFAULT_BLOCK_SIZE;
+
+    //realloc ptr pool
+    long cpos = (long)mempool->cptr-(long)mempool->ptr; //save cptr as relative int
+    mempool->ptr = (void**) realloc(mempool->ptr, newsize*sizeof(void*));
+    mempool->cptr = (void**)((long)mempool->ptr+cpos); //restore cptr
+
+    //allocate new data bloc
+    char * data = (char * )malloc((newsize-mempool->size) * mempool->itemSize);
+    //std::cerr <<"alloc of size "<<newsize<<" "<<(void*)data<<std::endl;
+    //std::cerr << mempool->itemSize<<" for "<<sz<<std::endl;
+    //std::cerr <<"pool base: "<<mempool->ptr<<std::endl;
+    for (int i=0;i<newsize-mempool->size;i++)
     {
-      mempool = new BlockPool;
-      mempool->cptr--;
-      const int align = sizeof(void*);
-      int asz = sz;
-      if (asz%align)
-	asz = asz+align-(asz%align);
-      mempool->itemSize = asz;
+      mempool->cptr++;
+      *mempool->cptr = (data+mempool->itemSize*i);
     }
-
-  if (!mempool->ptr || mempool->cptr<mempool->ptr)
-    {
-      int newsize = (mempool->size*3)/2 + DEFAULT_BLOCK_SIZE;
-
-      //realloc ptr pool
-      long cpos = (long)mempool->cptr-(long)mempool->ptr; //save cptr as relative int
-      mempool->ptr = (void**) realloc(mempool->ptr, newsize*sizeof(void*));
-      mempool->cptr = (void**)((long)mempool->ptr+cpos); //restore cptr
-
-      //allocate new data bloc
-      char * data = (char * )malloc((newsize-mempool->size) * mempool->itemSize);
-      //std::cerr <<"alloc of size "<<newsize<<" "<<(void*)data<<std::endl;
-      //std::cerr << mempool->itemSize<<" for "<<sz<<std::endl;
-      //std::cerr <<"pool base: "<<mempool->ptr<<std::endl;
-      for (int i=0;i<newsize-mempool->size;i++)
-	{
-	  mempool->cptr++;
-	  *mempool->cptr = (data+mempool->itemSize*i);
-	}
-      mempool->size = newsize;
-    }
+    mempool->size = newsize;
+  }
 
   void* result = *mempool->cptr;
   mempool->cptr--;
