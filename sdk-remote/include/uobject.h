c4c37b9424c4c6b5a8ad8436c3c493f0e0b32576
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2006-11-21 15:56:51 +0000

    uobject.h 	* include/uobject.h: New. 	* Makefile.am: Adjust. 	* src/liburbi/uobject/urbi/uobject.hh.template: Style changes.
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@280 b8db33d8-8104-0410-b827-8403f563b8eb

---
+++ sdk-remote/include/uobject.h
@@ -0,0 +1,11 @@
+#ifndef URBI_UOBJECT_H
+# define URBI_UOBJECT_H
+
+# warn please include <urbi/uobject.hh> instead of <uobject.h>.
+# include <urbi/uobject.hh>
+
+#endif // ! URBI_UOBJECT_HH
+
+/// Local Variables:
+/// mode: c++
+/// End:
