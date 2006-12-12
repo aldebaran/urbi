4ec27fefb2d8a0c0d1e392897cad9c43e635f09d
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2006-12-12 16:30:33 +0000

    Formatting changes.
    git-svn-id: file:///srv/svn/kernel1/trunk@437 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/parser/bison/flex-lexer.hh
+++ src/parser/bison/flex-lexer.hh
@@ -1,10 +1,10 @@
+/// \file flex-lexer.hh
+
 // This file is based on FlexLexer.h, installed on systems that have
 // Flex installed.  But then, it is way too dangerous to use the same
 // name, as the installed one is read instead of this one.  So make
 // sure *not* to name this file FlexLexer.h.
 
-
-// $Header: /cvsroot/urbi/./devel-code/urbiserver/kernel/FlexLexer.h,v 1.5 2005/06/28 14:12:07 jcbaillie Exp $
 
 // FlexLexer.h -- define interfaces for lexical analyzer classes generated
 //		  by flex
@@ -47,12 +47,6 @@
 //	#include <FlexLexer.h>
 //	...
 
-// Note: july 2004-2005.=> modified by Jean-Christophe Baillie
-// Added yy::parser::semantic_type *lvalp to the definition of the yylex function.
-/*! \file flex-lexer.hh
- *******************************************************************************
-
- **************************************************************************** */
 
 #ifndef FLEX_LEXER_HH
 # define FLEX_LEXER_HH
