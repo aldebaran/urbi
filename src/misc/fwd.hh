17a0c1d568744eef76b9f327eb9135718559a50c
baillie <baillie@1e37f2df-8004-0410-ba96-adcf012cb36e>
2006-12-30 08:59:58 +0000

    add misc namespace and misc/fwd.hh 	* src/misc/fwd.hh: New.
    git-svn-id: file:///srv/svn/kernel1/branches/k1.5@624 1e37f2df-8004-0410-ba96-adcf012cb36e

---
+++ src/misc/fwd.hh
@@ -0,0 +1,16 @@
+/**
+ ** \file misc/fwd.hh
+ ** \brief Forward declarations for misc:: items.
+ */
+
+#ifndef MISC_FWD_HH
+# define MISC_FWD_HH
+
+namespace misc
+{
+  // From select-const.hh.
+  template<typename T> struct constify_traits;
+  template<typename T> struct id_traits;
+}
+
+#endif // !MISC_FWD_HH
