b3092ee738524145dcb81b3fd23a5a6c557cff10
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2007-01-04 21:34:11 +0000

    Don't use #warning for MSC 	* include/uobject.h: here. 	Besides, use #warning, not #warn.
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@340 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/include/uobject.h
+++ sdk-remote/include/uobject.h
@@ -1,8 +1,15 @@
 #ifndef URBI_UOBJECT_H
 # define URBI_UOBJECT_H
 
-# warn please include <urbi/uobject.hh> instead of <uobject.h>.
-# include <urbi/uobject.hh>
+// There is no macro expansion after #warning, we can't factor
+// these messages.
+
+# ifndef _MSC_VER
+#  warning please include <urbi/uobject.hh> instead of <uobject.h>.
+#  include <urbi/uobject.hh>
+# else
+#  error include <urbi/uobject.hh> instead of <uobject.h>.
+# endif
 
 #endif // ! URBI_UOBJECT_HH
 
