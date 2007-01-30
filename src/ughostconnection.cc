44bbc0486b5d81013a0303b5b78b121d333bac93
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-01-30 15:51:55 +0000

    Don't hard code values, use sizeof 	* src/ughostconnection.cc (UGhostConnection::effectiveSend): here. 	Add "tmp" is about the most useless word to include in an 	identifier.
    git-svn-id: file:///srv/svn/kernel1/trunk@798 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/ughostconnection.cc
+++ src/ughostconnection.cc
@@ -61,16 +61,13 @@ UGhostConnection::closeConnection()
 int
 UGhostConnection::effectiveSend(const ubyte *buffer, int length)
 {
-  char tmpbuf[1024];
-  int real_length = length;
-  if (real_length >= 1024)
-    real_length = 1023;
+  char buf[1024];
+  int len = std::min (length, (int) sizeof buf - 1);
 
-  memcpy (static_cast<void*> (tmpbuf),
-	  static_cast<const void*> (buffer),
-	  real_length);
-  tmpbuf[real_length] = 0;
-  ::urbiserver->debug("%s",tmpbuf);
+  memcpy (static_cast<void*> (buf), static_cast<const void*> (buffer),
+	  len);
+  buf[len] = 0;
+  ::urbiserver->debug("%s", buf);
 
   return length;
 }
