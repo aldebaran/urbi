aldebaran/urbi

commit 2959d0b2182327dc5cff2c9c962fa4b69cdfe962

Fix bug #146 (compilation on ARM): memory alignment and bad code. 	* src/network/bsdnet/network.cc (TCPServerPipe::init): Fix wrong usage 	of the struct hostent returned by gethostbyname: the code was 	converting a char* representing an IP address (eg: "127.0.0.1") in 	int* and then dereferencing this int in hope to get the numerical IP 	address. oO 	* src/uvalue.cc (UValue::operator urbi::USound): Declare the local 	`struct wavheader' as __attribute__ __packed__.  A raw buffer of bytes 	is casted in a pointer to struct wavheader, this requires that the 	compiler does not align the members of the structure on word 	boundaries.
git-svn-id: file:///srv/svn/kernel1/branches/1.0@1014 1e37f2df-8004-0410-ba96-adcf012cb36e

src/network/bsdnet/network.cc
@@ -83,16 +83,18 @@ namespace Network
     address.sin_port = htons((unsigned short) port);
     if (addr.empty() || addr == "0.0.0.0")
       address.sin_addr.s_addr = INADDR_ANY;
-    else {
+    else
+    {
       //attempt name resolution
       hostent* hp = gethostbyname (addr.c_str());
       if (!hp) //assume IP address in case of failure
-	address.sin_addr.s_addr = inet_addr(addr.c_str());
+        // FIXME: Check that inet_addr did not return INADDR_NONE.
+	address.sin_addr.s_addr = inet_addr (addr.c_str());
       else
-	address.sin_addr.s_addr = *(int*)hp->h_addr;
+	address.sin_addr.s_addr = inet_addr (hp->h_addr);
     }
     /* bind to port */
-    rc = bind(fd, (struct sockaddr *)&address, sizeof (struct sockaddr));
+    rc = bind(fd, (struct sockaddr*) &address, sizeof (struct sockaddr));
     if (rc == -1)
       return false;
     /* listen for connections */

src/uvalue.cc
@@ -227,7 +227,7 @@ UValue::operator urbi::USound()
     short bitperchannel;
     char data[4];
     int datalength;
-  };
+  } __attribute__ ((__packed__));
 
   urbi::USound snd;
   snd.data=0;

commit ecdf10924d6a105e25295e0a4379cbe03bd81e7a

Fix a bug introduced in r1014. 	* src/network/bsdnet/network.cc (TCPServerPipe::init): Properly 	transform the IP address returned by gethostbyname into a in_addr_t.
git-svn-id: file:///srv/svn/kernel1/branches/1.0@1015 1e37f2df-8004-0410-ba96-adcf012cb36e

src/network/bsdnet/network.cc
@@ -91,7 +91,24 @@ namespace Network
         // FIXME: Check that inet_addr did not return INADDR_NONE.
 	address.sin_addr.s_addr = inet_addr (addr.c_str());
       else
-	address.sin_addr.s_addr = inet_addr (hp->h_addr);
+      {
+        /* hp->h_addr is now a char* such as the IP is:
+         *    a.b.c.d
+         * where
+         *    a = hp->h_addr[0]
+         *    b = hp->h_addr[1]
+         *    c = hp->h_addr[2]
+         *    d = hp->h_addr[3]
+         * hence the following calculation.  Don't cast this to an int*
+         * because of the alignment problems (eg: ARM) and also because
+         * sizeof (int) is not necessarily 4.
+         */
+        in_addr_t ip = (hp->h_addr[0] << 24)
+          + (hp->h_addr[1] << 16)
+          + (hp->h_addr[2] << 8)
+          + hp->h_addr[3];
+	address.sin_addr.s_addr = ip;
+      }
     }
     /* bind to port */
     rc = bind(fd, (struct sockaddr*) &address, sizeof (struct sockaddr));