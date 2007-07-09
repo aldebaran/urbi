fa8af54eb11e862814ec6873507d6211632f14f8
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-07-09 07:40:10 +0000

    Add Runner framework 	* src/runner/Makefile.am, src/runner/runner.cc, 	* src/runner/runner.hh, src/runner/runner.hxx: New. 	* configure.ac, src/Makefile.am: Adjust.
    git-svn-id: file:///srv/svn/kernel1/branches/2.0@1215 1e37f2df-8004-0410-ba96-adcf012cb36e

---
+++ src/runner/runner.hh
@@ -0,0 +1,39 @@
+/**
+ ** \file runner/runner.hh
+ ** \brief Definition of runner::Runner.
+ */
+
+#ifndef RUNNER_RUNNER_HH
+# define RUNNER_RUNNER_HH
+
+# include <functional>
+# include <ostream>
+# include "ast/default-visitor.hh"
+
+namespace runner
+{
+
+  /// Ast pretty-printer.
+  class Runner : public ast::DefaultConstVisitor
+  {
+    /** \name Ctor & dtor.
+     ** \{ */
+  public:
+    /// Super class type.
+    typedef ast::DefaultConstVisitor super_type;
+
+    /// Construct a Runner.
+    Runner ();
+
+    /// Destroy a Runner.
+    virtual ~Runner ();
+    /** \} */
+
+    using super_type::operator();
+  };
+
+} // namespace runner
+
+# include "runner/runner.hxx"
+
+#endif // !RUNNER_RUNNER_HH
---
+++ src/runner/runner.hxx
@@ -0,0 +1,25 @@
+/**
+ ** \file runner/runner.hxx
+ ** \brief Inline implementation of runner::Runner.
+ */
+
+#ifndef RUNNER_RUNNER_HXX
+# define RUNNER_RUNNER_HXX
+
+# include "runner/runner.hh"
+
+namespace runner
+{
+
+  Runner::Runner ()
+  {}
+
+  Runner::~Runner ()
+  {}
+
+
+} // namespace runner
+
+# include "runner/runner.hxx"
+
+#endif // !RUNNER_RUNNER_HXX
