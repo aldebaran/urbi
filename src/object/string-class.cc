0b6fa0114927b152381d478275ef66b580aa435d
moulard <moulard@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-09-13 10:02:00 +0000

    Add string primitives.
    	* src/object/string-class.cc: Here.
    
    git-svn-id: file:///srv/svn/kernel1/branches/2.0@1510 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/object/string-class.cc
+++ src/object/string-class.cc
@@ -3,8 +3,10 @@
  ** \brief Creation of the URBI object string.
  */
 
-#include "object/object.hh"
 #include "object/string-class.hh"
+#include "object/object.hh"
+#include "object/primitives.hh"
+#include "object/atom.hh"
 
 namespace object
 {
@@ -14,8 +16,59 @@ namespace object
   | String primitives.  |
   `--------------------*/
 
+  namespace
+  {
+
+#define PRIMITIVE_OP_STRING(Name, Op, TypeRet)                  \
+    static rObject                                              \
+    Name (rString& a, rString& b)                               \
+    {                                                           \
+      return new TypeRet (a->value_get () Op b->value_get ());  \
+    }
+
+    PRIMITIVE_OP_STRING(equ, ==, Integer);
+    PRIMITIVE_OP_STRING(nequ, !=, Integer);
+    PRIMITIVE_OP_STRING(lth, <, Integer);
+
+#undef PRIMITIVE_OP_STRING
+
+    /// Return string's length.
+    static rObject
+    size (rString& s)
+    {
+      return new Integer (s->value_get ().name_get ().size ());
+    }
+
+  };
+
+#define PRIMITIVE_1_STRING(Name)                  \
+  PRIMITIVE_1(string, Name, Name, String)
+
+#define PRIMITIVE_2_STRING(Name, Type2)           \
+  PRIMITIVE_2(string, Name, Name, String, Type2)
+
+  PRIMITIVE_2_STRING(equ, String);
+  PRIMITIVE_2_STRING(nequ, String);
+
+  PRIMITIVE_2_STRING(lth, String);
+
+  PRIMITIVE_1_STRING(size);
+
+#undef PRIMITIVE_2_STRING
+#undef PRIMITIVE_1_STRING
+
   void
   string_class_initialize ()
   {
+#define DECLARE(Name, Implem)                   \
+    DECLARE_PRIMITIVE(string, Name, Implem)
+
+    DECLARE(==, equ);
+    DECLARE(!=, nequ);
+    DECLARE(<, lth);
+
+    DECLARE(size, size);
+    DECLARE(length, size);
+#undef DECLARE
   }
 }; // namespace object
