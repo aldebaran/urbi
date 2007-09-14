813e9e48190ae0b0059694505d16ab6042846966
sigoure <sigoure@b8db33d8-8104-0410-b827-8403f563b8eb>
2007-09-14 12:22:12 +0000

    Use the proper mailing lists to send commit messages.
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@487 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/vcs/local.rb
+++ sdk-remote/vcs/local.rb
@@ -7,7 +7,7 @@ class Vcs
 
   def local_commit! ( *args )
     common_commit!("urbi-sdk <%= rev %>: <%= title %>", *args) do |subject|
-      mail!(:to => %w[liburbi-cpp@gostai.com],
+      mail!(:to => %w[liburbi-cpp-patches@lists.gostai.com],
             :subject => subject)
     end
   end
