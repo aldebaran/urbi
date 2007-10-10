e646f75a579aec4bc423d48ef5ddf6f084ad74ce
sigoure <sigoure@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-10-10 09:20:46 +0000

    Fix the non-recursive Makefile.
    	* dev/ast-nodes-mk-gen: Prefix the paths to the files listed in the
    	piece of Makefile by `ast/'.
    	* src/ast/ast.mk: Be nicer to automake by enabling it to include the
    	pieces of Makefile and properly resolve the dependencies.  Fix the
    	names of several targets because now, file paths must be prefixed by
    	`ast/'.
    
    git-svn-id: file:///srv/svn/kernel1/branches/2.0@1710 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/ast/ast.mk
+++ src/ast/ast.mk
@@ -1,6 +1,7 @@
 ast_srcdir = $(srcdir)/ast
-include $(ast_srcdir)/ast-nodes.mk
-ast_nodes = $(foreach f,$(AST_NODES),ast/$(f))
+# Don't use ast_srcdir in the include, otherwise Automake can't resolve it.
+include $(srcdir)/ast/ast-nodes.mk
+ast_nodes = $(AST_NODES)
 
 EXTRA_DIST += ast/ast.yml
 BUILT_SOURCES += $(BUILT_SOURCES_ast)
@@ -19,7 +20,6 @@ gen_dir = $(top_srcdir)/dev
 ast_gen_deps = $(gen_dir)/ast.py $(gen_dir)/ast_params.py \
 	       $(gen_dir)/tools.py $(ast_srcdir)/ast.yml
 
-
 ## -------------------------- ##
 ## Using the AST generators.  ##
 ## -------------------------- ##
@@ -34,19 +34,19 @@ $(ast_srcdir)/%.stamp: $(gen_dir)/ast-%-gen $(ast_gen_deps)
 	@mv -f $@.tmp $@
 
 # ast-nodes.mk
-EXTRA_DIST += nodes-mk.stamp
+EXTRA_DIST += ast/nodes-mk.stamp
 $(ast_srcdir)/ast-nodes.mk: $(ast_srcdir)/nodes-mk.stamp
 
 # all.hh.
-EXTRA_DIST += all.stamp
+EXTRA_DIST += ast/all.stamp
 all.hh: $(ast_srcdir)/all.stamp
 
 # fwd.hh.
-EXTRA_DIST += fwd.stamp
+EXTRA_DIST += ast/fwd.stamp
 fwd.hh: $(ast_srcdir)/fwd.stamp
 
 # ignores.
-EXTRA_DIST += ignores ignores.stamp
+EXTRA_DIST += ast/ignores ast/ignores.stamp
 $(ast_srcdir)/ignores.stamp: $(gen_dir)/ast-ignores-gen $(ast_gen_deps)
 	@rm -rf $@
 	@rm -rf $@.tmp
@@ -59,7 +59,7 @@ $(ast_srcdir)/ignores.stamp: $(gen_dir)/ast-ignores-gen $(ast_gen_deps)
 ignores: $(ast_srcdir)/ignores.stamp
 
 # AST itself.
-EXTRA_DIST += nodes.stamp
+EXTRA_DIST += ast/nodes.stamp
 $(ast_nodes): $(ast_srcdir)/nodes.stamp
 
 # ast.dot
@@ -68,16 +68,16 @@ $(ast_srcdir)/ast.dot: $(gen_dir)/ast-graph-gen $(ast_gen_deps)
 	mv -f $@.tmp $@
 
 # DefaultVisitor
-EXTRA_DIST += default-visitor.stamp
-default-visitor.hh default-visitor.hxx: $(ast_srcdir)/default-visitor.stamp
+EXTRA_DIST += ast/default-visitor.stamp
+ast/default-visitor.hh default-visitor.hxx: $(ast_srcdir)/default-visitor.stamp
 
 # Visitor.
-EXTRA_DIST += visitor.stamp
-visitor.hh: $(ast_srcdir)/visitor.stamp
+EXTRA_DIST += ast/visitor.stamp
+ast/visitor.hh: $(ast_srcdir)/visitor.stamp
 
 # PrettyPrinter.
-EXTRA_DIST += pretty-printer.stamp
-pretty-printer.hh pretty-printer.hxx pretty-printer.cc: $(ast_srcdir)/pretty-printer.stamp
+EXTRA_DIST += ast/pretty-printer.stamp
+ast/pretty-printer.hh ast/pretty-printer.hxx ast/pretty-printer.cc: $(ast_srcdir)/pretty-printer.stamp
 
 
 MAINTAINERCLEANFILES += $(BUILT_SOURCES_ast)
