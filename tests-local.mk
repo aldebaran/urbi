c9935b8793c7cba90b8adc19445f9c78be0e3740
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-10-15 18:56:52 +0000

    Update test suite.
    	* tests-local.mk: now we must specify the directory of the failed
    	tests.
    
    git-svn-id: file:///srv/svn/kernel1/branches/2.0@1750 1e37f2df-8004-0410-ba96-adcf012cb36e

--- tests-local.mk
+++ tests-local.mk
@@ -2,8 +2,8 @@
 # are specific to a given project or even Svn branch.
 UCONSOLE_CHECK_FLAGS = -k2
 
-K2_TESTS = $(notdir $(wildcard $(srcdir)/k2/*.chk))
-TESTS += $(K2_TESTS)
+K2_TESTS = $(call tests_for,2.x)
+TESTS_DIRS += 2.x
 
 # Run k2 tests only.
 k2-check:
@@ -11,53 +11,53 @@ k2-check:
 
 # k2 tests that fail.
 XFAIL_TESTS +=					\
-k2-closure.chk
+2.x/closure.chk
 
 # k1 tests that currently don't pass.
 XFAIL_TESTS +=					\
-alias.chk					\
-arithmetics-failures.chk			\
-armor-unarmor.chk				\
-basic.chk					\
-begin-end-report.chk				\
-bin-copy.chk					\
-bin.chk						\
-bug76.chk					\
-class-definition.chk				\
-comparison.chk					\
-doublecolon.chk					\
-events.chk					\
-exec.chk					\
-freeze-time.chk					\
-function-declaration.chk			\
-function-delete.chk				\
-function-return.chk				\
-groups.chk					\
-hash-and-list.chk				\
-hierarchical-tags.chk				\
-illegal-operation-on-void.chk			\
-init-in-pipe-with-time.chk			\
-lazy-test-eval.chk				\
-load.chk					\
-long-identifiers.chk				\
-multiinheritance.chk				\
-nameresolution-array.chk			\
-nameresolution-dollars.chk			\
-nameresolution-objects.chk			\
-nameresolution.chk				\
-new-init.chk					\
-object-events.chk				\
-onleave.chk					\
-onleave2.chk					\
-parse-errors.chk				\
-simple-alias.chk				\
-size.chk					\
-speed-zero.chk					\
-stopif.chk					\
-string-escapes.chk				\
-switch.chk					\
-synchro-assignment.chk				\
-tag.chk						\
-undefall.chk					\
-variable-qualifiers.chk				\
-whenever-bad.chk
+checkfiles/alias.chk				\
+checkfiles/arithmetics-failures.chk		\
+checkfiles/armor-unarmor.chk			\
+checkfiles/basic.chk				\
+checkfiles/begin-end-report.chk			\
+checkfiles/bin-copy.chk				\
+checkfiles/bin.chk				\
+checkfiles/bug76.chk				\
+checkfiles/class-definition.chk			\
+checkfiles/comparison.chk			\
+checkfiles/doublecolon.chk			\
+checkfiles/events.chk				\
+checkfiles/exec.chk				\
+checkfiles/freeze-time.chk			\
+checkfiles/function-declaration.chk		\
+checkfiles/function-delete.chk			\
+checkfiles/function-return.chk			\
+checkfiles/groups.chk				\
+checkfiles/hash-and-list.chk			\
+checkfiles/hierarchical-tags.chk		\
+checkfiles/illegal-operation-on-void.chk	\
+checkfiles/init-in-pipe-with-time.chk		\
+checkfiles/lazy-test-eval.chk			\
+checkfiles/load.chk				\
+checkfiles/long-identifiers.chk			\
+checkfiles/multiinheritance.chk			\
+checkfiles/nameresolution-array.chk		\
+checkfiles/nameresolution-dollars.chk		\
+checkfiles/nameresolution-objects.chk		\
+checkfiles/nameresolution.chk			\
+checkfiles/new-init.chk				\
+checkfiles/object-events.chk			\
+checkfiles/onleave.chk				\
+checkfiles/onleave2.chk				\
+checkfiles/parse-errors.chk			\
+checkfiles/simple-alias.chk			\
+checkfiles/size.chk				\
+checkfiles/speed-zero.chk			\
+checkfiles/stopif.chk				\
+checkfiles/string-escapes.chk			\
+checkfiles/switch.chk				\
+checkfiles/synchro-assignment.chk		\
+checkfiles/tag.chk				\
+checkfiles/undefall.chk				\
+checkfiles/variable-qualifiers.chk		\
+checkfiles/whenever-bad.chk
