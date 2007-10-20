90f04f6548aa19d2bec321d9ddd5417cf6501539
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-10-20 05:11:52 +0000

    We fail no 1.x tests.
    	* tests-local.mk: Update.
    
    git-svn-id: file:///srv/svn/kernel1/trunk@1802 1e37f2df-8004-0410-ba96-adcf012cb36e

--- tests-local.mk
+++ tests-local.mk
@@ -1,5 +1,4 @@
 # This file is loaded by tests/Makefile.am.  It contains things that
 # are specific to a given project or even Svn branch.
 
-XFAIL_TESTS +=					\
-1.x/function-declaration.chk
+# XFAIL_TESTS +=
