f68e199e4852e1d79bebed1cacef92e97c5ebe21
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-10-23 09:35:56 +0000

    Work again with src=build.
    	* src/userver-package-info.cc: Help it to find src/config.h, not
    	libport/config.h.
    
    git-svn-id: file:///srv/svn/kernel1/trunk@1815 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/userver-package-info.cc
+++ src/userver-package-info.cc
@@ -1,4 +1,14 @@
 #include "kernel/userver.hh"
+
+// The following file includes "config.h", unfortunately, it's going
+// to catch libport/config.h because when one includes "dir/foo.h"
+// which includes "bar.h", then it's "dir/bar.h" that takes precedence
+// over the local "bar.h" :(
+// 
+// So we force the loading of src/config.h.  This is fragile, but I
+// don't have time for something more intelligent now -- Akim.
+#include "src/config.h"
+
 #include "libport/package-info.hcc"
 
 const libport::PackageInfo&
