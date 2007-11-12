8bba2ade03ae5b7a24c136cbb3303deb73f15269
demaille <demaille@b8db33d8-8104-0410-b827-8403f563b8eb>
2007-11-12 16:07:38 +0000

    Help the test suite.
    	* tests/Makefile.am: Define URBISERVER and pass it to the
    	TESTS_ENVIRONMENT.
    	* tests/tests.cc: Formatting changes.
    	Use lexical_cast, it is much safer.
    
    git-svn-id: file:///srv/svn/liburbi-cpp/trunk@574 b8db33d8-8104-0410-b827-8403f563b8eb

--- sdk-remote/tests/tests.cc
+++ sdk-remote/tests/tests.cc
@@ -1,63 +1,69 @@
+#include <boost/lexical_cast.hpp>
+
 #include <urbi/uclient.hh>
 #include <urbi/usyncclient.hh>
 
 #include "tests.hh"
 
 libport::Semaphore dumpSem;
+
 urbi::UCallbackAction
-dump(const urbi::UMessage & msg)
+dump(const urbi::UMessage& msg)
 {
-  
   unsigned char type;
   switch (msg.type)
   {
-  case urbi::MESSAGE_DATA:
-    type =  'D';
-    break;
-    
-  case urbi::MESSAGE_ERROR:
-    type =  'E';
-    break;
-  case urbi::MESSAGE_SYSTEM:
-    type = 'S';
-    break;
-  default:
-    type = '?';
-  }
-  std::cout << type << ' ';
-   switch (msg.type)
-    {
     case urbi::MESSAGE_DATA:
-		std::cout << msg.tag << ' ' << *msg.value << std::endl;
+      type =  'D';
+      break;
+
+    case urbi::MESSAGE_ERROR:
+      type =  'E';
+      break;
+    case urbi::MESSAGE_SYSTEM:
+      type = 'S';
+      break;
+    default:
+      type = '?';
+  }
+
+  std::cout << type << ' ';
+  switch (msg.type)
+  {
+    case urbi::MESSAGE_DATA:
+      std::cout << msg.tag << ' ' << *msg.value << std::endl;
       break;
 
     case urbi::MESSAGE_ERROR:
     case urbi::MESSAGE_SYSTEM:
-		std::cout  << msg.tag << ' ' << msg.message << std::endl;
+      std::cout  << msg.tag << ' ' << msg.message << std::endl;
       break;
-    }
+  }
   dumpSem++;
   return urbi::URBI_CONTINUE;
 }
 
 
-urbi::UCallbackAction removeOnZero(const urbi::UMessage & msg)
+urbi::UCallbackAction
+removeOnZero(const urbi::UMessage& msg)
 {
   dump(msg);
-  if (msg.type == urbi::MESSAGE_DATA && msg.value->type == urbi::DATA_DOUBLE
-    && msg.value->val == 0)
+  if (msg.type == urbi::MESSAGE_DATA
+      && msg.value->type == urbi::DATA_DOUBLE
+      && msg.value->val == 0)
     return urbi::URBI_REMOVE;
-    return urbi::URBI_CONTINUE;
+  return urbi::URBI_CONTINUE;
 }
 
 
-int main(int argc, const char * argv[])
+int
+main(int argc, const char* argv[])
 {
   if (argc != 4)
     exit(1);
-  const char * host = argv[1];
-  int port = strtol(argv[2],0,0);
-  const char * call = argv[3];
+  const char* host = argv[1];
+  int port = boost::lexical_cast<int>(argv[2]);
+  const char* call = argv[3];
   urbi::UClient client(host, port);
   urbi::USyncClient syncClient(host, port);
   dispatch(call, client, syncClient);
