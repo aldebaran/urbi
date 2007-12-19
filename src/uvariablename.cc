e3f373463b2b04a051b80dae7da5612fa09a12e5
deslandes <deslandes@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-12-19 16:02:28 +0000

    Remove warning at alias use.
    	* src/uvariablename.cc: Here.
    
    git-svn-id: file:///srv/svn/kernel1/trunk@2140 1e37f2df-8004-0410-ba96-adcf012cb36e

--- src/uvariablename.cc
+++ src/uvariablename.cc
@@ -502,9 +502,6 @@ UVariableName::buildFullname (UCommand* command,
     std::string resolved = resolve_aliases (::urbiserver->aliastab, n);
     if (resolved != n)
     {
-      send_error(connection, command,
-		 ("'" + n + "' is an alias for '" + resolved + "'. "
-		   "Using aliases is deprecated.").c_str());
       name = resolved;
       if (name.find('.') == std::string::npos)
 	name = device->str () + "." + suffix(name);
