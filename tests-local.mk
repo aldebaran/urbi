5d98efc5c617669d92de8f7f7e5071934a9ebc31
demaille <demaille@1e37f2df-8004-0410-ba96-adcf012cb36e>
2007-12-21 12:38:34 +0000

    Update tests.
    	* tests-local.mk (TESTS_DIRS): Define it.
    
    git-svn-id: file:///srv/svn/kernel1/trunk@2156 1e37f2df-8004-0410-ba96-adcf012cb36e

--- tests-local.mk
+++ tests-local.mk
@@ -1,7 +1,6 @@
 # This file is loaded by tests/Makefile.am.  It contains things that
 # are specific to a given project or even Svn branch.
 
-TESTS_DIRS += 1.x 1.5
+TESTS_DIRS = 1.x 1.5 # uob
 URBI_SERVER = $(abs_top_builddir)/src/urbi-console
-
 XFAIL_TESTS +=
