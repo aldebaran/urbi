61dfd44d68e7fb6e5bd32693d8f5734db72c0f17
Samuel Tardieu <sam@rfc1149.net>
2008-01-21 14:23:03 +0100

    Include libcoroutine in the build.
    	* src/runner/libcoroutine/libcoroutine.mk: Add libcoroutine files
    	to libkernel.la.
    	* src/Makefile.am: Include previous file.

---
+++ src/runner/libcoroutine/libcoroutine.mk
@@ -0,0 +1,8 @@
+dist_libkernel_la_SOURCES +=			\
+runner/libcoroutine/Base.h			\
+runner/libcoroutine/Common.h			\
+runner/libcoroutine/Coro.c			\
+runner/libcoroutine/Coro.h			\
+runner/libcoroutine/PortableUContext386.h	\
+runner/libcoroutine/PortableUContext.c		\
+runner/libcoroutine/PortableUContext.h
