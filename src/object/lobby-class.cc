b3f93633fc43750ec5ae3d2e422d031f78d10cdb
Matthieu Nottale <nottale@gostai.com>
2008-02-11 21:04:04 +0100

    Implement send in lobby.
    	* src/object/loby-class.cc (lobby_class_send): Implement and bind.

--- src/object/lobby-class.cc
+++ src/object/lobby-class.cc
@@ -6,6 +6,8 @@
 #include "object/lobby-class.hh"
 #include "object/object.hh"
 
+#include "kernel/uconnection.hh"
+
 #include "runner/runner.hh"
 
 namespace object
@@ -24,12 +26,29 @@ namespace object
     return r.lobby_get ();
   }
 
+  static rObject
+  lobby_class_send(runner::Runner&, objects_type args)
+  {
+    CHECK_ARG_COUNT_RANGE (2, 3);
+    // Second argument is the tag name.
+    std::string tag;
+    if (args.size() == 3)
+    {
+      FETCH_ARG(2, String);
+      tag = arg2->value_get().name_get();
+    }
+    FETCH_ARG(0, Lobby);
+    arg0->value_get().connection.send (args[1], tag.c_str(), "");
+    return void_class;
+  }
+
   void
   lobby_class_initialize ()
   {
 #define DECLARE(Name)				\
     DECLARE_PRIMITIVE(lobby, Name, Name)
     DECLARE (self);
+    DECLARE (send);
 #undef DECLARE
   }
 
