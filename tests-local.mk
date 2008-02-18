9f56f1bc6deb2ca35dd42e91ba93c0b75d98dde2
Akim Demaille <demaille@gostai.com>
2008-02-18 18:04:25 +0100

    Protect all the changes of valp.
    My current approach was too naive: we need to be able to
    change the start conditions to avoid catching ( and so
    forth in strings or comments.  My previous patch, which
    does not run the actions at all, does not change start
    conditions, so it fails to protect strings etc.
    
    Now that I plan to run the actions for real, we do not
    want to grow valp when its value is not used.  So use
    a macro for all the assignments to valp.
    
    	* src/parser/utoken.l (SET_VAL, GROW_VAL, APPEND_VAL, STEP, ERROR):
    	New.
    	Use them.
    	(LINES): Take the number of lines as arg.
    	Also, report the guilty escapes.
    	* tests-local.mk (TFAIL_TESTS): Add symbols.chk.

--- tests-local.mk
+++ tests-local.mk
@@ -14,7 +14,8 @@ k2-check:
 ENABLE_HARD_ERRORS = false
 
 # k2 tests that fail currently but need to be fixed soon.
-TFAIL_TESTS +=	\
+TFAIL_TESTS +=					\
+2.x/symbols.chk					\
 2.x/large-string.chk
 
 # k2 tests that fail.
