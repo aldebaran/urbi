add32af8099d5b56dcce8868a4bcf83608ba061e
Akim Demaille <demaille@gostai.com>
2008-02-19 14:13:09 +0100

    Space changes.
    	* src/parser/utoken.hh: here.

--- src/parser/utoken.hh
+++ src/parser/utoken.hh
@@ -5,18 +5,18 @@
    regular scanner that feeds its parser, and "prescanner" as a
    pre-scanner that tries to find a complete sentence to feed the
    parser with.
-   
+
    In the prescanner mode, the scanner looks for the complete command,
    i.e., up to the next terminator (";" or ","), taking care of
    tracking braces, and, of course, occurrences of the terminators in
    the strings or comments etc.
-   
+
    Flex is not meant to embed two sets of actions in a single scanner,
    so we have to play tricks.  yylex has three optional arguments:
    valp, locp, and up.  In prescanner mode, up = 0, and we are valp
    (which is a pointer) to store a size_t: the length of the full
    sentence.
-   
+
    There is a number of macros to implement the prescanner mode, and
    to skip the rest of the action in that case: they are all prefixed
    with PRE_*. */
