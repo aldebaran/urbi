ca97e947810313f301fabfb75bc6f5608e0c5990
Matthieu Nottale <nottale@gostai.com>
2008-02-22 15:25:09 +0100

    Add apply to Delegate type.
    	* src/object/delegate-class.cc (delegate_class_apply): New.

--- src/object/delegate-class.cc
+++ src/object/delegate-class.cc
@@ -8,6 +8,7 @@
 #include "object/primitives.hh"
 
 #include "runner/runner.hh"
+
 namespace object
 {
   rObject delegate_class;
@@ -16,11 +17,20 @@ namespace object
   | Primitive primitives.  |
   `-----------------------*/
 
+  static rObject
+  delegate_class_apply (runner::Runner& r, objects_type args)
+  {
+    CHECK_ARG_COUNT (2);
+    FETCH_ARG (1, List);
+    return r.apply (args[0], arg1);
+  }
+
   void
   delegate_class_initialize ()
   {
-#define DECLARE(Name)                           \
-        DECLARE_PRIMITIVE(delegate, Name)
+#define DECLARE(Name)				\
+    DECLARE_PRIMITIVE(delegate, Name)
+    DECLARE (apply);
 #undef DECLARE
   }
 
