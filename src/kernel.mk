13d73f0fa4e31ad1b5cf266fb6609ff7de97c09e
Akim Demaille <demaille@gostai.com>
2008-02-24 12:16:01 +0100

    Use Automake comments.
    When they don't make sense in the resulting Makefile.
    
    	* src/kernel.mk: here.

--- src/kernel.mk
+++ src/kernel.mk
@@ -1,30 +1,30 @@
 include $(top_srcdir)/build-aux/init.mk
-# Get $(libport_la).
-# lib_libport = ../lib/libport
-# include $(top_srcdir)/lib/libport/libport-sources.mk
+## Get $(libport_la).
+## lib_libport = ../lib/libport
+## include $(top_srcdir)/lib/libport/libport-sources.mk
 
-# Find the kernel private headers.  location.hh etc. are generated.
+## Find the kernel private headers.  location.hh etc. are generated.
 AM_CPPFLAGS += -I$(srcdir) -I.
 
-# Find the uobject headers
+## Find the uobject headers
 AM_CPPFLAGS += -I$(srcdir)/uobject -Iuobject
 
-# Find the kernel, libport, etc. headers.
+## Find the kernel, libport, etc. headers.
 AM_CPPFLAGS += -I$(top_srcdir)/include -I$(top_builddir)/include
 
-# lib/network
+## lib/network
 AM_CPPFLAGS += -I$(top_srcdir)/lib
 
-# Find sdk/config.h.
+## Find sdk/config.h.
 AM_CPPFLAGS += -I$(top_builddir)
 
-# Find uobject headers.
+## Find uobject headers.
 AM_CPPFLAGS += -I$(srcdir)/uobject
 
 AM_CXXFLAGS += $(WARNING_CXXFLAGS) $(PTHREAD_CFLAGS)
 
-# Add boost libraries.
+## Add boost libraries.
 AM_CPPFLAGS += $(BOOST_CPPFLAGS)
 AM_LDFLAGS  += $(BOOST_THREAD_LDFLAGS)
-# There is no AM_LIBADD.
+## There is no AM_LIBADD.
 LIBS        += $(BOOST_THREAD_LIBS)
