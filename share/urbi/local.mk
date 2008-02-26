27dde7faeb8e6eeaad89cb66314d6985ae53c710
Akim Demaille <demaille@gostai.com>
2008-02-26 16:31:30 +0100

    urbi/singleton.u
    	* share/urbi/singleton.u: New.
    	* share/urbi/urbi.u: Load it.
    	(nil, void): Use it.
    	* share/urbi/local.mk: New.
    	* Makefile.am: Use it.

---
+++ share/urbi/local.mk
@@ -0,0 +1,8 @@
+dist_pkgdata_DATA =				\
+share/urbi/float.u				\
+share/urbi/group.u				\
+share/urbi/k1.u					\
+share/urbi/list.u				\
+share/urbi/singleton.u				\
+share/urbi/string.u				\
+share/urbi/urbi.u
