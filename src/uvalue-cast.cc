51eee93ca90e0d54d6b761275b40f4b26ff5919b
Matthieu Nottale <nottale@gostai.com>
2008-03-04 14:39:19 +0100

    Implement casting between UValue and k2 binary representation.
    	* src/uvalue-cast.cc(object_cast)(uvalue_cast): Handle DATA_BINARY
    	  and kind_object cases.

--- src/uvalue-cast.cc
+++ src/uvalue-cast.cc
@@ -16,7 +16,6 @@ urbi::UValue uvalue_cast(object::rObject o)
   case object::Object::kind_delegate:
   case object::Object::kind_global:
   case object::Object::kind_lobby:
-  case object::Object::kind_object:
   case object::Object::kind_primitive:
   case object::Object::kind_scope:
   case object::Object::kind_tag:
@@ -24,7 +23,27 @@ urbi::UValue uvalue_cast(object::rObject o)
     throw object::WrongArgumentType
       (object::Object::kind_float, o->kind_get(), "cast");
     break;
-
+  case object::Object::kind_object:
+    if (!is_a(o, object::global_class->slot_get(SYMBOL(Binary))))
+      boost::throw_exception (object::WrongArgumentType
+       (object::Object::kind_object, o->kind_get(), "bin-cast"));
+      {
+	const std::string& data =
+	  o->slot_get(SYMBOL(data))->value<object::String>().name_get();
+	const std::string& keywords =
+	  o->slot_get(SYMBOL(keywords))->value<object::String>().name_get();
+	std::list<urbi::BinaryData> l;
+	l.push_back(urbi::BinaryData(const_cast<char*>(data.c_str()),
+			      data.size()));
+	std::list<urbi::BinaryData>::iterator i = l.begin();
+	res.type = urbi::DATA_BINARY;
+	res.binary = new urbi::UBinary();
+	res.binary->parse(
+		   (boost::lexical_cast<std::string>(data.size()) +
+		   " " + keywords + '\n').c_str(), 0, l, i);
+	return res;
+      }
+    break;
 #define HANDLE_TYPE(k, t) \
   case object::Object::k:  \
     res = o.cast<object::t>()->value_get(); \
@@ -62,10 +81,25 @@ object::rObject object_cast(const urbi::UValue& v)
       foreach (urbi::UValue *cv, v.list->array)
 	res.cast<object::List>()->value_get().push_back(object_cast(*cv));
       break;
+    case urbi::DATA_BINARY:
+      {
+	res = object::Object::fresh();
+	res->proto_add(object::object_class->slot_get(SYMBOL(Binary)));
+	std::string msg = v.binary->getMessage();
+	// Trim it.
+	if (!msg.empty() && msg[0] == ' ' )
+	  msg = msg.substr(1, msg.npos);
+	res->slot_set(SYMBOL(keywords), object::String::fresh
+		    (libport::Symbol(msg)));
+	res->slot_set(SYMBOL(data), object::String::fresh(libport::Symbol(
+		std::string(static_cast<char*>(v.binary->common.data),
+	      v.binary->common.size))));
+      }
+      break;
     default:
       throw
 	object::WrongArgumentType(object::Object::kind_float,
-				  object::Object::kind_float, "cast");
+				  object::Object::kind_float, "backcast");
       break;
   }
   return res;
