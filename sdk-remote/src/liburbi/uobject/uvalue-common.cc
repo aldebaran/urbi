35f87d2727d95484c2f163290fbda8642ed7f795
Guillaume Deslandes <deslandes@gostai.com>
2008-03-12 16:46:55 +0100

    Replace paborts by UNKNOWN_FORMAT returns.
    	* src/liburbi/uobject.uvalue-common.cc: Here.
    	These aborts are dangerous. Since UBinary use
    	UImage and Usound in an union, it is not possible
    	to add clean constructors. And as usercan use
    	these classes, abort is way to dangerous.

--- sdk-remote/src/liburbi/uobject/uvalue-common.cc
+++ sdk-remote/src/liburbi/uobject/uvalue-common.cc
@@ -316,7 +316,10 @@ namespace urbi
 	return "unknown format";
     }
     // To pacify "warning: control reaches end of non-void function".
-    pabort(soundFormat);
+    // FIXME: This should not abort. UImage should be initialized with IMAGE_UKNOWN.
+    //        This is not done because data is stored in an union in UBinary and
+    //        union members cannot have constructors.
+    return "unknown format";
   }
 
   /*---------.
@@ -339,7 +342,11 @@ namespace urbi
 	return "unknown format";
     }
     // To pacify "warning: control reaches end of non-void function".
-    pabort(imageFormat);
+    // pabort(imageFormat);
+    // FIXME: This should not abort. UImage should be initialized with IMAGE_UKNOWN.
+    //        This is not done because data is stored in an union in UBinary and
+    //        union members cannot have constructors.
+    return "unknown format";
   }
 
   /*----------.
