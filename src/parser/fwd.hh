665f4c7bbd562e7412c24a55a16335c32657bb63
Akim Demaille <demaille@gostai.com>
2008-04-07 11:43:10 +0200

    Provide parsing namepsace-level functions.
    	* src/parser/uparser.hh, src/parser/uparser.cc (parser::parse): New.

--- src/parser/fwd.hh
+++ src/parser/fwd.hh
@@ -1,11 +1,13 @@
 /**
  ** \file parser/fwd.hh
- ** \brief Forward declarations of all PARSER classes
+ ** \brief Forward declarations of all parser classes
  ** (needed by the visitors).
  */
 
 #ifndef PARSER_FWD_HH
 # define PARSER_FWD_HH
+
+class UParser;
 
 // parser/flex-lexer.hh.
 class FlexLexer;
