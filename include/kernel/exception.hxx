0f31a57068d1382efb498703005336c3ad01bab8
Akim Demaille <demaille@gostai.com>
2008-04-22 18:47:05 +0200

    Remove spurious ;.

--- include/kernel/exception.hxx
+++ include/kernel/exception.hxx
@@ -7,13 +7,13 @@ namespace kernel
   inline
   exception::~exception ()
   {
-  };
+  }
 
   inline std::string
   exception::what () const throw()
   {
     return "unknown exception";
-  };
+  }
 
   inline
   void rethrow (const exception_ptr& e)
