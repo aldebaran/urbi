77a8d9c9bba7d28a2dcfa60c26820e8f148d8aea
Samuel Tardieu <sam@rfc1149.net>
2008-05-23 12:53:42 +0200

    Move complex or virtual subprograms from hxx to cc.
    	* src/runner/interpreter.hxx (Interpreter, ~Interpreter, init):
    	Move from here...
    	* src/runner/interpreter.cc: ...to here.

--- src/runner/interpreter.hxx
+++ src/runner/interpreter.hxx
@@ -15,66 +15,6 @@ namespace runner
 {
 
   inline
-  Interpreter::Interpreter (rLobby lobby,
-			    rObject locals,
-			    scheduler::Scheduler& sched,
-			    const ast::Ast* ast,
-			    bool free_ast_after_use)
-    : Interpreter::super_type(),
-      Runner(lobby, sched),
-      ast_(ast),
-      free_ast_after_use_(free_ast_after_use),
-      code_(0),
-      current_(0),
-      locals_(locals)
-  {
-    init();
-  }
-
-  inline
-  Interpreter::Interpreter(const Interpreter& model, rObject code)
-    : Interpreter::super_type(),
-      Runner(model),
-      ast_(0),
-      free_ast_after_use_(false),
-      code_(code),
-      current_(0),
-      locals_(model.locals_)
-  {
-    init();
-  }
-
-  inline
-  Interpreter::Interpreter(const Interpreter& model,
-			   const ast::Ast* ast,
-			   bool free_ast_after_use)
-    : Interpreter::super_type (),
-      Runner(model),
-      ast_(ast),
-      free_ast_after_use_(free_ast_after_use),
-      code_(0),
-      current_(0),
-      locals_(model.locals_)
-  {
-  }
-
-  inline
-  Interpreter::~Interpreter ()
-  {
-  }
-
-  inline
-  void Interpreter::init()
-  {
-    if (!locals_)
-      locals_ = object::Object::make_method_scope(lobby_);
-    // If the lobby has a slot connectionTag, push it
-    rObject connection_tag = lobby_->slot_locate(SYMBOL(connectionTag));
-    if (connection_tag)
-      push_tag(extract_tag(connection_tag->slot_get(SYMBOL(connectionTag))));
-  }
-
-  inline
   const object::rObject&
   Interpreter::locals_get () const
   {
