12a666f3cd6c2488923cdaa5d824f25e9ec203c1
Akim Demaille <demaille@gostai.com>
2008-05-26 15:24:18 +0200

    Fix installation of *.u files.
    	* share/urbi/local.mk (urbidir): New, so that we have file
    	installed as urbi/*.u.
    	Use wildcard instead of maintaining by hand a list which is never
    	up to date.

--- share/urbi/local.mk
+++ share/urbi/local.mk
@@ -1,12 +1,3 @@
-dist_pkgdata_DATA =				\
-share/urbi/binary.u				\
-share/urbi/channel.u				\
-share/urbi/event.u				\
-share/urbi/float.u				\
-share/urbi/group.u				\
-share/urbi/k1.u					\
-share/urbi/list.u				\
-share/urbi/semaphore.u				\
-share/urbi/singleton.u				\
-share/urbi/string.u				\
-share/urbi/urbi.u
+urbidir = pkgdatadir/urbi
+# Yes, this is not by the book, but it is so cooler.
+dist_urbi_DATA := $(wildcard share/urbi/*.u)
