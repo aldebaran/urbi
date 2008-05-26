d36626557d2a984e7344907b4dc20e9af1639a92
Samuel Tardieu <sam@rfc1149.net>
2008-05-26 11:05:44 +0200

    Use unchecked version of list primitives.
    We already do the bounds checking, so use the unchecked access version.
    
    	* src/object/list-class.cc (list_class_nth): Use "[]" instead of
    	"at".

--- src/object/list-class.cc
+++ src/object/list-class.cc
@@ -191,7 +191,7 @@ namespace object
     int index = ufloat_to_int(arg1->value_get(), "nth");
     if (index < 0 || index >= static_cast<int>(arg0->value_get().size()))
       throw PrimitiveError("nth", "invalid index");
-    return arg0->value_get().at(index);
+    return arg0->value_get()[index];
   }
 
   /*-------------------------.
