aa9ee04945b16491af4dbd8948860ccd7f3032cc
Akim Demaille <demaille@gostai.com>
2008-06-05 13:09:50 +0200

    Work properly when SYMBOL is used on unknown symbols.
    	* src/object/symbols.cc (SymbolMap::operator[]): New.
    	(symbol_get): Use it.

--- src/object/symbols.cc
+++ src/object/symbols.cc
@@ -3,7 +3,7 @@
  ** \brief Frequently used symbol names.
  */
 
-#include "object/symbols.hh"
+#include <object/symbols.hh>
 
 #if defined SYMBOLS_PRECOMPILED
 
@@ -21,15 +21,14 @@ namespace object
 
 # include <map>
 # include <string>
-# include "object/precompiled-symbols.hh"
+# include <object/precompiled-symbols.hh>
 
 namespace object
 {
 
-  struct SymbolMap
+  class SymbolMap
   {
-    typedef std::map<const std::string, libport::Symbol> map_type;
-    map_type map_;
+  public:
     SymbolMap()
       : map_()
     {
@@ -38,14 +37,36 @@ namespace object
       SYMBOLS_APPLY(SYMBOL_DEFINE);
 # undef SYMBOL_DEFINE
     }
+
+    /// Return the symbol associated to \a s if there is one.
+    /// Otherwise create a symbol equal to \a s.
+    /// \warning: Special characters (SP, LT, etc.) are *not*
+    /// handled in this case.
+    libport::Symbol
+    operator[](const std::string& s)
+    {
+      // See ``Efficient STL''.
+      map_type::iterator i = map_.lower_bound(s);
+      if (i != map_.end() && !map_.key_comp()(s, i->first))
+        return i->second;
+      else
+      {
+        libport::Symbol res(s);
+        map_.insert(map_type::value_type(s, res));
+        return res;
+      }
+    }
+
+  private:
+    typedef std::map<const std::string, libport::Symbol> map_type;
+    map_type map_;
   };
 
-  // FIXME: More const.
   libport::Symbol
   symbol_get(const std::string& s)
   {
     static SymbolMap map;
-    return map.map_[s];
+    return map[s];
   }
 
 } // namespace object
