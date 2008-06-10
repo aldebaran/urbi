7bbd853c94129766221b5d886aa058e02e0a0f8b
mefyl <hocquet@gostai.com>
2008-06-10 10:06:36 +0200

    Use a separate timer for the binding process.
        * src/binder/bind.cc (bind): Bench binding operation separately..

--- src/binder/bind.cc
+++ src/binder/bind.cc
@@ -1,13 +1,18 @@
 #include <binder/bind.hh>
 #include <binder/binder.hh>
 
+#include <server-timer.hh>
+
 namespace binder
 {
   ast::rAst
   bind(ast::rConstAst a)
   {
+    TIMER_PUSH("bind");
     Binder bind;
     bind(a);
-    return bind.result_get();
+    ast::rAst ast = bind.result_get();
+    TIMER_POP("bind");
+    return ast;
   }
 } // namespace binder
