1287c1eb9fe1a5d07d44c5f3bb688fd190ae167d
Akim Demaille <demaille@gostai.com>
2008-06-13 16:20:18 +0200

    Add missing file.

---
+++ src/kernel/local.mk
@@ -0,0 +1,23 @@
+dist_libkernel_la_SOURCES +=			\
+kernel/debug.cc					\
+kernel/debug.hh					\
+kernel/kernconf.cc				\
+kernel/server-timer.hh				\
+kernel/server-timer.cc				\
+kernel/ubanner.hh				\
+kernel/ubanner.cc				\
+kernel/ucomplaints.cc				\
+kernel/uconnection.cc				\
+kernel/ughostconnection.hh			\
+kernel/ughostconnection.cc			\
+kernel/utypes.cc				\
+kernel/uobject.cc				\
+kernel/uobject.hh				\
+kernel/uqueue.hh				\
+kernel/uqueue.hxx				\
+kernel/uqueue.cc				\
+kernel/userver.cc				\
+kernel/usystem.cc				\
+kernel/uvalue-cast.cc				\
+kernel/uvalue-cast.hh
+
