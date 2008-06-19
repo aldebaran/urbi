a5dd9a97012398c7b2aa6c6195c56a229513a8a1
Akim Demaille <demaille@gostai.com>
2008-06-19 11:06:37 +0200

    Fix compilation issue.
    I don't know which compiler is right, but mine does not like the
    following.  CxxObject includes two inner classes Binder and
    TypeInitializer.  Binder's ctor is private, but CxxObject is declared
    to be a friend, yet it is not CxxObject which invokes the ctor, but
    TypeInitializer::make_class.
    
    	* src/object/cxx-object.hh (Binder): Declare
    	TypeInitializer::make_class as friend, not Binder.
    	Move people around so that declarations precede uses.

--- src/object/cxx-object.hh
+++ src/object/cxx-object.hh
@@ -7,83 +7,88 @@
 
 namespace object
 {
-  /// Base class for Urbi bound C++ classes
+  /// Base class for Urbi bound C++ classes.
   class CxxObject: public Object
   {
+  public:
+
+    /// Build a CxxObject.
+    CxxObject();
+
+    /// Bind all registered objects.
+    /** This function should be called once to bind C++ objects.
+     *  \param global Where to store the new classes.
+     */
+    static void initialize(rObject global);
+    static void create();
+
+    /// Register a C++ class to be bound on the urbi side.
+    /** \param T      The class to bind.
+     *  \param name   Name of the class on the Urbi side.
+     *  \param tgt    Where to store the result.
+     */
+    template<typename T>
+    static bool add(const std::string& name, rObject& tgt);
+
+    virtual std::string type_name_get() const = 0;
+
+  private:
+
+    class Initializer
+    {
     public:
+      Initializer(rObject& tgt);
+      virtual ~Initializer();
+      virtual rObject make_class() = 0;
+      virtual void create() = 0;
+      virtual libport::Symbol name() = 0;
+    protected:
+      rObject& res_;
+    };
 
-      /// Build a CxxObject
-      CxxObject();
+    template <typename T>
+    class TypeInitializer: public Initializer
+    {
+    public:
+      TypeInitializer(const std::string& name, rObject& tgt);
+      virtual rObject make_class();
+      virtual void create();
+      virtual libport::Symbol name();
+    private:
+      libport::Symbol name_;
+    };
 
-      /// Bind all registered objects
-      /** This function should be called once to bind C++ objects.
-       *  \param global Where to store the new classes.
-       */
-      static void initialize(rObject global);
-      static void create();
+    typedef std::vector<Initializer*> initializers_type;
+    static initializers_type& initializers_get();
 
-      /// Register a C++ class to be bound on the urbi side.
-      /** \param T      The class to bind.
-       *  \param name   Name of the class on the Urbi side.
-       *  \param tgt    Where to store the result.
-       */
-      template<typename T>
-      static bool add(const std::string& name, rObject& tgt);
-
-      virtual std::string type_name_get() const = 0;
-
-      /// Functor to bind methods on the urbi side.
-      /** An instance of this class is given to the static initialize
-       *  method of the bound classes. It can then be used to bind
-       *  method and/or attributes on the Urbi side.
-       */
-      template <typename T>
-      class Binder
-      {
-        public:
-          /// Bind \a method with \a name
-          template <typename M>
-          void operator()(const libport::Symbol& name, M method);
-
-        private:
-          friend class CxxObject;
-          Binder(rObject tgt);
-          rObject tgt_;
-      };
+  public:
+    /// Functor to bind methods on the urbi side.
+    /** An instance of this class is given to the static initialize
+     *  method of the bound classes. It can then be used to bind
+     *  method and/or attributes on the Urbi side.
+     */
+    template <typename T>
+    class Binder
+    {
+    public:
+      /// Bind \a method with \a name
+      template <typename M>
+      void operator()(const libport::Symbol& name, M method);
 
     private:
+      Binder(rObject tgt);
+      // This method is allowed to construct a Binder.
+      template <typename U>
+      friend rObject TypeInitializer<U>::make_class();
+      rObject tgt_;
+    };
 
-      class Initializer
-      {
-        public:
-          Initializer(rObject& tgt);
-          virtual ~Initializer();
-          virtual rObject make_class() = 0;
-          virtual void create() = 0;
-          virtual libport::Symbol name() = 0;
-        protected:
-          rObject& res_;
-      };
-
-      template <typename T>
-      class TypeInitializer: public Initializer
-      {
-        public:
-          TypeInitializer(const std::string& name, rObject& tgt);
-          virtual rObject make_class();
-          virtual void create();
-          virtual libport::Symbol name();
-        private:
-          libport::Symbol name_;
-      };
-
-      typedef std::vector<Initializer*> initializers_type;
-      static initializers_type& initializers_get();
   };
 
   /// Raise an exception if this is not of type \a T
   template<typename T>
   void type_check(rObject o, const std::string& fun);
+
 
 }
 
