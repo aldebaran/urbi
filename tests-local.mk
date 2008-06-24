b97d10e2b0770a07cd156c8ee40c2ed2d34e72d8
Samuel Tardieu <sam@rfc1149.net>
2008-06-24 14:35:06 +0200

    Update tests dependency and tests list.
    	* tests: Update.
    	* tests-local.mk: Refresh.

--- tests-local.mk
+++ tests-local.mk
@@ -26,43 +26,45 @@ TFAIL_TESTS +=					\
 2.x/unique-variable.chk
 
 # k1 tests that currently don't pass, but we should.
-# Not really sorted yet, please help.
-XFAIL_TESTS +=					\
-1.x/begin-end-report.chk			\
-1.x/channels-tags.chk				\
-1.x/connection-delete.chk			\
-1.x/events.chk					\
-1.x/events-emit-then-return-in-function.chk	\
-1.x/every.chk					\
-1.x/every-emit.chk				\
-1.x/freeze-time.chk				\
-1.x/function-delete.chk                         \
-1.x/hash-and-list.chk				\
-1.x/hierarchical-tags.chk			\
-1.x/init-in-pipe-with-time.chk			\
-1.x/lazy-test-eval.chk				\
-1.x/loadwav.chk					\
-1.x/modifier/accel-adaptive.chk			\
-1.x/modifier/accel.chk				\
-1.x/modifier/smooth-adaptive.chk		\
-1.x/modifier/smooth.chk				\
-1.x/modifier/speed-adaptive.chk			\
-1.x/modifier/speed.chk				\
-1.x/modifier/time-adaptive.chk			\
-1.x/modifier/time.chk				\
-1.x/modifier/syntax.chk				\
-1.x/nameresolution-array.chk			\
-1.x/nameresolution-dollar-computed.chk		\
-1.x/nameresolution-dollars.chk			\
-1.x/nameresolution-objects.chk			\
-1.x/object-events.chk				\
-1.x/sinus.chk					\
-1.x/speed-zero.chk				\
-1.x/synchro-assignment.chk			\
-1.x/tag.chk					\
-1.x/tag-reduce.chk                              \
-1.x/variable-normalize.chk			\
-1.x/variable-qualifiers.chk
+# In fact, the above list has been removed to gain some time
+# when running tests. The following tests should be examined
+# and reintroduced in 1.x as needed.
+TO_CHECK_TESTS =				\
+1.-/begin-end-report.chk			\
+1.-/channels-tags.chk				\
+1.-/connection-delete.chk			\
+1.-/events.chk					\
+1.-/events-emit-then-return-in-function.chk	\
+1.-/every.chk					\
+1.-/every-emit.chk				\
+1.-/freeze-time.chk				\
+1.-/function-delete.chk                         \
+1.-/hash-and-list.chk				\
+1.-/hierarchical-tags.chk			\
+1.-/init-in-pipe-with-time.chk			\
+1.-/lazy-test-eval.chk				\
+1.-/loadwav.chk					\
+1.-/modifier/accel-adaptive.chk			\
+1.-/modifier/accel.chk				\
+1.-/modifier/smooth-adaptive.chk		\
+1.-/modifier/smooth.chk				\
+1.-/modifier/speed-adaptive.chk			\
+1.-/modifier/speed.chk				\
+1.-/modifier/time-adaptive.chk			\
+1.-/modifier/time.chk				\
+1.-/modifier/syntax.chk				\
+1.-/nameresolution-array.chk			\
+1.-/nameresolution-dollar-computed.chk		\
+1.-/nameresolution-dollars.chk			\
+1.-/nameresolution-objects.chk			\
+1.-/object-events.chk				\
+1.-/sinus.chk					\
+1.-/speed-zero.chk				\
+1.-/synchro-assignment.chk			\
+1.-/tag.chk					\
+1.-/tag-reduce.chk                              \
+1.-/variable-normalize.chk			\
+1.-/variable-qualifiers.chk
 
 # Uobject tests that we fail because features are not implemented
 XFAIL_TESTS +=                                  \
