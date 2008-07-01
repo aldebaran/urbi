3daed8a956751c5f7e76c37d6e8bd2cd1cdfa933
mefyl <hocquet@gostai.com>
2008-07-01 17:10:10 +0200

    Use make_primitive in the C++ binding system.
        * src/object/cxx-object.hxx: Here.

--- src/object/cxx-object.hxx
+++ src/object/cxx-object.hxx
@@ -4,6 +4,7 @@
 # include <boost/bind.hpp>
 
 # include <object/atom.hh>
+# include <object/cxx-primitive.hh>
 # include <object/primitive-class.hh>
 # include <object/string-class.hh>
 # include <object/object-class.hh>
@@ -83,191 +84,17 @@ namespace object
   }
 
   // BINDER
-
   template <typename T>
   CxxObject::Binder<T>::Binder(rObject tgt)
     : tgt_(tgt)
   {}
-
-  namespace
-  {
-
-# define PRINT_true(X) X
-# define PRINT_false(X)
-# define PRINT_truetrue(X) X
-# define PRINT_falsetrue(X)
-# define PRINT_truefalse(X)
-# define PRINT_falsefalse(X)
-# define NPRINT_true(X)
-# define NPRINT_false(X) X
-
-# define WHEN(Cond, X) PRINT_##Cond(X)
-# define WHEN_NOT(Cond, X) NPRINT_##Cond(X)
-
-# define IF(Cond, Then, Else) WHEN(Cond, Then) WHEN_NOT(Cond, Else)
-
-# define COMMA_ ,
-# define COMMA(Cond) PRINT_##Cond(COMMA_)
-
-# define COMMA2(Cond1, Cond2) PRINT_##Cond1##Cond2(COMMA_)
-
-# define MET(Met, Name, Ret, Run, Arg1, Arg2, Arg3)     \
-    IF(Ret, libport::shared_ptr<R>, void)               \
-    (WHEN(Met, T::)*Name)(                              \
-      WHEN(Run, runner::Runner&) COMMA2(Run, Arg1)      \
-      WHEN(Arg1, libport::shared_ptr<A1>)               \
-      COMMA(Arg2) WHEN(Arg2, libport::shared_ptr<A2>)   \
-      COMMA(Arg3) WHEN(Arg3, libport::shared_ptr<A3>)   \
-      )
-
-#define GET_ARG(Met, N)                                                 \
-    type_check<A##N>(args[N WHEN_NOT(Met, - 1)], name.name_get());     \
-    libport::shared_ptr<A##N> a##N =                                   \
-      args[N WHEN_NOT(Met, - 1)].unsafe_cast<A##N>();                  \
-    assert(a##N);
-
-#define FETCH_TGT()                                                     \
-    type_check<T>(args[0], name.name_get());                            \
-    libport::shared_ptr<T> tgt = args[0].unsafe_cast<T>();
-
-#define PRIMITIVE(Met, Ret, ArgsC, Run, Arg1, Arg2, Arg3)               \
-    template <typename T                                                \
-              COMMA(Ret) WHEN(Ret, typename R)                          \
-      COMMA(Arg1) WHEN(Arg1, typename A1)                               \
-      COMMA(Arg2) WHEN(Arg2, typename A2)                               \
-      COMMA(Arg3) WHEN(Arg3, typename A3)                               \
-      >                                                                 \
-    struct primitive<T, MET(Met, , Ret, Run, Arg1, Arg2, Arg3)>         \
-    {                                                                   \
-        static rObject make(                                            \
-          MET(Met, method, Ret, Run, Arg1, Arg2, Arg3),                 \
-          const libport::Symbol& IF(Met, name, WHEN(Arg1, name)),       \
-          runner::Runner& WHEN(Run, r),                                 \
-          objects_type args)                                            \
-        {                                                               \
-	  (void)args;							\
-          assert(args.size() ==                                         \
-                 ArgsC IF(Met, + 1, WHEN_NOT(Arg1, + 1)));              \
-          WHEN(Met, FETCH_TGT());                                       \
-          WHEN(Arg1, GET_ARG(Met, 1))                                   \
-            WHEN(Arg2, GET_ARG(Met, 2))                                 \
-            WHEN(Arg3, GET_ARG(Met, 3))                                 \
-            WHEN(Ret, return)                                           \
-            (WHEN(Met, tgt.get()->*)method)(                            \
-              WHEN(Run, r) COMMA2(Run, Arg1)                            \
-              WHEN(Arg1, a1)                                            \
-              COMMA(Arg2) WHEN(Arg2, a2)                                \
-              COMMA(Arg3) WHEN(Arg3, a3)                                \
-              );                                                        \
-          return void_class;                                            \
-        }                                                               \
-      }
-
-#define LIST_MET(Name, Met, Ret, Run)                                  \
-    IF(Ret, R, void)                                                   \
-    (WHEN(Met, T::)*Name)(WHEN(Run, runner::Runner&) COMMA(Run)        \
-                          objects_type)                                \
-
-#define LIST_PRIMITIVE(Met, Ret, Run)                                   \
-    template <typename T                                                \
-              COMMA(Ret) WHEN(Ret, typename R)                          \
-      >                                                                 \
-    struct primitive<T, LIST_MET(, Met, Ret, Run)>                      \
-    {                                                                   \
-        static rObject make(                                            \
-          LIST_MET(method, Met, Ret, Run),                              \
-          const libport::Symbol& name,                                  \
-          runner::Runner& WHEN(Run, r),                                 \
-          objects_type args)                                            \
-        {                                                               \
-	  (void)name;                                                   \
-          WHEN(Met, FETCH_TGT());                                       \
-          WHEN(Met, args.pop_front());                                  \
-          WHEN(Ret, return)                                             \
-            (WHEN(Met, tgt.get()->*)method)(                            \
-              WHEN(Run, r) COMMA(Run)                                   \
-              args                                                      \
-              );                                                        \
-          return void_class;                                            \
-        }                                                               \
-      }
-
-    template <typename T, typename M>
-    struct primitive
-    {
-    };
-
-    /* Python for these:
-for met in ('true', 'false'):
-    for ret in ('true', 'false'):
-        for run in ('true', 'false'):
-            print "    LIST_PRIMITIVE(%s, %s, %s);" % (met, ret, run)
-     */
-    LIST_PRIMITIVE(true, true, true);
-    LIST_PRIMITIVE(true, true, false);
-    LIST_PRIMITIVE(true, false, true);
-    LIST_PRIMITIVE(true, false, false);
-    LIST_PRIMITIVE(false, true, true);
-    LIST_PRIMITIVE(false, true, false);
-    LIST_PRIMITIVE(false, false, true);
-    LIST_PRIMITIVE(false, false, false);
-
-    /* Python for these:
-max = 3
-for ret in ('true', 'false'):
-    for run in ('true', 'false'):
-        for nargs in range(max + 1):
-            for macro in ('PRIMITIVE ', 'PRIMITIVEF'):
-                print "    %s(%s, %i, %s" % (macro, ret, nargs, run),
-                for i in range(max):
-                    print ", %s" % str(i < nargs).lower(),
-                print ");"
-    */
-    PRIMITIVE(true , true, 0, true , false , false , false );
-    PRIMITIVE(true , true, 1, true , true , false , false );
-    PRIMITIVE(true , true, 2, true , true , true , false );
-    PRIMITIVE(true , true, 3, true , true , true , true );
-    PRIMITIVE(true , true, 0, false , false , false , false );
-    PRIMITIVE(true , true, 1, false , true , false , false );
-    PRIMITIVE(true , true, 2, false , true , true , false );
-    PRIMITIVE(true , true, 3, false , true , true , true );
-    PRIMITIVE(true , false, 0, true , false , false , false );
-    PRIMITIVE(true , false, 1, true , true , false , false );
-    PRIMITIVE(true , false, 2, true , true , true , false );
-    PRIMITIVE(true , false, 3, true , true , true , true );
-    PRIMITIVE(true , false, 0, false , false , false , false );
-    PRIMITIVE(true , false, 1, false , true , false , false );
-    PRIMITIVE(true , false, 2, false , true , true , false );
-    PRIMITIVE(true , false, 3, false , true , true , true );
-    PRIMITIVE(false, true, 0, true , false , false , false );
-    PRIMITIVE(false, true, 1, true , true , false , false );
-    PRIMITIVE(false, true, 2, true , true , true , false );
-    PRIMITIVE(false, true, 3, true , true , true , true );
-    PRIMITIVE(false, true, 0, false , false , false , false );
-    PRIMITIVE(false, true, 1, false , true , false , false );
-    PRIMITIVE(false, true, 2, false , true , true , false );
-    PRIMITIVE(false, true, 3, false , true , true , true );
-    PRIMITIVE(false, false, 0, true , false , false , false );
-    PRIMITIVE(false, false, 1, true , true , false , false );
-    PRIMITIVE(false, false, 2, true , true , true , false );
-    PRIMITIVE(false, false, 3, true , true , true , true );
-    PRIMITIVE(false, false, 0, false , false , false , false );
-    PRIMITIVE(false, false, 1, false , true , false , false );
-    PRIMITIVE(false, false, 2, false , true , true , false );
-    PRIMITIVE(false, false, 3, false , true , true , true );
-  }
 
   template <typename T>
   template <typename M>
   void CxxObject::Binder<T>::operator()(const libport::Symbol& name,
                                         M method)
   {
-    // If make is unfound here, you passed an unsupported pointer type
-    // to the binder.
-    rObject p =
-      new Primitive(boost::bind(primitive<T, M>::make,
-                                method, name, _1, _2));
-    tgt_->slot_set(name, p);
+    tgt_->slot_set(name, make_primitive(method, name));
   }
 
 
@@ -283,16 +110,6 @@ for ret in ('true', 'false'):
     else if (!o->is_a<T>())
       throw object::WrongArgumentType(T::type_name, co->type_name_get(), fun);
   }
-
-  template <>
-  inline void
-  type_check<Object>(rObject, const std::string&)
-  {}
-
-  template <>
-  inline void
-  type_check<List>(rObject, const std::string&)
-  {}
 
 }
 
