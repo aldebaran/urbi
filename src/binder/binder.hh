d03f52d40b52dd79e620f1857bef2abbc4c4c38d
Akim Demaille <demaille@gostai.com>
2008-07-07 17:00:41 +0200

    Formatting changes.

--- src/binder/binder.hh
+++ src/binder/binder.hh
@@ -23,113 +23,113 @@ namespace binder
   /// Ast local variables binder.
   class Binder : public ast::Cloner
   {
-    public:
-      /// \name Useful shorthands.
-      /// \{
-      /// Super class type.
-      typedef ast::Cloner super_type;
-      /// Import rObject
-      typedef object::rObject rObject;
-      /// \}
+  public:
+    /// \name Useful shorthands.
+    /// \{
+    /// Super class type.
+    typedef ast::Cloner super_type;
+    /// Import rObject
+    typedef object::rObject rObject;
+    /// \}
 
-      /// \name Ctor & dtor.
-      /// \{
-      /// Construct a \c Binder.
-      Binder ();
+    /// \name Ctor & dtor.
+    /// \{
+    /// Construct a \c Binder.
+    Binder ();
 
-      /// Destroy a Binder.
-      virtual ~Binder ();
-      /// \}
+    /// Destroy a Binder.
+    virtual ~Binder ();
+    /// \}
 
-      /// Import visit from DefaultVisitor.
-      using super_type::visit;
+    /// Import visit from DefaultVisitor.
+    using super_type::visit;
 
-    protected:
-      CONST_VISITOR_VISIT_NODES((And)
-                                (Assignment)
-                                (Call)
-                                (CallMsg)
-                                (Closure)
-                                (Declaration)
-                                (Do)
-                                (Foreach)
-                                (Function)
-                                (Nary)
-                                (Scope));
+  protected:
+    CONST_VISITOR_VISIT_NODES((And)
+                              (Assignment)
+                              (Call)
+                              (CallMsg)
+                              (Closure)
+                              (Declaration)
+                              (Do)
+                              (Foreach)
+                              (Function)
+                              (Nary)
+                              (Scope));
 
-      template <typename Code>
-      void handleRoutine(libport::shared_ptr<const Code> code);
+    template <typename Code>
+    void handleRoutine(libport::shared_ptr<const Code> code);
 
-      template <typename Node, typename NewNode>
-      void link_to_declaration(Node input,
-                               NewNode current,
-                               const libport::Symbol& name,
-                               unsigned depth);
+    template <typename Node, typename NewNode>
+    void link_to_declaration(Node input,
+                             NewNode current,
+                             const libport::Symbol& name,
+                             unsigned depth);
 
-    private:
-      /// Actions to perform at exit of the innermost scope.
-      typedef std::list<libport::Finally> unbind_type;
-      unbind_type unbind_;
+  private:
+    /// Actions to perform at exit of the innermost scope.
+    typedef std::list<libport::Finally> unbind_type;
+    unbind_type unbind_;
 
-      /// Declaration * depth
-      typedef std::pair<ast::rDeclaration, unsigned> binding_type;
-      typedef std::list<binding_type> Bindings;
-      typedef std::map<libport::Symbol, Bindings> Environment;
-      /// Map of currently bound variables
-      Environment env_;
+    /// Declaration * depth
+    typedef std::pair<ast::rDeclaration, unsigned> binding_type;
+    typedef std::list<binding_type> Bindings;
+    typedef std::map<libport::Symbol, Bindings> Environment;
+    /// Map of currently bound variables
+    Environment env_;
 
-      /// Whether to apply setSlot on self
-      typedef std::list<bool> set_on_self_type;
-      set_on_self_type setOnSelf_;
+    /// Whether to apply setSlot on self
+    typedef std::list<bool> set_on_self_type;
+    set_on_self_type setOnSelf_;
 
-      /// \name Routine stack.
-      /// \{
+    /// \name Routine stack.
+    /// \{
 
-      /// The stack of current number of local variables, and maximum
-      /// number of local variable used by the current routine.
-      typedef std::list<ast::rCode> routine_stack_type;
-      routine_stack_type routine_stack_;
-      /// Helpers routines to manipulate the frame size stack
-      void routine_push(ast::rCode f);
-      void routine_pop();
+    /// The stack of current number of local variables, and maximum
+    /// number of local variable used by the current routine.
+    typedef std::list<ast::rCode> routine_stack_type;
+    routine_stack_type routine_stack_;
+    /// Helpers routines to manipulate the frame size stack
+    void routine_push(ast::rCode f);
+    void routine_pop();
 
-      /// The routine currently defined.
-      /// Cannot be called if there is none.
-      ast::rCode routine() const;
+    /// The routine currently defined.
+    /// Cannot be called if there is none.
+    ast::rCode routine() const;
 
-      /// The innermost function (not closure).
-      /// May return 0.
-      ast::rFunction function() const;
-      /// \}
+    /// The innermost function (not closure).
+    /// May return 0.
+    ast::rFunction function() const;
+    /// \}
 
-      /// Level of routine nesting.
-      unsigned depth_;
-      /// Local index at the toplevel
-      unsigned toplevel_index_;
+    /// Level of routine nesting.
+    unsigned depth_;
+    /// Local index at the toplevel
+    unsigned toplevel_index_;
 
-      /// Register that \a var is bound in any subscope, \a being its
-      /// declaration
-      void bind(ast::rDeclaration decl);
+    /// Register that \a var is bound in any subscope, \a being its
+    /// declaration
+    void bind(ast::rDeclaration decl);
 
-      /// \return 0 if the variable isn't local, or the depth in
-      /// number of nested routines otherwise.
-      unsigned depth_get(const libport::Symbol& name);
-      ast::rDeclaration decl_get(const libport::Symbol& name);
+    /// \return 0 if the variable isn't local, or the depth in
+    /// number of nested routines otherwise.
+    unsigned depth_get(const libport::Symbol& name);
+    ast::rDeclaration decl_get(const libport::Symbol& name);
 
-      /// Factored method to handle scopes.
-      ast::rExp handleScope(ast::rConstAbstractScope scope, bool setOnSelf);
+    /// Factored method to handle scopes.
+    ast::rExp handleScope(ast::rConstAbstractScope scope, bool setOnSelf);
 
-      /// Factored method to create updateSlot/setSlot calls.
-      ast::rCall changeSlot(const ast::loc& l,
-                            const libport::Symbol& name,
-                            const libport::Symbol& method,
-                            ast::rConstExp value);
+    /// Factored method to create updateSlot/setSlot calls.
+    ast::rCall changeSlot(const ast::loc& l,
+                          const libport::Symbol& name,
+                          const libport::Symbol& method,
+                          ast::rConstExp value);
 
-      /// Make a lazy from \a arg
-      ast::rExp lazify (ast::rExp arg, const ast::loc& loc);
+    /// Make a lazy from \a arg
+    ast::rExp lazify (ast::rExp arg, const ast::loc& loc);
 
-      /// Make a closure from \a arg
-      ast::rClosure make_closure(ast::rConstExp e, const ast::loc& loc);
+    /// Make a closure from \a arg
+    ast::rClosure make_closure(ast::rConstExp e, const ast::loc& loc);
   };
 
 } // namespace binder
