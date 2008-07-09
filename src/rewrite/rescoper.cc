d2d5629c8087ce35c4a58325c1432ca6b00e9ad6
mefyl <hocquet@gostai.com>
2008-07-09 17:38:30 +0200

    Factor unscoping between '&' and ','.
        * src/rewrite/rescoper.cc (unscope): New.

--- src/rewrite/rescoper.cc
+++ src/rewrite/rescoper.cc
@@ -31,6 +31,29 @@ namespace rewrite
     {
       return new ast::Assignment(l, s, value, 0);
     }
+
+    static
+    /**
+     *  Helper to extract declarations to a nary.
+     *
+     *  If \a subject is 'var x = v;', push back 'var x;' in \a nary
+     *  and return 'x = v;'
+     *  Otherwise, just return \a subject.
+     */
+    ast::rExp unscope(ast::rExp subject, ast::rNary nary)
+    {
+      if (ast::rConstDeclaration dec =
+          subject.unsafe_cast<const ast::Declaration>())
+      {
+        ast::loc l = subject->location_get();
+        const libport::Symbol name = dec->what_get();
+        nary->push_back(make_declaration(l, name),
+                        ast::flavor_pipe);
+        return make_assignment(l, name, dec->value_get());
+      }
+      else
+        return subject;
+    }
   }
 
   Rescoper::Rescoper()
@@ -46,13 +69,7 @@ namespace rewrite
     ast::rNary nary = new ast::Nary(l);
     foreach (ast::rExp child, a->children_get())
     {
-      if (ast::rConstDeclaration dec =
-          child.unsafe_cast<const ast::Declaration>())
-      {
-        const libport::Symbol name = dec->what_get();
-        nary->push_back(make_declaration(l, name), ast::flavor_pipe);
-        child = make_assignment(l, name, dec->value_get());
-      }
+      child = unscope(child, nary);
       // Wrap every child in a closure
       res->children_get().push_back(recurse(parser::ast_closure(child)));
     }
@@ -70,21 +87,10 @@ namespace rewrite
       ast::rStmt stm = child.unsafe_cast<ast::Stmt>();
       if (stm && stm->flavor_get() == ast::flavor_comma)
       {
-        if (ast::rDeclaration dec =
-            stm->expression_get().unsafe_cast<ast::Declaration>())
-        {
-          const ast::loc l = dec->location_get();
-          const libport::Symbol s = dec->what_get();
-          res->push_back(recurse(make_declaration(l, s)),
-                         ast::flavor_semicolon);
-          res->push_back(
-            recurse(
-              parser::ast_closure(make_assignment(l, s, dec->value_get()))),
-            ast::flavor_comma);
-        }
-        else
-          res->push_back(recurse(parser::ast_closure(child)),
-                         ast::flavor_comma);
+        ast::rExp child = stm->expression_get();
+        child = unscope(child, res);
+        res->push_back(recurse(parser::ast_closure(child)),
+                       ast::flavor_comma);
       }
       else
         res->push_back(recurse(child));
