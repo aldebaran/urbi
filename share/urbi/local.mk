d1fd3457f950f1c31c7592853ab8ad3beb5c310d
mefyl <hocquet@gostai.com>
2008-07-21 14:21:35 +0200

    Generate tutorial's content.
        * share/urbi/tutorial/tutorial.xml: New. Tutorial content.
        * share/urbi/tutorial/tutorial.py: New. XML to Urbi converter
        * share/urbi/tutorial.u: Improve tutorial library.
        * share/urbi/local.mk: Add tutorial generation.
        * share/urbi/urbi.u: Provide 'help'.
        * share/urbi/help.u: Deleted.

--- share/urbi/local.mk
+++ share/urbi/local.mk
@@ -1,2 +1,7 @@
 # Yes, this is not by the book, but it is so cooler.
 dist_urbi_DATA := $(wildcard $(srcdir)/share/urbi/*.u)
+
+$(srcdir)/share/urbi/tutorial-content.u: share/urbi/tutorial/tutorial.xml share/urbi/tutorial/tutorial.py
+	rm -f $@ $@.tmp
+	$(srcdir)/share/urbi/tutorial/tutorial.py $< > $@.tmp
+	mv $@.tmp $@
