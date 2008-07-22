6c80011d65a276de43b002bd3bd7d583ed6ec15f
Akim Demaille <demaille@gostai.com>
2008-07-22 18:57:19 +0200

    Formatting changes.

--- src/kernel/ughostconnection.hh
+++ src/kernel/ughostconnection.hh
@@ -1,23 +1,4 @@
-/*! \file ughostconnection.hh
- *******************************************************************************
-
- File: ughostconnection.h\n
- Definition of the UGhostConnection class.
-
- This file is part of
- %URBI Kernel, version __kernelversion__\n
- (c) Jean-Christophe Baillie, 2004-2005.
-
- Permission to use, copy, modify, and redistribute this software for
- non-commercial use is hereby granted.
-
- This software is provided "as is" without warranty of any kind,
- either expressed or implied, including but not limited to the
- implied warranties of fitness for a particular purpose.
-
- For more information, comments, bug reports: http://www.urbiforge.net
-
- **************************************************************************** */
+/// \file kernel/ughostconnection.hh
 
 #ifndef KERNEL_UGHOSTCONNECTION_HH
 # define KERNEL_UGHOSTCONNECTION_HH
@@ -32,14 +13,14 @@
 class UGhostConnection : public UConnection
 {
 public:
-  UGhostConnection (UServer& s);
-  virtual ~UGhostConnection ();
-  virtual UConnection& close ();
+  UGhostConnection(UServer& s);
+  virtual ~UGhostConnection();
+  virtual UConnection& close();
 
 protected:
-  virtual size_t effective_send (const char* buffer, size_t length);
+  virtual size_t effective_send(const char* buffer, size_t length);
 public:
-  virtual UConnection& endline ();
+  virtual UConnection& endline();
 };
 
 #endif // !KERNEL_UGHOSTCONNECTION_HH
