f0ff910d1c401fc939bacccdd3bd155041ca9c83
Akim Demaille <demaille@gostai.com>
2008-07-22 18:17:33 +0200

    Formatting changes.

--- src/kernel/uconnection.cc
+++ src/kernel/uconnection.cc
@@ -1,23 +1,5 @@
-/*! \file uconnection.cc
- *******************************************************************************
+/// \file kernel/uconnection.cc
 
- File: uconnection.cc\n
- Implementation of the UConnection class.
-
- This file is part of
- %URBI Kernel, version __kernelversion__\n
- (c) Jean-Christophe Baillie, 2004-2005.
-
- Permission to use, copy, modify, and redistribute this software for
- non-commercial use is hereby granted.
-
- This software is provided "as is" without warranty of any kind,
- either expressed or implied, including but not limited to the
- implied warranties of fitness for a particular purpose.
-
- For more information, comments, bug reports: http://www.urbiforge.net
-
- **************************************************************************** */
 //#define ENABLE_DEBUG_TRACES
 #include <libport/compiler.hh>
 
