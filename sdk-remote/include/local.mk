4704376251ad5329bc75a4419f70aed7e09853e1
Akim Demaille <demaille@gostai.com>
2008-07-29 14:52:51 +0200

    Adjust to sdk/local.mk.
    	* externals/libport: Update.
    	* Makefile.am: Do it.
    	Move include/ related stuff to...
    	* include/local.mk: here.

---
+++ sdk-remote/include/local.mk
@@ -0,0 +1,23 @@
+# Backward compatibility.
+include_HEADERS =				\
+include/uobject.h
+
+urbiinclude_HEADERS =				\
+include/urbi/uabstractclient.hh			\
+include/urbi/uclient.hh				\
+include/urbi/uconversion.hh			\
+include/urbi/uexternal.hh			\
+include/urbi/usyncclient.hh
+
+# liburbi is installed in $prefix, and its headers depend on libport.
+# So we must provide libport in $prefix, although they are also
+# installed as part of the SDK.  Using symlinks is not portable.
+#include_libport_dir = $(includedir)/libport
+#nodist_include_libport__HEADERS = $(libport_HEADERS) $(nodist_libport_HEADERS)
+#include_libport_sys_dir = $(include_libport_dir)/sys
+#nodist_include_libport_sys__HEADERS = $(libportsys_HEADERS)
+
+include_libport = include/libport
+include include/libport/libport.mk
+
+
