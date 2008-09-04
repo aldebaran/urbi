dce7af0bc91ed12d310b23b8494d1ea48e5f68ab
mefyl <hocquet@gostai.com>
2008-09-04 15:16:07 +0200

    Remove doc/share, now located in a separated repository.

--- doc/share/make/handout.mk
+++
@@ -1,7 +0,0 @@
-%-handout.tex: %.tex
-	perl -pe 's/(documentclass)({.*})/$$1\[handout]$$2/' $< >$@
-
-%-handout-4.pdf: %-handout.pdf
-	$(share_bin_dir)/beamer2handout $< 4up $@
-
-CLEANFILES += *-handout-4.pdf
