5c8da1e9af6be0576040842055ddaa2aa0089e91
mefyl <hocquet@gostai.com>
2008-09-18 16:10:11 +0200

    Update tests: adapt to the new exception system.
    	* tests: Here.

--- tests-local.mk
+++ tests-local.mk
@@ -19,7 +19,8 @@ TFAIL_TESTS +=					\
 
 # test we don't want to care about temporarily
 TFAIL_TESTS +=					\
-2.x/derive.chk
+2.x/derive.chk					\
+2.x/profiling.chk
 
 # k1 tests that currently don't pass, but we should.
 # In fact, the above list has been removed to gain some time
