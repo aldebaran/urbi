1cf465179067f7acaac1c86e7e51d3521e227e57
mefyl <hocquet@gostai.com>
2008-09-24 17:26:44 +0200

    Add C++/Urbi conversion for std::sets.
    	* src/object/cxx-conversions.hxx: Here.

--- src/object/cxx-conversions.hxx
+++ src/object/cxx-conversions.hxx
@@ -199,6 +199,30 @@ namespace object
     }
   };
 
+  // Conversion with std::set
+  template <typename T>
+  struct CxxConvert<std::set<T> >
+  {
+    static std::set<T>
+    to(const rObject& o, const libport::Symbol& name, unsigned idx)
+    {
+      type_check(o, List::proto);
+      std::set<T> res;
+      foreach (const rObject& elt, o->as<List>()->value_get())
+        res.insert(CxxConvert<T>::to(elt, name, idx));
+      return res;
+    }
+
+    static rObject
+    from(const std::set<T>& v, const libport::Symbol& name)
+    {
+      objects_type res;
+      foreach (const T& elt, v)
+        res.push_back(CxxConvert<T>::from(elt, name));
+      return new List(res);
+    }
+  };
+
 }
 
 #endif
