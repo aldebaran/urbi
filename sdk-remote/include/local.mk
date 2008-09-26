448f2bf1521f146330484dffe4b6ef290500b9e4
Akim Demaille <demaille@gostai.com>
2008-09-26 18:14:11 +0200

    We no longer need to split include/urbi in two places.
    This was when the kernel subscribed to liburbi/ via svn-externals:
    it needed to find some of the headers.  Now that it has the whole
    SDK-Remote, it is useless and too complex.
    
    	* src/liburbi/libuco/template_autogen.pl: Rename as...
    	* include/template_autogen.pl: this.
    	* src/liburbi/libuco/urbi/export.hh: Rename as...
    	* include/urbi/export.hh: this.
    	* src/liburbi/libuco/urbi/fwd.hh: Rename as...
    	* include/urbi/fwd.hh: this.
    	* src/liburbi/libuco/urbi/qt_umain.hh: Rename as...
    	* include/urbi/qt_umain.hh: this.
    	* src/liburbi/libuco/urbi/ubinary.hh: Rename as...
    	* include/urbi/ubinary.hh: this.
    	* src/liburbi/libuco/urbi/ublend-type.hh: Rename as...
    	* include/urbi/ublend-type.hh: this.
    	* src/liburbi/libuco/urbi/ucallbacks.hh.template: Rename as...
    	* include/urbi/ucallbacks.hh.template: this.
    	* src/liburbi/libuco/urbi/umain.hh: Rename as...
    	* include/urbi/umain.hh: this.
    	* src/liburbi/libuco/urbi/uobject.hh: Rename as...
    	* include/urbi/uobject.hh: this.
    	* src/liburbi/libuco/urbi/uproperty.hh: Rename as...
    	* include/urbi/uproperty.hh: this.
    	* src/liburbi/libuco/urbi/ustarter.hh: Rename as...
    	* include/urbi/ustarter.hh: this.
    	* src/liburbi/libuco/urbi/ustarter.hxx: Rename as...
    	* include/urbi/ustarter.hxx: this.
    	* src/liburbi/libuco/urbi/usystem.hh: Rename as...
    	* include/urbi/usystem.hh: this.
    	* src/liburbi/libuco/urbi/utable.hh: Rename as...
    	* include/urbi/utable.hh: this.
    	* src/liburbi/libuco/urbi/utimer-callback.hh: Rename as...
    	* include/urbi/utimer-callback.hh: this.
    	* src/liburbi/libuco/urbi/utimer-table.hh: Rename as...
    	* include/urbi/utimer-table.hh: this.
    	* src/liburbi/libuco/urbi/uvalue.hh: Rename as...
    	* include/urbi/uvalue.hh: this.
    	* src/liburbi/libuco/urbi/uvar.hh: Rename as...
    	* include/urbi/uvar.hh: this.
    
    	* src/liburbi/libuco/local.mk, include/local.mk: Adjust.

--- sdk-remote/include/local.mk
+++ sdk-remote/include/local.mk
@@ -2,13 +2,54 @@
 include_HEADERS =				\
 include/uobject.h
 
+ucallbacks_hh = include/urbi/ucallbacks.hh
+
 urbiinclude_HEADERS =				\
-include/urbi/uabstractclient.hh			\
-include/urbi/uclient.hh				\
-include/urbi/uconversion.hh			\
-include/urbi/uexternal.hh			\
-include/urbi/usyncclient.hh			\
-include/urbi/utag.hh
+  $(ucallbacks_hh)				\
+  include/urbi/export.hh			\
+  include/urbi/fwd.hh				\
+  include/urbi/qt_umain.hh			\
+  include/urbi/uabstractclient.hh		\
+  include/urbi/ubinary.hh			\
+  include/urbi/ublend-type.hh			\
+  include/urbi/uclient.hh			\
+  include/urbi/uconversion.hh			\
+  include/urbi/uexternal.hh			\
+  include/urbi/umain.hh				\
+  include/urbi/uobject.hh			\
+  include/urbi/uproperty.hh			\
+  include/urbi/ustarter.hh			\
+  include/urbi/ustarter.hxx			\
+  include/urbi/usyncclient.hh			\
+  include/urbi/usystem.hh			\
+  include/urbi/utable.hh			\
+  include/urbi/utag.hh				\
+  include/urbi/utimer-callback.hh		\
+  include/urbi/utimer-table.hh			\
+  include/urbi/uvalue.hh			\
+  include/urbi/uvar.hh
+
+## --------------- ##
+## ucallbacks.hh.  ##
+## --------------- ##
+
+EXTRA_DIST +=					\
+  include/urbi/template_autogen.pl		\
+  $(ucallbacks_hh).template
+MAINTAINERCLEANFILES += $(ucallbacks_hh)
+
+# We used to generate this file in builddir and not ship it, but it
+# required to adjust -I paths to find it in builddir.  Since this was
+# the only such file, it is easier to put it in srcdir (and ship it to
+# comply with the Automake model).
+$(srcdir)/$(ucallbacks_hh): $(ucallbacks_hh).template include/template_autogen.pl
+	rm -f $@.tmp $@
+	$(mkdir_p) $$(dirname "$@.tmp")
+	$(srcdir)/include/template_autogen.pl $(srcdir)/$(ucallbacks_hh).template >$@.tmp
+# Avoid accidental edition.
+	chmod a-w $@.tmp
+	mv $@.tmp $@
+
 
 # liburbi is installed in $prefix, and its headers depend on libport.
 # So we must provide libport in $prefix, although they are also
--- sdk-remote/src/liburbi/libuco/local.mk
+++ sdk-remote/src/liburbi/libuco/local.mk
@@ -2,28 +2,7 @@
 ## Sources and headers.  ##
 ## --------------------- ##
 
-ucallbacks_hh = libuco/urbi/ucallbacks.hh
-
 noinst_LTLIBRARIES = libuco/libuco.la
-
-dist_urbiinclude_HEADERS =			\
-  $(ucallbacks_hh)				\
-  libuco/urbi/export.hh				\
-  libuco/urbi/fwd.hh				\
-  libuco/urbi/qt_umain.hh			\
-  libuco/urbi/ubinary.hh			\
-  libuco/urbi/ublend-type.hh			\
-  libuco/urbi/umain.hh				\
-  libuco/urbi/uobject.hh			\
-  libuco/urbi/uproperty.hh			\
-  libuco/urbi/ustarter.hh			\
-  libuco/urbi/ustarter.hxx			\
-  libuco/urbi/usystem.hh			\
-  libuco/urbi/utable.hh				\
-  libuco/urbi/utimer-callback.hh		\
-  libuco/urbi/utimer-table.hh			\
-  libuco/urbi/uvalue.hh				\
-  libuco/urbi/uvar.hh
 
 dist_libuco_libuco_la_SOURCES =			\
   libuco/uobject-hub-common.cc			\
@@ -32,24 +11,3 @@ dist_libuco_libuco_la_SOURCES =			\
   libuco/uvalue-common.cc			\
   libuco/urbi-main.cc				\
   libuco/uvar-common.cc
-
-## --------------- ##
-## ucallbacks.hh.  ##
-## --------------- ##
-
-EXTRA_DIST +=				\
-  libuco/template_autogen.pl		\
-  $(ucallbacks_hh).template
-MAINTAINERCLEANFILES += $(ucallbacks_hh)
-
-# We used to generate this file in builddir and not ship it, but it
-# required to adjust -I paths to find it in builddir.  Since this was
-# the only such file, it is easier to put it in srcdir (and ship it to
-# comply with the Automake model).
-$(srcdir)/$(ucallbacks_hh): $(ucallbacks_hh).template libuco/template_autogen.pl
-	rm -f $@.tmp $@
-	$(mkdir_p) $$(dirname "$@.tmp")
-	$(srcdir)/libuco/template_autogen.pl $(srcdir)/$(ucallbacks_hh).template >$@.tmp
-# Avoid accidental edition.
-	chmod a-w $@.tmp
-	mv $@.tmp $@
