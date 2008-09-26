9a0e37657909a3f85f23a0f8f94cd4d7f775b53b
Akim Demaille <demaille@gostai.com>
2008-09-26 15:45:24 +0200

    src/utils/Makefile.am -> local.mk.
    	* src/utils/Makefile.am: Rename as...
    	* src/utils/local.mk: this.
    	Adjust.
    	* configure.ac, src/Makefile.am: Adjust.

---
+++ sdk-remote/src/utils/local.mk
@@ -0,0 +1,26 @@
+if EXAMPLES
+bin_PROGRAMS +=					\
+  utils/urbicycle				\
+  utils/urbireverse				\
+  utils/urbiscale				\
+  utils/urbiwalk
+endif
+
+utils_urbicycle_SOURCES =			\
+  utils/parse-header.hh				\
+  utils/urbicycle.cc
+utils_urbireverse_SOURCES =			\
+  utils/parse-header.hh				\
+  utils/urbireverse.cc
+utils_urbiscale_SOURCES =			\
+  utils/parse-header.hh				\
+  utils/urbiscale.cc
+
+utils_urbiwalk_SOURCES =			\
+  utils/urbiwalk.cc				\
+  utils/move.cc					\
+  utils/move.hh
+
+utils_urbiwalk_LDADD =				\
+  $(top_builddir)/jpeg-6b/libjpeg.la		\
+  $(top_builddir)/src/liburbi/liburbi.la
