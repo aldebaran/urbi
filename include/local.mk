ab1d30f9d787aa1838e1931ac468cbd798ae49f9
Akim Demaille <demaille@gostai.com>
2008-09-27 17:54:50 +0200

    Makefile cleanups.
    	* dev/dev.mk: Rename as...
    	* dev/local.mk: this, for consistency.
    	* include/local.mk: New, extracted from...
    	* Makefile.am: here.
    	* src/ast/local.mk: Ask for the creation of ast.dot, so that it is
    	not updated only for make dist.

---
+++ include/local.mk
@@ -0,0 +1,51 @@
+## -------------------------- ##
+## Installed kernel headers.  ##
+## -------------------------- ##
+
+kernelincludekerneldir = $(kernelincludedir)/kernel
+kernelincludekernel_HEADERS =			\
+include/kernel/exception.hh			\
+include/kernel/exception.hxx			\
+include/kernel/fwd.hh				\
+include/kernel/kernconf.hh			\
+include/kernel/uconnection.hh			\
+include/kernel/uconnection.hxx			\
+include/kernel/userver.hh			\
+include/kernel/userver.hxx			\
+include/kernel/utypes.hh
+
+include share/urbi/local.mk
+
+nodist_kernelincludekernel_HEADERS =		\
+include/kernel/version.hh
+
+
+## ------- ##
+## Check.  ##
+## ------- ##
+
+kernel_headers = 				\
+  $(kernelincludekernel_HEADERS)		\
+  $(nodist_kernelincludekernel_HEADERS)
+
+SKIP_HEADERS_STD = \
+  $(EGREP) -v '^<(cstring|string|cstdarg|iomanip|sstream|iosfwd)>$$'
+
+nullstring :=
+space := $(nullstring) # end of the line
+
+SKIP_HEADERS_KERNEL_PUBLIC := \
+  $(EGREP) -v "^<$(subst $(space),|,$(subst include/,,$(kernel_headers)))>$$"
+
+.PHONY: check-headers
+check-local: check-headers
+check-headers:
+# It is forbidden for public headers to depend on non public headers.
+	@echo "Checking whether private headers are included by public headers."
+	for i in $(kernelincludekernel_HEADERS);			\
+	do								\
+	  ! < $(srcdir)/$$i sed -ne '/^# *include */{s///;p;}' |	\
+	      $(EGREP) -v '^<(boost|libport)/' |			\
+	      $(SKIP_HEADERS_STD) |					\
+	      $(SKIP_HEADERS_KERNEL_PUBLIC);				\
+	done
--- src/ast/local.mk
+++ src/ast/local.mk
@@ -9,54 +9,58 @@ BUILT_SOURCES += $(BUILT_SOURCES_ast)
 # Don't use LOCAL_BUILT_SOURCES since it looks like the SOURCES of the
 # program `LOCAL_BUILT' and triggers Automake warnings.
 BUILT_SOURCES_ast_libuobject =			\
-ast/fwd.hh					\
-ast/all.hh					\
-ast/analyzer.hh					\
-ast/analyzer.cc					\
-ast/error.hh					\
-ast/error.hxx					\
-ast/error.cc					\
-ast/ignores					\
-ast/visitor.hh					\
-ast/cloner.hh					\
-ast/cloner.hxx					\
-ast/cloner.cc					\
-ast/default-visitor.hh				\
-ast/default-visitor.hxx				\
-ast/pretty-printer.hh				\
-ast/pretty-printer.cc				\
-$(ast_nodes)
+  ast/fwd.hh					\
+  ast/all.hh					\
+  ast/analyzer.hh				\
+  ast/analyzer.cc				\
+  ast/error.hh					\
+  ast/error.hxx					\
+  ast/error.cc					\
+  ast/ignores					\
+  ast/visitor.hh				\
+  ast/cloner.hh					\
+  ast/cloner.hxx				\
+  ast/cloner.cc					\
+  ast/default-visitor.hh			\
+  ast/default-visitor.hxx			\
+  ast/pretty-printer.hh				\
+  ast/pretty-printer.cc				\
+  $(ast_nodes)
 
 BUILT_SOURCES_ast = 				\
-$(BUILT_SOURCES_ast_libuobject) 			\
-ast/dot-printer.hh				\
-ast/dot-printer.cc
+  $(BUILT_SOURCES_ast_libuobject) 		\
+  ast/dot-printer.hh				\
+  ast/dot-printer.cc
 
 gen_dir = $(top_srcdir)/dev
-ast_gen_deps = $(gen_dir)/ast.py \
-	       $(gen_dir)/tools.py $(ast_srcdir)/ast.yml
+ast_gen_deps =					\
+  $(gen_dir)/ast.py				\
+  $(gen_dir)/tools.py $(ast_srcdir)/ast.yml
 
 include $(top_srcdir)/dev/ast-gen.mk
 
-dist_libuobject_la_SOURCES +=                   \
-ast/all.hh                                      \
-ast/catches-type.hh                             \
-ast/catches-type.cc                             \
-ast/exps-type.hh                                \
-ast/exps-type.cc                                \
-ast/flavor.hh                                   \
-ast/flavor.cc                                   \
-ast/loc.hh                                      \
-ast/local-declarations-type.cc                  \
-ast/local-declarations-type.hh                  \
-ast/nary-fwd.hh                                 \
-ast/new-clone.hh                                \
-ast/new-clone.hxx                               \
-ast/parametric-ast.hh                           \
-ast/parametric-ast.hxx                          \
-ast/parametric-ast.cc                           \
-ast/print.hh ast/print.cc                       \
-ast/symbols-type.hh                             \
-ast/symbols-type.cc                             \
-ast/visitor.hxx                                 \
-$(BUILT_SOURCES_ast_libuobject)
+dist_libuobject_la_SOURCES +=			\
+  ast/all.hh					\
+  ast/catches-type.hh				\
+  ast/catches-type.cc				\
+  ast/exps-type.hh				\
+  ast/exps-type.cc				\
+  ast/flavor.hh					\
+  ast/flavor.cc					\
+  ast/loc.hh					\
+  ast/local-declarations-type.cc		\
+  ast/local-declarations-type.hh		\
+  ast/nary-fwd.hh				\
+  ast/new-clone.hh				\
+  ast/new-clone.hxx				\
+  ast/parametric-ast.hh				\
+  ast/parametric-ast.hxx			\
+  ast/parametric-ast.cc				\
+  ast/print.hh ast/print.cc			\
+  ast/symbols-type.hh				\
+  ast/symbols-type.cc				\
+  ast/visitor.hxx				\
+  $(BUILT_SOURCES_ast_libuobject)
+
+# To ensure that the file is kept up to date.
+dist_noinst_DATA = ast/ast.dot
