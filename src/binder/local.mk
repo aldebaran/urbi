5a81df16f7457b381e462f1b0a5e1386c7a2afc8
Akim Demaille <demaille@gostai.com>
2008-09-28 10:23:50 +0200

    Fix distcheck.
    	* src/binder/local.mk: Ship bind-debug.hh.

--- src/binder/local.mk
+++ src/binder/local.mk
@@ -1,7 +1,8 @@
 dist_libuobject_la_SOURCES +=			\
-binder/fwd.hh					\
-binder/bind.hh					\
-binder/bind.cc					\
-binder/binder.hh				\
-binder/binder.hxx				\
-binder/binder.cc
+  binder/fwd.hh					\
+  binder/bind-debug.hh				\
+  binder/bind.hh				\
+  binder/bind.cc				\
+  binder/binder.hh				\
+  binder/binder.hxx				\
+  binder/binder.cc
