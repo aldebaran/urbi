2b04032862804c36a1ee5c6582a4a7bc3184e7b2
Akim Demaille <demaille@gostai.com>
2008-10-02 15:50:21 +0200

    Formatting changes.

--- src/scheduler/local.mk
+++ src/scheduler/local.mk
@@ -1,16 +1,16 @@
 dist_libuobject_la_SOURCES +=			\
-scheduler/coroutine.hh				\
-scheduler/coroutine.hxx				\
-scheduler/fwd.hh				\
-scheduler/job.cc				\
-scheduler/job.hh				\
-scheduler/job.hxx				\
-scheduler/pthread-coro.hh			\
-scheduler/pthread-coro.hxx			\
-scheduler/pthread-coro.cc			\
-scheduler/scheduler.hh				\
-scheduler/scheduler.hxx				\
-scheduler/scheduler.cc				\
-scheduler/tag.cc				\
-scheduler/tag.hh				\
-scheduler/tag.hxx
+  scheduler/coroutine.hh			\
+  scheduler/coroutine.hxx			\
+  scheduler/fwd.hh				\
+  scheduler/job.cc				\
+  scheduler/job.hh				\
+  scheduler/job.hxx				\
+  scheduler/pthread-coro.cc			\
+  scheduler/pthread-coro.hh			\
+  scheduler/pthread-coro.hxx			\
+  scheduler/scheduler.cc			\
+  scheduler/scheduler.hh			\
+  scheduler/scheduler.hxx			\
+  scheduler/tag.cc				\
+  scheduler/tag.hh				\
+  scheduler/tag.hxx
