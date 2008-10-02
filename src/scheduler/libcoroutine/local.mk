716612244a9ea73090f81a6aa82f0bf70283d81d
Akim Demaille <demaille@gostai.com>
2008-10-02 15:49:57 +0200

    Fix distcheck.
    	* src/scheduler/libcoroutine/local.mk: Ship Coro.c.

--- src/scheduler/libcoroutine/local.mk
+++ src/scheduler/libcoroutine/local.mk
@@ -1,9 +1,13 @@
 dist_libuobject_la_SOURCES +=			\
-scheduler/libcoroutine/Base.h			\
-scheduler/libcoroutine/Common.h			\
-scheduler/libcoroutine/Coro.cc			\
-scheduler/libcoroutine/Coro.h			\
-scheduler/libcoroutine/PortableUContext386.h	\
-scheduler/libcoroutine/PortableUContext.c	\
-scheduler/libcoroutine/PortableUContext.h	\
-scheduler/libcoroutine/PortableUContext_asm.S
+  scheduler/libcoroutine/Base.h			\
+  scheduler/libcoroutine/Common.h		\
+  scheduler/libcoroutine/Coro.cc		\
+  scheduler/libcoroutine/Coro.h			\
+  scheduler/libcoroutine/PortableUContext.c	\
+  scheduler/libcoroutine/PortableUContext.h	\
+  scheduler/libcoroutine/PortableUContext386.h	\
+  scheduler/libcoroutine/PortableUContext_asm.S
+
+# This is distributed, but not compiled directly: it is included by
+# Coro.cc.
+EXTRA_DIST += scheduler/libcoroutine/Coro.c
