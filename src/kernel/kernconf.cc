d90b6d46d42b9b35557f4b02a0c3a0dd17c10362
Samuel Tardieu <sam@rfc1149.net>
2008-10-09 14:36:38 +0200

    Make the minimum stack size depend on the default stack size.
    More exactly, it depends on the stack size chosen at compilation time.
    There is no reason to change the guard even if the stack size is
    changed at run time.
    
    	* src/kernel/kernconf.cc: Set the minimum stack size from the
    	default stack size.

--- src/kernel/kernconf.cc
+++ src/kernel/kernconf.cc
@@ -5,5 +5,5 @@ USDK_API kernconf_type kernconf = {
   // Default is large enough until we enable a mechanism to dynamically
   // reallocate task space on demand.
   /* .default_stack_size = */         URBI_KERNEL_STACK_SIZE * 1024,
-  /* .minimum_stack_size = */         4 * 1024
+  /* .minimum_stack_size = */         URBI_KERNEL_STACK_SIZE * 1024 / 8
 };
