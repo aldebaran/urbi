e11f53af9e6a7a5b24991aa15d38c5ccc946422f
Akim Demaille <demaille@gostai.com>
2008-10-13 20:07:04 +0200

    Fix make check-headers.
    BTW, it is already bound to make check.
    
    	* include/check-headers: New.
    	* include/local.mk: Remove all the code that used to do it here.
    	For the time being, neutralize this test, since userver.hh relies
    	on Boost.

--- include/local.mk
+++ include/local.mk
@@ -13,35 +13,16 @@ dist_kernelincludekernel_HEADERS =		\
   include/kernel/utypes.hh
 
 nodist_kernelincludekernel_HEADERS =		\
-include/kernel/version.hh
+  include/kernel/version.hh
 
 
 ## ------- ##
 ## Check.  ##
 ## ------- ##
 
-kernel_headers = 				\
-  $(dist_kernelincludekernel_HEADERS)		\
-  $(nodist_kernelincludekernel_HEADERS)
-
-SKIP_HEADERS_STD = \
-  $(EGREP) -v '^<(cstring|string|cstdarg|iomanip|sstream|iosfwd)>$$'
-
-nullstring :=
-space := $(nullstring) # end of the line
-
-SKIP_HEADERS_KERNEL_PUBLIC := \
-  $(EGREP) -v "^<$(subst $(space),|,$(subst include/,,$(kernel_headers)))>$$"
-
 .PHONY: check-headers
 check-local: check-headers
 check-headers:
 # It is forbidden for public headers to depend on non public headers.
-	@echo "Checking whether private headers are included by public headers."
-	for i in $(kernelincludekernel_HEADERS);			\
-	do								\
-	  ! < $(srcdir)/$$i sed -ne '/^# *include */{s///;p;}' |	\
-	      $(EGREP) -v '^<(boost|libport)/' |			\
-	      $(SKIP_HEADERS_STD) |					\
-	      $(SKIP_HEADERS_KERNEL_PUBLIC);				\
-	done
+	-srcdir=$(srcdir) \
+	  $(srcdir)/include/check-headers $(dist_kernelincludekernel_HEADERS)
