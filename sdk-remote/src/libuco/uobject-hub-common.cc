1ff3afb916f60b64987f51a9e2e31297247058ed
Akim Demaille <demaille@gostai.com>
2008-10-17 15:46:16 +0200

    Comment changes.

--- sdk-remote/src/libuco/uobject-hub-common.cc
+++ sdk-remote/src/libuco/uobject-hub-common.cc
@@ -1,22 +1,4 @@
-/*! \file uobject-hub-common.cc
- *******************************************************************************
-
- File: uobject-hub-common.cc\n
- Implementation of the UObject class.
-
- This file is part of LIBURBI\n
- (c) Jean-Christophe Baillie, 2004-2006.
-
- Permission to use, copy, modify, and redistribute this software for
- non-commercial use is hereby granted.
-
- This software is provided "as is" without warranty of any kind,
- either expressed or implied, including but not limited to the
- implied warranties of fitness for a particular purpose.
-
- For more information, comments, bug reports: http://www.urbiforge.com
-
- **************************************************************************** */
+/// \file libuco/uobject-hub-common.cc
 
 #include <urbi/uobject.hh>
 
