1fde33d0d74f2503400f3ca9c2993dd6330e8e77
Akim Demaille <demaille@gostai.com>
2008-11-06 17:04:13 +0100

    Save white bear cubs.
    	* src/kernel/local.mk (kernel/git-version.hh): Depend on .version
    	to avoid endless useless recompilations (and relinking).

--- src/kernel/local.mk
+++ src/kernel/local.mk
@@ -23,10 +23,28 @@ dist_libuobject_la_SOURCES +=			\
 
 
 # Create the file git-config.hh with accurate revision information.
-# This is time consumming.  We will try to make it less costly later.
+#
+# We depend on .version to avoid frequent regeneration of this file.
+#
+# Alternatively you could make it "PHONY" in which case Make knows it
+# has to recreate each time "make" is run.  Of course in that case you
+# really want to use "move-if-change" to avoid updating the timestamp
+# of this file, so that we don't have to recompile all its
+# dependencies.
+#
+# But that does not work because in that case, as it is PHONY, when
+# "recreated" the file is recent in the tables of Make (although its
+# actual timestamp was not changed on the disk), so it wants to
+# recompile all the files that depend on it.  In other words,
+# "move-if-change" is useless :(
+#
+# Why don't we have the same problem with ".version"? (Indeed, it is
+# declared PHONY, so recreated all the time, but then since
+# git-version.hh depends on it, it will be recreated etc.).  Because
+# it is not in the same Makefile (Makefile vs. src/Makefile).
 BUILT_SOURCES += kernel/git-version.hh
-.PHONY: kernel/git-version.hh
-kernel/git-version.hh:
-	rm -f $@.tmp
-	(cd $(top_srcdir) && build-aux/git-version-gen --header) >$@.tmp
-	$(build_aux_dir)/move-if-change $@.tmp $@
+kernel/git-version.hh: $(top_srcdir)/.version
+	$(build_aux_dir)/git-version-gen	\
+		--srcdir=$(top_srcdir)		\
+		--header --output=$@
+
