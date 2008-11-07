fa3bf9d79e5fd85c31f648fa5582eae332950c16
Akim Demaille <demaille@gostai.com>
2008-11-07 09:25:18 +0100

    Comment changes.

--- sdk-remote/src/libuco/urbi-main.cc
+++ sdk-remote/src/libuco/urbi-main.cc
@@ -1,22 +1,4 @@
-/*! \file urbi-main.cc.cc
- *******************************************************************************
-
- File: urbi-main.cc\n
- Implementation of the urbi_main function.
-
- This file is part of LIBURBI\n
- Copyright (c) 2004, 2005, 2006, 2007, 2008 Gostai S.A.S.
-
- Permission to use, copy, modify, and redistribute this software for
- non-commercial use is hereby granted.
-
- This software is provided "as is" without warranty of any kind,
- either expressed or implied, including but not limited to the
- implied warranties of fitness for a particular purpose.
-
- For more information, comments, bug reports: http://www.urbiforge.com
-
- **************************************************************************** */
+/// \file libuco/urbi-main.cc
 
 #include <urbi/uobject.hh>
 
