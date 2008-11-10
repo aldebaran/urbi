ed42fd1d8176093ae0e487e24d011b0e67abba0d
Akim Demaille <demaille@gostai.com>
2008-11-10 20:29:58 +0100

    Formatting changes.

--- sdk-remote/src/liburbi/umain.cc
+++ sdk-remote/src/liburbi/umain.cc
@@ -1,20 +1,4 @@
-/*! \file liburbi/umain.cc
- *******************************************************************************
- The main() function.
-
- This file is part of liburbi-cpp\n
- (c) Jean-Christophe Baillie, 2004-2006.
-
- Permission to use, copy, modify, and redistribute this software for
- non-commercial use is hereby granted.
-
- This software is provided "as is" without warranty of any kind,
- either expressed or implied, including but not limited to the
- implied warranties of fitness for a particular purpose.
-
- For more information, comments, bug reports: http://www.urbiforge.com
-
- **************************************************************************** */
+/// \file liburbi/umain.cc
 
 #include <urbi/umain.hh>
 
