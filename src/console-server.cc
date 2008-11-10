51347620834fe52fa838a8b2840d89674814c57f
Akim Demaille <demaille@gostai.com>
2008-11-10 15:11:09 +0100

    Formatting changes.

--- src/console-server.cc
+++ src/console-server.cc
@@ -100,7 +100,7 @@ namespace
 {
   static
   void
-  usage ()
+  usage()
   {
     std::cout <<
       "usage: " << libport::program_name << " [OPTION].. [FILE]...\n"
@@ -124,179 +124,180 @@ namespace
 
   static
   void
-  version ()
+  version()
   {
     userver_package_info_dump(std::cout) << std::endl;
     exit (EX_OK);
   }
 }
 
-namespace urbi {
-
-
-/// Command line options needed in the main loop.
-struct LoopData
+namespace urbi
 {
-  bool interactive;
-  bool fast;
-  bool network;
-  ConsoleServer* s;
-};
 
-int main_loop(LoopData& l);
-
-USDK_API int
-main (int argc, const char* argv[], bool block)
-{
-  libport::program_name = argv[0];
-  LoopData data;
-  // Input files.
-  typedef std::vector<const char*> files_type;
-  files_type files;
-  /// The IP to bind. "" means every interface.
-  std::string arg_host;
-  /// The port to use.  0 means automatic selection.
-  int arg_port = 0;
-  /// Where to write the port we use.
-  const char* arg_port_filename = 0;
-  /// The size of the stacks.
-  size_t arg_stack_size = 0;
-  /// fast mode
-  data.fast = false;
-  /// interactive mode
-  data.interactive = false;
-  /// enable network
-  data.network = true;
-
-  // Parse the command line.
-  for (int i = 1; i < argc; ++i)
+  /// Command line options needed in the main loop.
+  struct LoopData
   {
-    std::string arg = argv[i];
+    bool interactive;
+    bool fast;
+    bool network;
+    ConsoleServer* s;
+  };
 
-    if (arg == "--fast" || arg == "-f")
-      data.fast = true;
-    else if (arg == "--help" || arg == "-h")
-      usage();
-    else if (arg == "--host" || arg == "-H")
-      arg_host = argv[++i];
-    else if (arg == "--interactive" || arg == "-i")
-      data.interactive = true;
-    else if (arg == "--no-network" || arg == "-n")
-      data.network = false;
-    else if (arg == "--period" || arg == "-P")
-      (void) libport::convert_argument<int> (arg, argv[++i]);
-    else if (arg == "--port" || arg == "-p")
-      arg_port = libport::convert_argument<int> (arg, argv[++i]);
-    else if (arg == "--port-file" || arg == "-w")
-      arg_port_filename = argv[++i];
-    else if (arg == "--stack-size" || arg == "-s")
-      arg_stack_size = libport::convert_argument<size_t> (arg, argv[++i]);
-    else if (arg == "--version" || arg == "-v")
-      version();
-    else if (arg[0] == '-' && arg[1] != 0)
-      libport::invalid_option (arg);
-    else
-      // An argument: a file.
-      files.push_back(STREQ(argv[i], "-") ? "/dev/stdin" : argv[i]);
-  }
+  int main_loop(LoopData& l);
 
-  // If not defined in line, use the envvar.
-  if (!arg_stack_size
-      && getenv("URBI_STACK_SIZE"))
-    arg_stack_size = libport::convert_envvar<size_t> ("URBI_STACK_SIZE");
-
-  if (arg_stack_size)
+  USDK_API int
+  main(int argc, const char* argv[], bool block)
   {
-    // Make sure the result is a multiple of the page size.  This
-    // required at least on OSX (which unfortunately fails with errno
-    // = 0).
-    arg_stack_size *= 1024;
-    size_t pagesize = getpagesize();
-    arg_stack_size = ((arg_stack_size + pagesize - 1) / pagesize) * pagesize;
-    kernconf.default_stack_size = arg_stack_size;
-  }
+    libport::program_name = argv[0];
+    LoopData data;
+    // Input files.
+    typedef std::vector<const char*> files_type;
+    files_type files;
+    /// The IP to bind. "" means every interface.
+    std::string arg_host;
+    /// The port to use.  0 means automatic selection.
+    int arg_port = 0;
+    /// Where to write the port we use.
+    const char* arg_port_filename = 0;
+    /// The size of the stacks.
+    size_t arg_stack_size = 0;
+    /// fast mode
+    data.fast = false;
+    /// interactive mode
+    data.interactive = false;
+    /// enable network
+    data.network = true;
 
-  data.s = new ConsoleServer(data.fast);
-  ConsoleServer& s = *data.s;
-  int port = 0;
-  if (data.network && !(port=Network::createTCPServer(arg_port, arg_host)))
-  {
-    std::cerr << libport::program_name
-	      << ": cannot bind to port " << arg_port;
-    if (!arg_host.empty())
-      std::cerr << " on " << arg_host;
-    std::cerr << std::endl
-	      << libport::exit (EX_UNAVAILABLE);
-  }
+    // Parse the command line.
+    for (int i = 1; i < argc; ++i)
+    {
+      std::string arg = argv[i];
 
-  s.initialize ();
+      if (arg == "--fast" || arg == "-f")
+        data.fast = true;
+      else if (arg == "--help" || arg == "-h")
+        usage();
+      else if (arg == "--host" || arg == "-H")
+        arg_host = argv[++i];
+      else if (arg == "--interactive" || arg == "-i")
+        data.interactive = true;
+      else if (arg == "--no-network" || arg == "-n")
+        data.network = false;
+      else if (arg == "--period" || arg == "-P")
+        (void) libport::convert_argument<int> (arg, argv[++i]);
+      else if (arg == "--port" || arg == "-p")
+        arg_port = libport::convert_argument<int> (arg, argv[++i]);
+      else if (arg == "--port-file" || arg == "-w")
+        arg_port_filename = argv[++i];
+      else if (arg == "--stack-size" || arg == "-s")
+        arg_stack_size = libport::convert_argument<size_t> (arg, argv[++i]);
+      else if (arg == "--version" || arg == "-v")
+        version();
+      else if (arg[0] == '-' && arg[1] != 0)
+        libport::invalid_option (arg);
+      else
+        // An argument: a file.
+        files.push_back(STREQ(argv[i], "-") ? "/dev/stdin" : argv[i]);
+    }
 
-  // Write the port file after initialize returned; that is, after
-  // urbi.u is loaded.
-  if (arg_port_filename)
-    std::ofstream(arg_port_filename, std::ios::out) << port << std::endl;
+    // If not defined in line, use the envvar.
+    if (!arg_stack_size
+        && getenv("URBI_STACK_SIZE"))
+      arg_stack_size = libport::convert_envvar<size_t> ("URBI_STACK_SIZE");
 
-  UConnection& c = s.ghost_connection_get();
-  std::cerr << libport::program_name
-	    << ": got ghost connection" << std::endl;
+    if (arg_stack_size)
+    {
+      // Make sure the result is a multiple of the page size.  This
+      // required at least on OSX (which unfortunately fails with errno
+      // = 0).
+      arg_stack_size *= 1024;
+      size_t pagesize = getpagesize();
+      arg_stack_size = ((arg_stack_size + pagesize - 1) / pagesize) * pagesize;
+      kernconf.default_stack_size = arg_stack_size;
+    }
 
-  foreach (const char* f, files)
-    if (s.load_file(f, c.recv_queue_get ()) != USUCCESS)
+    data.s = new ConsoleServer(data.fast);
+    ConsoleServer& s = *data.s;
+    int port = 0;
+    if (data.network && !(port=Network::createTCPServer(arg_port, arg_host)))
+    {
       std::cerr << libport::program_name
-		<< ": failed to process " << f << std::endl
-		<< libport::exit(EX_NOINPUT);
+                << ": cannot bind to port " << arg_port;
+      if (!arg_host.empty())
+        std::cerr << " on " << arg_host;
+      std::cerr << std::endl
+                << libport::exit (EX_UNAVAILABLE);
+    }
 
-  c.new_data_added_get() = true;
+    s.initialize();
 
-  std::cerr << libport::program_name << ": going to work..." << std::endl;
-  if (block)
-    return main_loop(data);
-  else
-    libport::startThread(new boost::function0<void>(
-      boost::bind(&main_loop, data)));
-  return 0;
+    // Write the port file after initialize returned; that is, after
+    // urbi.u is loaded.
+    if (arg_port_filename)
+      std::ofstream(arg_port_filename, std::ios::out) << port << std::endl;
+
+    UConnection& c = s.ghost_connection_get();
+    std::cerr << libport::program_name
+              << ": got ghost connection" << std::endl;
+
+    foreach (const char* f, files)
+      if (s.load_file(f, c.recv_queue_get ()) != USUCCESS)
+        std::cerr << libport::program_name
+                  << ": failed to process " << f << std::endl
+                  << libport::exit(EX_NOINPUT);
+
+    c.new_data_added_get() = true;
+
+    std::cerr << libport::program_name << ": going to work..." << std::endl;
+    if (block)
+      return main_loop(data);
+    else
+      libport::startThread(new boost::function0<void>(
+                             boost::bind(&main_loop, data)));
+    return 0;
   }
 
-int main_loop(LoopData& data)
-{
-  ConsoleServer& s = *data.s;
-  libport::utime_t next_time = 0;
-  while (true)
+  int
+  main_loop(LoopData& data)
   {
-    if (data.interactive)
+    ConsoleServer& s = *data.s;
+    libport::utime_t next_time = 0;
+    while (true)
     {
-      std::string input;
-      try
-      {
-	input = libport::read_stdin();
-      }
-      catch (libport::exception::Exception e)
-      {
-	std::cerr << e.what() << std::endl;
-	data.interactive = false;
-      }
-      if (!input.empty())
-	s.ghost_connection_get().received(input.c_str(), input.length());
-    }
-    libport::utime_t select_time = 0;
-    if (!data.fast)
-    {
-      libport::utime_t ctime = libport::utime();
-      if (ctime < next_time)
-	select_time = next_time - ctime;
       if (data.interactive)
-	select_time = std::min(100000LL, select_time);
+      {
+        std::string input;
+        try
+        {
+          input = libport::read_stdin();
+        }
+        catch (libport::exception::Exception e)
+        {
+          std::cerr << e.what() << std::endl;
+          data.interactive = false;
+        }
+        if (!input.empty())
+          s.ghost_connection_get().received(input.c_str(), input.length());
+      }
+      libport::utime_t select_time = 0;
+      if (!data.fast)
+      {
+        libport::utime_t ctime = libport::utime();
+        if (ctime < next_time)
+          select_time = next_time - ctime;
+        if (data.interactive)
+          select_time = std::min(100000LL, select_time);
+      }
+      if (data.network)
+        Network::selectAndProcess(select_time);
+
+      next_time = s.work ();
+      if (next_time == scheduler::SCHED_EXIT)
+        break;
+      s.ctime = std::max (next_time, s.ctime + 1000L);
     }
-    if (data.network)
-      Network::selectAndProcess(select_time);
 
-    next_time = s.work ();
-    if (next_time == scheduler::SCHED_EXIT)
-      break;
-    s.ctime = std::max (next_time, s.ctime + 1000L);
+    return EX_OK;
   }
-
-  return EX_OK;
-}
 
 }
