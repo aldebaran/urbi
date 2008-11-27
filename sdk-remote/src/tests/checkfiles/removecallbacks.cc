5b48afb674cd41cbbf6b924b1c5c5a2b77370584
Akim Demaille <demaille@gostai.com>
2008-11-27 11:24:57 +0100

    Make tests outcome easier to check.
    	* src/tests/checkfiles/removecallbacks.cc: Use different numbers.

--- sdk-remote/src/tests/checkfiles/removecallbacks.cc
+++ sdk-remote/src/tests/checkfiles/removecallbacks.cc
@@ -9,28 +9,28 @@ SEND("output << 1;");
 //= D output 1
 dumpSem--;
 dumpSem--;
-SEND("error << 1;");   //ping
-//= D error 1
+SEND("error << 2;");   //ping
+//= D error 2
 dumpSem--;
 client.deleteCallback(i1);
-SEND("output << 1;");
-//= D output 1
-SEND("error << 1;");   //ping
-//= D error 1
+SEND("output << 3;");
+//= D output 3
+SEND("error << 4;");   //ping
+//= D error 4
 dumpSem--;
 dumpSem--;
 client.deleteCallback(i2);
-SEND("output << 1;"); //no callback left, not displayed
-SEND("error << 1;");   //ping
-//= D error 1
+SEND("output << 5;"); //no callback left, not displayed
+SEND("error << 6;");   //ping
+//= D error 6
 dumpSem--;
 client.setCallback(&removeOnZero, "output");
-SEND("output << 1;");
-//= D output 1
+SEND("output << 7;");
+//= D output 7
 SEND("output << 0;");
 //= D output 0
-SEND("output << 1;");
-SEND("error << 1;");   //ping
-//= D error 1
+SEND("output << 9;");
+SEND("error << 10;");   //ping
+//= D error 10
 sleep(5);
 END_TEST
