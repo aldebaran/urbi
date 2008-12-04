88a35ec137abca7b1274de20770972c125d10719
Samuel Tardieu <sam@rfc1149.net>
2008-12-04 15:13:09 +0100

    Add proper export form for GCC.
    	* include/urbi/export.hh: Properly export for GCC.

--- sdk-remote/include/urbi/export.hh
+++ sdk-remote/include/urbi/export.hh
@@ -1,22 +1,16 @@
 #ifndef URBI_EXPORT_HH
-# define URBI_EXPORT_HH
+# define URBI_SDK_EXPORT_HH
 
 # include <libport/detect-win32.h>
 
-# ifdef BUILDING_URBI_SDK
-#  ifdef WIN32
+# ifdef WIN32
+#  ifdef BUILDING_URBI_SDK
 #   define URBI_SDK_API __declspec(dllexport)
 #  else
-#    define URBI_SDK_API __attribute__((visibility("default")))
-#  endif
-# endif
-
-# ifndef URBI_SDK_API
-#  ifdef WIN32
 #   define URBI_SDK_API __declspec(dllimport)
-#  else
-#   define URBI_SDK_API
 #  endif
+# else
+#  define URBI_SDK_API __attribute__((visibility("default")))
 # endif
 
-#endif
+#endif // URBI_EXPORT_HH
