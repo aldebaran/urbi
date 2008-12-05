fd4364287d9be5ba517c15ca33a1f51929495d9c
Akim Demaille <demaille@gostai.com>
2008-12-05 15:14:34 +0100

    Space changes.

--- sdk-remote/src/libuco/utable.cc
+++ sdk-remote/src/libuco/utable.cc
@@ -21,13 +21,13 @@ Copyright (c) 2004, 2005, 2006, 2007, 2008 Jean-Christophe Baillie.
 #include <urbi/utable.hh>
 #include <urbi/ucallbacks.hh>
 
-  // Lists and hashtables used.
-  STATIC_INSTANCE_NS_EX(UTable, accessmap, urbi, URBI_SDK_API);
-  STATIC_INSTANCE_NS_EX(UTable, eventendmap, urbi, URBI_SDK_API);
-  STATIC_INSTANCE_NS_EX(UTable, eventmap, urbi, URBI_SDK_API);
-  STATIC_INSTANCE_NS_EX(UTable, functionmap, urbi, URBI_SDK_API);
-  STATIC_INSTANCE_NS_EX(UTable, monitormap, urbi, URBI_SDK_API);
-  STATIC_INSTANCE_NS_EX(UVarTable, varmap, urbi, URBI_SDK_API);
+// Lists and hashtables used.
+STATIC_INSTANCE_NS_EX(UTable, accessmap, urbi, URBI_SDK_API);
+STATIC_INSTANCE_NS_EX(UTable, eventendmap, urbi, URBI_SDK_API);
+STATIC_INSTANCE_NS_EX(UTable, eventmap, urbi, URBI_SDK_API);
+STATIC_INSTANCE_NS_EX(UTable, functionmap, urbi, URBI_SDK_API);
+STATIC_INSTANCE_NS_EX(UTable, monitormap, urbi, URBI_SDK_API);
+STATIC_INSTANCE_NS_EX(UVarTable, varmap, urbi, URBI_SDK_API);
 
 namespace urbi
 {
