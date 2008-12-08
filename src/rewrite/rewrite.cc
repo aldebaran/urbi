86491aff7f5c85d1ff23b8bbc0b715826142403d
Samuel Tardieu <sam@rfc1149.net>
2008-12-08 13:29:02 +0100

    Make inner call instead of duplicating the code.
    	* src/rewrite/rewrite.cc (rewrite(ast::rConstNary)): Put
    	appropriate type casts around an inner call rather than
    	duplicate the code.

--- src/rewrite/rewrite.cc
+++ src/rewrite/rewrite.cc
@@ -5,21 +5,10 @@
 
 namespace rewrite
 {
-  // FIXME: template-factor those two
   ast::rNary
   rewrite(ast::rConstNary nary)
   {
-    Desugarer desugar;
-    Rescoper rescope;
-    ast::rAst res;
-
-    desugar(nary.get());
-    res = desugar.result_get();
-
-    rescope(res.get());
-    res = rescope.result_get();
-
-    return res.unsafe_cast<ast::Nary>();
+    return rewrite(ast::rConstExp(nary)).unsafe_cast<ast::Nary>();
   }
 
   ast::rExp
