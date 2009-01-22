0a2e5427417766d30f9aca4138070c297f4f3a97
Akim Demaille <demaille@gostai.com>
2009-01-22 14:23:18 +0100

    Convert the documentation into a genuine chk test suite.
    tex2chk converts the TeX files into a chk hierarchy.
    
    	* doc/tex2chk: New.
    	* doc/Makefile.am: Include the following file.
    	* doc/tests/local.mk: New, most of it is extracted from...
    	* doc/specs/local.mk: here.
    	* doc/check.py: Remove.
    	* doc/specs/URBI.INI: Rename as...
    	* doc/tests/test.u: this.
    	Adjust.
    	* .gitignore: Adjust.

--- doc/specs/local.mk
+++ doc/specs/local.mk
@@ -16,27 +16,27 @@ specs/.stamp:
 	-mkdir specs
 	test -f $@ || touch $@
 
-## ------- ##
-## Check.  ##
-## ------- ##
-
-include $(top_srcdir)/build-aux/check.mk
-
-TESTS = $(urbi_specs_sources)
-# This is not exact, but otherwise it is too long.
-# LAZY_TEST_SUITE = 1
-TEST_LOGS = $(TESTS:.tex=.log)
-# FIXME: Assigned to QH.
-TFAIL_TESTS = specs/lang.tex
-
-URBI_CONSOLE = $(top_builddir)/tests/bin/urbi-console
-
-# Set URBI_PATH to find URBI.INI.
-TESTS_ENVIRONMENT +=				\
-  URBI_CONSOLE=$(URBI_CONSOLE)			\
-  URBI_PATH=$(abs_srcdir)/specs:$$URBI_PATH
-
-TEST_LOGS: $(srcdir)/specs/URBI.INI $(srcdir)/check.py
-
-%.log: %.tex
-	@$(am__check_pre) $(srcdir)/check.py $${dir}$< $(am__check_post)
+# ## ------- ##
+# ## Check.  ##
+# ## ------- ##
+#
+# include $(top_srcdir)/build-aux/check.mk
+#
+# TESTS = $(urbi_specs_sources)
+# # This is not exact, but otherwise it is too long.
+# # LAZY_TEST_SUITE = 1
+# TEST_LOGS = $(TESTS:.tex=.log)
+# # FIXME: Assigned to QH.
+# TFAIL_TESTS = specs/lang.tex
+#
+# URBI_CONSOLE = $(top_builddir)/tests/bin/urbi-console
+#
+# # Set URBI_PATH to find URBI.INI.
+# TESTS_ENVIRONMENT +=				\
+#   URBI_CONSOLE=$(URBI_CONSOLE)			\
+#   URBI_PATH=$(abs_srcdir)/specs:$$URBI_PATH
+#
+# TEST_LOGS: $(srcdir)/specs/URBI.INI $(srcdir)/check.py
+#
+# %.log: %.tex
+# 	@$(am__check_pre) $(srcdir)/check.py $${dir}$< $(am__check_post)
---
+++ doc/tests/local.mk
@@ -0,0 +1,39 @@
+## ------- ##
+## Check.  ##
+## ------- ##
+
+TESTS =
+-include $(wildcard $(srcdir)/tests/*/*/local.mk)
+include $(top_srcdir)/build-aux/check.mk
+
+
+# This is not exact, but otherwise it is too long.
+# LAZY_TEST_SUITE = 1
+TEST_LOGS = $(TESTS:.chk=.log)
+# FIXME: Assigned to QH.
+TFAIL_TESTS = specs/lang.tex
+
+# Generating the test files.
+noinst_DATA = $(patsubst %.tex,$(srcdir)/tests/%/local.mk,$(call ls_files,*.tex))
+$(srcdir)/tests/%/local.mk: %.tex $(srcdir)/tex2chk
+	srcdir=$(srcdir) $(srcdir)/tex2chk $<
+
+
+URBI_CONSOLE = $(top_builddir)/tests/bin/urbi-console
+
+# Set URBI_PATH to find URBI.INI.
+TESTS_ENVIRONMENT +=				\
+  URBI_CONSOLE=$(URBI_CONSOLE)			\
+  URBI_PATH=$(abs_srcdir)/specs:$$URBI_PATH	\
+  srcdir=$(srcdir)/tests\
+  PATH=$(abs_top_builddir)/tests/bin:$$PATH
+
+UCONSOLE_CHECK = $(top_builddir)/tests/bin/uconsole-check $(UCONSOLE_CHECK_FLAGS)
+UCONSOLE_CHECKFLAGS = -k2 --no-locations
+
+%.log: %.chk
+	@$(am__check_pre) $(UCONSOLE_CHECK) $(UCONSOLE_CHECKFLAGS) $${dir}$< $(am__check_post)
+
+debug:
+	echo $(TESTS)
+	echo $(TEST_LOGS)
