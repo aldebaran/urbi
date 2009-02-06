5157834a30cc193ab53070a26f39b8387eab9b24
Matthieu Nottale <nottale@gostai.com>
2009-02-06 11:30:18 +0100

    Mark main with a default visibility.
    Some arch do not like a hidden 'main'. Do not use existing macros
    to minimize dependency on this special file.
    
    	* src/urbi-console.cc (main): Declare with default visibility.

--- src/urbi-console.cc
+++ src/urbi-console.cc
@@ -2,9 +2,14 @@
 // So we define one here.
 
 #include <urbi/umain.hh>
+#include <libport/detect-win32.h>
 
+#ifndef WIN32
+ __attribute__((visibility("default")))
+#endif
 int
 main(int argc, const char* argv[])
 {
   return urbi::main(argc, argv);
 }
+
