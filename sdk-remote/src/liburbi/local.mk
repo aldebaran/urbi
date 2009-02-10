ea5fb323523c18ffb372dd295c3a944dddc4ce9f
Akim Demaille <demaille@gostai.com>
2009-02-10 15:09:05 +0100

    Register the dependencies on libport.
    	* Makefile.am: Formatting changes.
    	* src/Makefile.am: Remove useless -I.
    	* src/liburbi/local.mk: Do it here.
    	Move src/version.hh into src/liburbi/version.hh.
    	* src/liburbi/package-info.cc (package_info): Subscribe to
    	libport's package_info.

--- sdk-remote/src/liburbi/local.mk
+++ sdk-remote/src/liburbi/local.mk
@@ -1,6 +1,11 @@
-AM_CPPFLAGS += $(BOOST_CPPFLAGS)
+## ------------ ##
+## version.hh.  ##
+## ------------ ##
 
-dist_umain_DATA = liburbi/umain.cc
+REVISION_FILE = liburbi/version.hh
+include $(top_srcdir)/build-aux/revision.mk
+
+nodist_env_DATA = liburbi/version.hh
 
 ## ------------ ##
 ## liburbi.la.  ##
@@ -15,8 +20,19 @@ liburbi_liburbi_la_SOURCES =			\
   liburbi/uconversion.cc			\
   liburbi/umessage.cc				\
   liburbi/usyncclient.cc
+
 liburbi_liburbi_la_CPPFLAGS =			\
   $(AM_CPPFLAGS)				\
   -DBUILDING_URBI_SDK
+# Find version.hh.
+liburbi_liburbi_la_CPPFLAGS += -I.
+
 liburbi_liburbi_la_LIBADD = $(LIBADD) $(top_builddir)/jpeg-6b/libjpeg.la
 liburbi_liburbi_la_LDFLAGS = -avoid-version -no-undefined
+
+
+## ---------- ##
+## umain.cc.  ##
+## ---------- ##
+
+dist_umain_DATA = liburbi/umain.cc
--- sdk-remote/src/liburbi/package-info.cc
+++ sdk-remote/src/liburbi/package-info.cc
@@ -1,13 +1,21 @@
 #include <sdk/config.h>
-#include <version.hh>
+#include <liburbi/version.hh>
 #include <urbi/package-info.hh>
+
+#include <libport/version.hh>
 
 namespace urbi
 {
   const libport::PackageInfo&
-  package_info ()
+  package_info()
   {
     LIBPORT_PACKAGE_INFO_STATIC_VAR(pi);
+    static bool first = true;
+    if (first)
+    {
+      first = false;
+      pi.dependency_add(libport::package_info());
+    }
     return pi;
   }
 
