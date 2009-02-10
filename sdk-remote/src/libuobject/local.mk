b9dffca8bba2eb4956b67e9ef6331726632198e4
Akim Demaille <demaille@gostai.com>
2009-02-10 15:57:09 +0100

    Comment changes.

--- sdk-remote/src/libuobject/local.mk
+++ sdk-remote/src/libuobject/local.mk
@@ -15,13 +15,3 @@ libuobject_libuobject_la_CPPFLAGS =		\
   -DBUILDING_URBI_SDK
 libuobject_libuobject_la_LIBADD = $(LIBADD) libuco/libuco.la liburbi/liburbi.la
 libuobject_libuobject_la_LDFLAGS = -avoid-version -no-undefined
-
-# We might need to have two versions of libuobject: one to link
-# against, and the other one to dlopen.  It seems that we can live
-# with a single one for the moment, but who knows...
-#
-# env_LTLIBRARIES += libuobject/uobject.la
-# libuobject_uobject_la_SOURCES  = $(libuobject_libuobject_la_SOURCES)
-# libuobject_uobject_la_CPPFLAGS = $(libuobject_libuobject_la_CPPFLAGS)
-# libuobject_uobject_la_LIBADD   = $(libuobject_libuobject_la_LIBADD)
-# libuobject_uobject_la_LDFLAGS  = $(libuobject_libuobject_la_LDFLAGS) -module
