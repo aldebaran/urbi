ff85b1284494ecf1a598dc054f9dbd17cfcd62fc
Akim Demaille <demaille@gostai.com>
2009-03-10 11:11:37 +0100

    Have urbi-console bounce to urbi-launch.
    The point is to factor the machinery to locate the various DLLs.  Besides,
    it provides a simplification for Windows.  On this architecture, it is
    hard to run a binary linked against a shlib which is not in the directory
    of the executable.  But we do not want to copy libuobject (which is
    what urbi-console actually is) in bindir because we have two (one for
    remote component, another for "plugins").  Since urbi-launch dlopens these
    shlibs, using a qualified path, we do not have the same problems.
    
    Therefore, we exchange shlib lookup by ld.so by a dlopen that we control.
    
    	* src/urbi-console.cc: Rewrite to exec urbi-console.
    	* src/Makefile.am: Do no link urbi-console with libuobject.

--- src/urbi-console.cc
+++ src/urbi-console.cc
@@ -1,16 +1,53 @@
 // Microsoft compiler does not allow main to be in a library.
 // So we define one here.
 
-#include <urbi/umain.hh>
-#include <libport/detect-win32.h>
-#include <libport/program-name.hh>
+#include <cerrno>
+#include <iostream>
+#include <vector>
 
-#ifndef WIN32
- __attribute__((visibility("default")))
-#endif
+#include <config.h>
+
+#include <libport/cstdlib>
+
+#include <libport/compiler.hh>
+#include <libport/containers.hh>
+#include <libport/detect-win32.h>
+#include <libport/foreach.hh>
+#include <libport/path.hh>
+#include <libport/program-name.hh>
+#include <libport/sysexits.hh>
+#include <libport/unistd.h>
+
+ATTRIBUTE_NORETURN
+void
+exec(std::vector<const char*> args)
+{
+  args.push_back(0);
+  char* const* argv = const_cast<char* const*>(&args[0]);
+  execv(argv[0], argv);
+
+  std::cerr << libport::program_name()
+            << ": failed to invoke " << args[0]
+            << ": " << strerror(errno)
+            << std::endl
+            << libport::exit(EX_OSFILE);
+}
+
 int
 main(int argc, char* argv[])
 {
   libport::program_initialize(argc, argv);
-  return urbi::main(argc, const_cast<const char**>(argv), true, true);
+  // Installation prefix.
+  std::string urbi_root = libport::xgetenv("URBI_ROOT", URBI_ROOT);
+
+  const char* URBI_LAUNCH = getenv("URBI_LAUNCH");
+  std::string urbi_launch = (URBI_LAUNCH
+                             ? URBI_LAUNCH
+                             : urbi_root + "/bin/urbi-launch" EXEEXT);
+
+  std::vector<const char*> args;
+  args.reserve(3 + argc + 1);
+  args << urbi_launch.c_str() << "--start" << "--";
+  args.insert(args.end(), argv + 1, argv + argc);
+  exec(args);
 }
