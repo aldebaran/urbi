b90ce9dde6929a60b2aa9b5a12425bd6fedd3e38
Akim Demaille <demaille@gostai.com>
2009-05-22 15:35:10 +0200

    build: fix built-sources.
    	* src/object/local.mk (dist_libuobject_la_SOURCES): Remove
    	object/executable.hh which is generated.

--- src/object/local.mk
+++ src/object/local.mk
@@ -94,7 +94,6 @@ dist_libuobject_la_SOURCES +=			\
   object/dictionary.hh				\
   object/directory.cc				\
   object/directory.hh				\
-  object/executable.hh				\
   object/executable.cc				\
   object/file.cc				\
   object/file.hh				\
