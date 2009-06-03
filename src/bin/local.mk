ccf4dbf7ef856b9ea26df6ceacff8317c81ad5ac
mefyl <hocquet@gostai.com>
2009-06-03 16:54:34 +0200

    Install ast manipulation programs.
    	* src/bin/local.mk: Here.

--- src/bin/local.mk
+++ src/bin/local.mk
@@ -4,19 +4,21 @@
 
 EXTRA_PROGRAMS = bin/ast-dump bin/urbi-parse bin/urbi-pp bin/urbi-compile
 
+if BUILD_PROGRAMS
+if !WIN32
+noinst_PROGRAMS = bin/ast-dump
+bin_PROGRAMS += bin/urbi-parse bin/urbi-pp bin/urbi-compile
+endif !WIN32
+endif BUILD_PROGRAMS
+
+# ast-dump.
 dist_bin_ast_dump_SOURCES = bin/ast-dump.cc
 bin_ast_dump_CPPFLAGS = $(libuobject_la_CPPFLAGS)
 bin_ast_dump_LDADD    =							\
   $(top_builddir)/sdk-remote/libport/lib/serialize/libserialize.la	\
   libuobject.la
 
-dist_bin_ast_dump_SOURCES =			\
-  bin/ast-dump.cc
-bin_ast_dump_CPPFLAGS = $(libuobject_la_CPPFLAGS)
-bin_ast_dump_LDADD    =					\
-  ../sdk-remote/libport/lib/serialize/libserialize.la	\
-  libuobject.la
-
+# urbi-parse.
 dist_bin_urbi_parse_SOURCES =			\
   bin/urbi-parse.cc
 bin_urbi_parse_CPPFLAGS = $(libuobject_la_CPPFLAGS)
@@ -24,6 +26,7 @@ bin_urbi_parse_LDADD    =						\
   $(top_builddir)/sdk-remote/libport/lib/serialize/libserialize.la	\
   libuobject.la
 
+# urbi-pp.
 dist_bin_urbi_pp_SOURCES =			\
   bin/urbi-pp.cc
 bin_urbi_pp_CPPFLAGS = $(libuobject_la_CPPFLAGS)
@@ -31,6 +34,7 @@ bin_urbi_pp_LDADD    =							\
   $(top_builddir)/sdk-remote/libport/lib/serialize/libserialize.la	\
   libuobject.la
 
+# urbi-compile.
 dist_bin_urbi_compile_SOURCES =			\
   bin/urbi-compile.cc
 bin_urbi_compile_CPPFLAGS = $(libuobject_la_CPPFLAGS)
@@ -38,11 +42,6 @@ bin_urbi_compile_LDADD    =						\
   $(top_builddir)/sdk-remote/libport/lib/serialize/libserialize.la	\
   libuobject.la
 
-if BUILD_PROGRAMS
-if !WIN32
-noinst_PROGRAMS = bin/ast-dump bin/urbi-parse bin/urbi-pp bin/urbi-compile
-endif !WIN32
-endif BUILD_PROGRAMS
 
 ## ------ ##
 ## urbi.  ##
