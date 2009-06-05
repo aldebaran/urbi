27ebd9a3ce4c5188fbb6690a99a116266a668aee
Akim Demaille <demaille@gostai.com>
2009-06-05 14:32:05 +0200

    parser: avoid unary naries in scopes.
    	* src/parser/ast-factory.cc (ast_scope): Use ast_strip.

--- src/parser/ast-factory.cc
+++ src/parser/ast-factory.cc
@@ -321,9 +321,9 @@ namespace parser
     if (ast::rScope res = e.unsafe_cast<ast::Scope>())
       return res;
     else if (target)
-      return new ast::Do(l, e, target);
+      return new ast::Do(l, ast_strip(e), ast_strip(target));
     else
-      return new ast::Scope(l, e);
+      return new ast::Scope(l, ast_strip(e));
   }
 
   ast::rScope
