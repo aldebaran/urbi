2a01a29b2e073a4c2e265e72021ed73a39f24dfd
Akim Demaille <demaille@gostai.com>
2009-06-05 12:57:40 +0200

    enable "make bin/ast-dump".
    	* src/bin/local.mk (EXTRA_PROGRAMS): New.
    	Always declare ast-dump.

--- src/bin/local.mk
+++ src/bin/local.mk
@@ -2,15 +2,16 @@
 ## ast-dump.  ##
 ## ---------- ##
 
-if BUILD_PROGRAMS
-if !WIN32
-noinst_PROGRAMS = bin/ast-dump
+EXTRA_PROGRAMS = bin/ast-dump
 dist_bin_ast_dump_SOURCES =			\
   bin/ast-dump.cc
 bin_ast_dump_CPPFLAGS = $(libuobject_la_CPPFLAGS)
 bin_ast_dump_LDADD    =				\
   libuobject.la
 
+if BUILD_PROGRAMS
+if !WIN32
+noinst_PROGRAMS = bin/ast-dump
 endif !WIN32
 endif BUILD_PROGRAMS
 
