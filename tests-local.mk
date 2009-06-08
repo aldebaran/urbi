19184774a478dad1cde00a1035caee0e8bc32e96
Akim Demaille <demaille@gostai.com>
2009-06-08 09:53:55 +0200

    build: pacify Automake.
    	* tests-local.mk: Don't play dirty tricks with \-continued comments.

--- tests-local.mk
+++ tests-local.mk
@@ -43,25 +43,25 @@ TFAIL_TESTS +=					\
 # In fact, the above list has been removed to gain some time
 # when running tests. The following tests should be examined
 # and reintroduced in 1.x as needed.
-TO_CHECK_TESTS =				\
-# +begin, +end                                  \
-1.-/begin-end-report.chk			\
-# chan << echo(foo) goes in chan                \
-1.-/channels-tags.chk				\
-# +connection                                   \
-1.-/connection-delete.chk			\
-# emit foo                                      \
-1.-/events.chk					\
-1.-/events-emit-then-return-in-function.chk	\
-1.-/every-emit.chk				\
-# []-access to list and hash                    \
-1.-/hash-and-list.chk				\
-# what should we do with very large literals?	\
-1.-/invalid-number.chk				\
-# events                                        \
-1.-/lazy-test-eval.chk				\
-1.-/loadwav.chk					\
-1.-/object-events.chk
+# TO_CHECK_TESTS =
+# # +begin, +end
+# 1.-/begin-end-report.chk
+# # chan << echo(foo) goes in chan
+# 1.-/channels-tags.chk
+# # +connection
+# 1.-/connection-delete.chk
+# # emit foo
+# 1.-/events.chk
+# 1.-/events-emit-then-return-in-function.chk
+# 1.-/every-emit.chk
+# # []-access to list and hash
+# 1.-/hash-and-list.chk
+# # what should we do with very large literals?
+# 1.-/invalid-number.chk
+# # events
+# 1.-/lazy-test-eval.chk
+# 1.-/loadwav.chk
+# 1.-/object-events.chk
 
 # Uobject tests that we fail because features are not implemented
 XFAIL_TESTS +=					\
