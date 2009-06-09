dc8df36d8220b7565b9c8eb790269a9d19ec1fa5
Akim Demaille <demaille@gostai.com>
2009-06-09 14:25:25 +0200

    USound: sort.
    	* src/libuco/uvalue-common.cc (USound::format_string)
    	(USound::operator std::string, operator>>): Move to...
    	* src/libuco/usound.cc: here.

--- sdk-remote/src/libuco/uvalue-common.cc
+++ sdk-remote/src/libuco/uvalue-common.cc
@@ -297,61 +297,6 @@ namespace urbi
   }
 
 
-  /*---------.
-  | USound.  |
-  `---------*/
-
-  const char* USound::format_string() const
-  {
-    switch (soundFormat)
-    {
-      case SOUND_RAW:
-	return "raw";
-      case SOUND_WAV:
-	return "wav";
-      case SOUND_MP3:
-	return "mp3";
-      case SOUND_OGG:
-	return "ogg";
-      case SOUND_UNKNOWN:
-	return "unknown format";
-    }
-    // To pacify "warning: control reaches end of non-void function".
-    // FIXME: This should not abort. UImage should be initialized with IMAGE_UKNOWN.
-    //        This is not done because data is stored in an union in UBinary and
-    //        union members cannot have constructors.
-    return "unknown format";
-  }
-
-  USound::operator std::string() const
-  {
-    std::ostringstream o;
-    o << "sound(format: " << format_string() << ", "
-      << "size: " << size << ", "
-      << "channels: " << channels << ", "
-      << "rate: " << rate << ", "
-      << "sample size: " << sampleSize << ", "
-      << "sample format: ";
-    switch (sampleFormat)
-    {
-    case SAMPLE_SIGNED:   o << "signed";  break;
-    case SAMPLE_UNSIGNED: o << "unsigned"; break;
-    default:              o << "unknown[" << (int)sampleFormat << "]";
-    }
-    o << ")";
-    return o.str();
-  }
-
-  std::istream&
-  operator>> (std::istream& is, USoundSampleFormat& f)
-  {
-    int v = 0;
-    is >> v;
-    f = USoundSampleFormat(v);
-    return is;
-  }
-
-
   /*----------.
   | UBinary.  |
   `----------*/
