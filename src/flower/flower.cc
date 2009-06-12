b0252c3f147fa03f23f37e8c3d42050de62960cd
Akim Demaille <demaille@gostai.com>
2009-06-12 18:03:50 +0200

    build: fix.
    Fix da3bc516bdfc03bbfa44753b248b1efcb476ebaa.
    
    	* src/flower/flower.cc: here.

--- src/flower/flower.cc
+++ src/flower/flower.cc
@@ -138,10 +138,7 @@ namespace flower
     }
     each % c;
 
-    result_ = exp(each);
-    if (has_break_)
-      result_ = brk(result_);
-
+    result_ = has_break_ ? brk(exp(each)) : exp(each);
     result_->original_set(code);
   }
 
