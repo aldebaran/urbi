e5aa1d750c95b34545ea99248f28d506e378e02b
Simon Nivault <nivault@gostai.com>
2009-07-06 15:31:09 +0200

    Urbi wrapper supports --debug.
    	* src/bin/urbi.cc: here.

--- src/bin/urbi.cc
+++ src/bin/urbi.cc
@@ -4,6 +4,7 @@
 
 #include <libport/cstdlib>
 
+#include <libport/debug.hh>
 #include <libport/containers.hh>
 #include <libport/detect-win32.h>
 #include <libport/program-name.hh>
@@ -19,7 +20,14 @@ main(int argc, char* argv[])
     libport::xgetenv("URBI_LAUNCH", urbi_root + "/bin/urbi-launch" EXEEXT);
   std::vector<const char*> args;
   args.reserve(3 + argc + 1);
-  args << urbi_launch.c_str() << "--start" << "--";
+  libport::OptionParser opt_parser;
+  opt_parser << libport::opts::debug;
+  opt_parser(libport::program_arguments());
+  args << urbi_launch.c_str()
+       << "--start";
+  if (libport::opts::debug.filled())
+    args << "--debug" << libport::opts::debug.value().c_str();
+  args << "--";
   args.insert(args.end(), argv + 1, argv + argc);
   libport::exec(args);
 }
