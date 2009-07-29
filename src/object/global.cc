5e973e28537e9d5cc4de45f24df8d9874777eba9
Simon Nivault <nivault@gostai.com>
2009-07-29 13:18:21 +0200

    add Global.format.
            Take a string and a FormatInfo and prints the string taking
            width, padding and alignment in account.
    
    	* src/object/global.cc,
    	* src/object/global.hh: here.

--- src/object/global.cc
+++ src/object/global.cc
@@ -3,6 +3,8 @@
  ** \brief Creation of the URBI object global.
  */
 
+#include <object/format-info.hh>
+
 #include <object/global.hh>
 #include <object/object.hh>
 
@@ -14,9 +16,28 @@ namespace object
   | Global primitives.  |
   `--------------------*/
 
+  std::string
+  global_format(const rObject, const std::string& str, rFormatInfo finfo)
+  {
+    int padsize;
+    if ((padsize = finfo->width_get() - str.size()) <= 0)
+      return str;
+
+    std::string res(padsize, finfo->pad_get());
+    if (finfo->alignment_get() == FormatInfo::Align::LEFT)
+      return std::string(str).append(res);
+    else if (finfo->alignment_get() == FormatInfo::Align::RIGHT)
+      return res.append(str);
+    else
+      return res.insert((res.size() + 1) / 2, str);
+  }
+
   void
   global_class_initialize()
   {
+    global_class->slot_set(SYMBOL(format),
+                           object::make_primitive(&global_format),
+                           true);
   }
 
 }; // namespace object
--- src/object/global.hh
+++ src/object/global.hh
@@ -10,12 +10,16 @@
 
 # include <object/fwd.hh>
 # include <object/object.hh>
+# include <object/symbols.hh>
 # include <urbi/export.hh>
 
 namespace object
 {
   /// The prototype for Global objects.
   extern URBI_SDK_API rObject global_class;
+
+  /// Default formatting function for objects.
+  std::string global_format(const rObject, const std::string& str, rFormatInfo finfo);
 
   /// Initialize the Global class.
   void global_class_initialize ();
