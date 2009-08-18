a3f4a7d790bd254e84cced31ba83f7b7c18deacb
Akim Demaille <demaille@gostai.com>
2009-08-18 10:21:09 +0200

    ast: recreate missing files when needed.
    	* dev/ast-gen.mk (AST_REGENERATE_IF_NEEDED): New.
    	Use it to ensure that file mistakenly removed are recreated.

--- dev/ast-gen.mk
+++ dev/ast-gen.mk
@@ -26,31 +26,47 @@
 
 ## We use GNU Make extensions.
 AUTOMAKE_OPTIONS += -Wno-portability
+
+## Generate the stamp file, i.e., run the generator.
 $(ast_srcdir)/%.stamp: $(gen_dir)/ast-%-gen $(ast_gen_deps)
 	@rm -f $@ $@.tmp
 	@touch $@.tmp
 	$(gen_dir)/ast-$*-gen $(ast_srcdir) < $(ast_srcdir)/ast.yml
 	@mv -f $@.tmp $@
 
+## If a generated file is missing, rerun the generation by removing
+## the stamp file.
+AST_REGENERATE_IF_NEEDED =			\
+  if test ! -f $@; then				\
+    rm -f $<;					\
+    $(MAKE) $(AM_MAKEFLAGS) $<;			\
+  fi
+
+
 ## ast-nodes.mk
 EXTRA_DIST += $(ast_basedir)nodes-mk.stamp
 $(ast_srcdir)/ast-nodes.mk: $(ast_srcdir)/nodes-mk.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## all.hh.
 EXTRA_DIST += $(ast_basedir)all.stamp
 $(ast_basedir)all.hh: $(ast_srcdir)/all.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## cloner.hh etc.
 EXTRA_DIST += $(ast_basedir)cloner.stamp
 $(ast_basedir)cloner.hh $(ast_basedir)cloner.cc: $(ast_srcdir)/cloner.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## transformer.hh etc.
 EXTRA_DIST += $(ast_basedir)transformer.stamp
 $(ast_basedir)transformer.hh $(ast_basedir)transformer.cc: $(ast_srcdir)/transformer.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## fwd.hh.
 EXTRA_DIST += $(ast_basedir)fwd.stamp
 $(ast_basedir)fwd.hh: $(ast_srcdir)/fwd.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## ignores.
 EXTRA_DIST += $(ast_basedir)ignores $(ast_basedir)ignores.stamp
@@ -65,29 +81,36 @@ $(ast_srcdir)/ignores.stamp: $(gen_dir)/ast-ignores-gen $(ast_gen_deps)
         fi
 	@mv -f $@.tmp $@
 $(ast_basedir)ignores: $(ast_srcdir)/ignores.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## AST itself.
 EXTRA_DIST += $(ast_basedir)nodes.stamp
 $(AST_NODES): $(ast_srcdir)/nodes.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## ast.dot
 EXTRA_DIST += $(ast_basedir)graph.stamp
 $(ast_basedir)ast.dot: $(ast_srcdir)/graph.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## DefaultVisitor
 EXTRA_DIST += $(ast_basedir)default-visitor.stamp
 $(ast_basedir)default-visitor.hh $(ast_basedir)default-visitor.hxx: $(ast_srcdir)/default-visitor.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## Visitor.
 EXTRA_DIST += $(ast_basedir)visitor.stamp
 $(ast_basedir)visitor.hh: $(ast_srcdir)/visitor.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## PrettyPrinter.
 EXTRA_DIST += $(ast_basedir)pretty-printer.stamp
 $(ast_basedir)pretty-printer.hh $(ast_basedir)pretty-printer.hxx $(ast_basedir)pretty-printer.cc: $(ast_srcdir)/pretty-printer.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 ## Dot
 EXTRA_DIST += $(ast_basedir)dot-printer.stamp
 $(ast_basedir)dot-printer.hh $(ast_basedir)dot-printer.cc: $(ast_srcdir)/dot-printer.stamp
+	@$(AST_REGENERATE_IF_NEEDED)
 
 MAINTAINERCLEANFILES += $(BUILT_SOURCES_ast)
