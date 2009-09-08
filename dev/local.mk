ed970f72f580ecf23d3762c55438edadc3ea5a84
Akim Demaille <demaille@gostai.com>
2009-09-08 14:42:05 +0200

    Comment changes.

--- dev/local.mk
+++ dev/local.mk
@@ -1,22 +1,22 @@
-# Define devdir to empty is this file is load from dev/Makefile.am,
+# Define devdir to empty if this file is loaded from dev/Makefile.am,
 # or to "dev/" if this file is loaded from the top-level.
 
 EXTRA_DIST +=					\
-$(devdir)ast-all-gen				\
-$(devdir)ast-cloner-gen				\
-$(devdir)ast-transformer-gen			\
-$(devdir)ast-default-visitor-gen		\
-$(devdir)ast-dot-printer-gen			\
-$(devdir)ast-fwd-gen				\
-$(devdir)ast-gen.mk				\
-$(devdir)ast-graph-gen				\
-$(devdir)ast-ignores-gen			\
-$(devdir)ast-nodes-gen				\
-$(devdir)ast-nodes-mk-gen			\
-$(devdir)ast-pretty-printer-gen			\
-$(devdir)ast-readme-gen				\
-$(devdir)ast-visitor-gen			\
-$(devdir)ast.py					\
-$(devdir)tools.py				\
-$(devdir)transform-all-gen			\
-$(devdir)transform-proxy-visitors-gen
+  $(devdir)ast-all-gen				\
+  $(devdir)ast-cloner-gen			\
+  $(devdir)ast-transformer-gen			\
+  $(devdir)ast-default-visitor-gen		\
+  $(devdir)ast-dot-printer-gen			\
+  $(devdir)ast-fwd-gen				\
+  $(devdir)ast-gen.mk				\
+  $(devdir)ast-graph-gen			\
+  $(devdir)ast-ignores-gen			\
+  $(devdir)ast-nodes-gen			\
+  $(devdir)ast-nodes-mk-gen			\
+  $(devdir)ast-pretty-printer-gen		\
+  $(devdir)ast-readme-gen			\
+  $(devdir)ast-visitor-gen			\
+  $(devdir)ast.py				\
+  $(devdir)tools.py				\
+  $(devdir)transform-all-gen			\
+  $(devdir)transform-proxy-visitors-gen
--- src/kernel/ughostconnection.cc
+++ src/kernel/ughostconnection.cc
@@ -1,23 +1,15 @@
-/*! \file kernel/ughostconnection.cc
- *******************************************************************************
+/*
+ * Copyright (C) 2004-2009, Gostai S.A.S.
+ *
+ * This software is provided "as is" without warranty of any kind,
+ * either expressed or implied, including but not limited to the
+ * implied warranties of fitness for a particular purpose.
+ *
+ * See the LICENSE file for more information.
+ */
 
- File: kernel/ughostconnection.cc\n
- Implementation of the UGhostConnection class.
-
- This file is part of
- %URBI Kernel, version __kernelversion__\n
- Copyright (c) 2004-2009, Jean-Christophe Baillie.
-
- Permission to use, copy, modify, and redistribute this software for
- non-commercial use is hereby granted.
-
- This software is provided "as is" without warranty of any kind,
- either expressed or implied, including but not limited to the
- implied warranties of fitness for a particular purpose.
-
- For more information, comments, bug reports: http://www.urbiforge.net
-
- **************************************************************************** */
+/// \file kernel/ughostconnection.cc
+/// \brief Implementation of the UGhostConnection class.
 
 #include <libport/cstring>
 #include <libport/windows.hh>
