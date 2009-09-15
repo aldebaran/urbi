7020ca449b6a7d00fe17a1f315ea0d5d4ca0e296
Akim Demaille <demaille@gostai.com>
2009-09-15 13:21:12 +0200

    factory: simplify.
    	* src/ast/factory.hh: Remove useless ast:: qualifications.

--- src/ast/factory.hh
+++ src/ast/factory.hh
@@ -24,7 +24,7 @@ namespace ast
   class Factory
   {
   public:
-    typedef std::pair<libport::Symbol, ast::rExp> modifier_type;
+    typedef std::pair<libport::Symbol, rExp> modifier_type;
     typedef modifier_type formal_type;
     typedef std::vector<formal_type> formals_type;
 
@@ -32,143 +32,143 @@ namespace ast
 
     /// at%flavor (%cond ~ %duration) {%body} onleave {%onleave}
     static
-    ast::rExp
+    rExp
     make_at(const yy::location& loc,
-            const location& flavor_loc, ast::flavor_type flavor,
-            ast::rExp cond,
-            ast::rExp body, ast::rExp onleave = 0,
-            ast::rExp duration = 0) /* const */;
+            const location& flavor_loc, flavor_type flavor,
+            rExp cond,
+            rExp body, rExp onleave = 0,
+            rExp duration = 0) /* const */;
 
     /// at(%flavor) (?(%event)(%payload) {%body} onleave {%onleave}
     static
-    ast::rExp
+    rExp
     make_at_event(const location& loc,
-                  const location& flavor_loc, ast::flavor_type flavor,
+                  const location& flavor_loc, flavor_type flavor,
                   EventMatch& event,
-                  ast::rExp body, ast::rExp onleave = 0) /* const */;
+                  rExp body, rExp onleave = 0) /* const */;
 
     /// Create a new Tree node composing \c Lhs and \c Rhs with \c Op.
     /// \param op can be any of the four cases.
     static
-    ast::rExp
+    rExp
     make_bin(const yy::location& l,
-             ast::flavor_type op,
-             ast::rExp lhs, ast::rExp rhs) /* const */;
+             flavor_type op,
+             rExp lhs, rExp rhs) /* const */;
 
     /// Create a binding, possibly const.
     /// Corresponds to "var <exp>" and "const var <exp>".
     /// Check that <exp> can indeed be declared: it must be an LValue.
     /// It must not be a descendant such as Property.
     static
-    ast::rBinding
+    rBinding
     make_binding(const yy::location& l,
                  bool constant,
-                 const yy::location& exp_loc, ast::rExp exp) /* const */;
+                 const yy::location& exp_loc, rExp exp) /* const */;
 
     /// "<method>"
     static
-    ast::rCall
+    rCall
     make_call(const yy::location& l,
               libport::Symbol method) /* const */;
 
 
     /// "<target> . <method> (args)".
     static
-    ast::rCall
+    rCall
     make_call(const yy::location& l,
-              ast::rExp target,
-              libport::Symbol method, ast::exps_type* args) /* const */;
+              rExp target,
+              libport::Symbol method, exps_type* args) /* const */;
 
     /// "<target> . <method> ()".
     static
-    ast::rCall
+    rCall
     make_call(const yy::location& l,
-              ast::rExp target, libport::Symbol method) /* const */;
+              rExp target, libport::Symbol method) /* const */;
 
     /// "<target> . <method> (<arg1>)".
     static
-    ast::rCall
+    rCall
     make_call(const yy::location& l,
-              ast::rExp target, libport::Symbol method, ast::rExp arg1) /* const */;
+              rExp target, libport::Symbol method, rExp arg1) /* const */;
 
 
     /// "<target> . <method> (<arg1>, <arg2>)".
     /// "<target> . <method> (<arg1>, <arg2>, <arg3>)".
     static
-    ast::rCall
+    rCall
     make_call(const yy::location& l,
-              ast::rExp target, libport::Symbol method,
-              ast::rExp arg1, ast::rExp arg2, ast::rExp arg3 = 0) /* const */;
+              rExp target, libport::Symbol method,
+              rExp arg1, rExp arg2, rExp arg3 = 0) /* const */;
 
 
     /// "class" lvalue protos block
     static
-    ast::rExp
+    rExp
     make_class(const yy::location& l,
-               ast::rCall lvalue,
-               ast::exps_type* protos, ast::rExp block) /* const */;
+               rCall lvalue,
+               exps_type* protos, rExp block) /* const */;
 
 
     /// closure () { <value> }
     static
-    ast::rExp
-    make_closure(ast::rExp value) /* const */;
+    rExp
+    make_closure(rExp value) /* const */;
 
     static
-    ast::rExp
+    rExp
     make_event_catcher(const location& loc,
                        EventMatch& event,
-                       ast::rExp body, ast::rExp onleave) /* const */;
+                       rExp body, rExp onleave) /* const */;
 
     /// Build an "every" loop.
     static
-    ast::rExp
+    rExp
     make_every(const yy::location& loc,
-               const location& flavor_loc, ast::flavor_type flavor,
-               ast::rExp test, ast::rExp body) /* const */;
+               const location& flavor_loc, flavor_type flavor,
+               rExp test, rExp body) /* const */;
 
     // Build a for(iterable) loop.
     static
-    ast::rExp
+    rExp
     make_for(const yy::location&,
-             const location& flavor_loc, ast::flavor_type flavor,
-             ast::rExp iterable, ast::rExp body) /* const */;
+             const location& flavor_loc, flavor_type flavor,
+             rExp iterable, rExp body) /* const */;
 
     // Build a for(var id : iterable) loop.
     static
-    ast::rExp
+    rExp
     make_for(const yy::location& l,
-             const location& flavor_loc, ast::flavor_type flavor,
+             const location& flavor_loc, flavor_type flavor,
              const yy::location& id_loc, libport::Symbol id,
-             ast::rExp iterable, ast::rExp body) /* const */;
+             rExp iterable, rExp body) /* const */;
 
     /// Build a C-for loop.
     static
-    ast::rExp
+    rExp
     make_for(const yy::location& l,
-             const location& flavor_loc, ast::flavor_type flavor,
-             ast::rExp init, ast::rExp test, ast::rExp inc,
-             ast::rExp body) /* const */;
+             const location& flavor_loc, flavor_type flavor,
+             rExp init, rExp test, rExp inc,
+             rExp body) /* const */;
 
 
     /// freezeif ( %cond ) %body.
     static
-    ast::rExp
+    rExp
     make_freezeif(const location& loc,
-                  ast::rExp cond, ast::rExp body) /* const */;
+                  rExp cond, rExp body) /* const */;
 
     /// \param iffalse can be 0.
     static
-    ast::rExp
+    rExp
     make_if(const yy::location& l,
-            ast::rExp cond, ast::rExp iftrue, ast::rExp iffalse) /* const */;
+            rExp cond, rExp iftrue, rExp iffalse) /* const */;
 
     /// loop %body.
     static
-    ast::rExp
+    rExp
     make_loop(const location& loc,
-              const location& flavor_loc, ast::flavor_type flavor,
-              const location& body_loc, ast::rExp body) /* const */;
+              const location& flavor_loc, flavor_type flavor,
+              const location& body_loc, rExp body) /* const */;
 
     /** Use these functions to avoid CPP-like problem when referring
      *  several times to an lvalue.  For instance, do not desugar
@@ -193,15 +193,28 @@ namespace ast
     // ParametricAst where able to pick the same variable several times,
     // which should pose no problem now that ast are refcounted.
     static
-    ast::rLValue
-    make_lvalue_once(const ast::rLValue& lvalue) /* const */;
+    rLValue
+    make_lvalue_once(const rLValue& lvalue) /* const */;
     static
-    ast::rExp
-    make_lvalue_wrap(const ast::rLValue& lvalue, const ast::rExp& e) /* const */;
+    rExp
+    make_lvalue_wrap(const rLValue& lvalue, const rExp& e) /* const */;
+
+    static
+    rNary
+    make_nary(const location& loc, const rExp& e);
+
+    /// Append \a e to \a lhs using \a flavor, and return \a lhs.
+    /// \return \a lhs, modified.
+    static
+    rNary
+    make_nary(const location& loc,
+              rNary lhs,
+              const location& flavor_loc, flavor_type flavor,
+              const rExp& e);
 
     static
     /// Return the ast for "nil".
-    ast::rExp make_nil() /* const */;
+    rExp make_nil() /* const */;
 
     /// Create a closure or a function.
     /// \param closure  whether building a closure.
@@ -211,27 +224,27 @@ namespace ast
     /// \param bloc     location of the body.
     /// \param b        body.
     static
-    ast::rRoutine
+    rRoutine
     make_routine(const location& loc, bool closure,
                  const location& floc, formals_type* f,
-                 const location& bloc, const ast::rExp b) /* const */;
+                 const location& bloc, const rExp b) /* const */;
 
-    /// Return \a e in a ast::Scope unless it is already one.
+    /// Return \a e in a Scope unless it is already one.
     static
-    ast::rScope
-    make_scope(const yy::location& l, ast::rExp target, ast::rExp e) /* const */;
-
-    static
-    ast::rScope
-    make_scope(const yy::location& l, ast::rExp e) /* const */;
+    rScope
+    make_scope(const yy::location& l, rExp target, rExp e) /* const */;
 
     static
-    ast::rExp
+    rScope
+    make_scope(const yy::location& l, rExp e) /* const */;
+
+    static
+    rExp
     make_stopif(const location& loc,
-                ast::rExp cond, ast::rExp body) /* const */;
+                rExp cond, rExp body) /* const */;
 
     static
-    ast::rExp
+    rExp
     make_string(const yy::location& l, libport::Symbol s) /* const */;
 
 
@@ -239,65 +252,65 @@ namespace ast
        static
        Naries with a single statement.  */
     static
-    ast::rExp make_strip(ast::rNary e) /* const */;
+    rExp make_strip(rNary e) /* const */;
     static
-    ast::rExp make_strip(ast::rExp e) /* const */;
+    rExp make_strip(rExp e) /* const */;
 
 
     /*--------.
     | Switch  |
     `--------*/
 
-    typedef std::pair<ast::rMatch, ast::rNary> case_type;
+    typedef std::pair<rMatch, rNary> case_type;
     typedef std::list<case_type> cases_type;
 
     static
-    ast::rExp
-    make_switch(const yy::location& l, ast::rExp cond,
-                const cases_type& cases, ast::rExp def) /* const */;
+    rExp
+    make_switch(const yy::location& l, rExp cond,
+                const cases_type& cases, rExp def) /* const */;
 
 
     /// timeout(duration) body
     static
-    ast::rExp
-    make_timeout(const ast::rExp& duration, const ast::rExp& body) /* const */;
+    rExp
+    make_timeout(const rExp& duration, const rExp& body) /* const */;
 
     /// waituntil (cond ~ duration);
     /// \param duration can be 0.
     static
-    ast::rExp
+    rExp
     make_waituntil(const yy::location& loc,
-                   const ast::rExp& cond, ast::rExp duration) /* const */;
+                   const rExp& cond, rExp duration) /* const */;
 
     /// waituntil (?(%event)(%payload))
     static
-    ast::rExp
+    rExp
     make_waituntil_event(const location& loc,
-                         ast::rExp event,
-                         ast::exps_type* payload) /* const */;
+                         rExp event,
+                         exps_type* payload) /* const */;
 
     /// whenever (%cond ~ %duration) {%body} onleave {%else_stmt}
     static
-    ast::rExp
+    rExp
     make_whenever(const yy::location& loc,
-                  ast::rExp cond,
-                  ast::rExp body, ast::rExp else_stmt = 0,
-                  ast::rExp duration = 0) /* const */;
+                  rExp cond,
+                  rExp body, rExp else_stmt = 0,
+                  rExp duration = 0) /* const */;
 
     /// whenever (?(%event)(%payload) {%body} onleave {%onleave}
     static
-    ast::rExp
+    rExp
     make_whenever_event(const location& loc,
                         EventMatch& event,
-                        ast::rExp body, ast::rExp onleave = 0) /* const */;
+                        rExp body, rExp onleave = 0) /* const */;
 
     /// while (%cond) %body.
     static
-    ast::rExp
+    rExp
     make_while(const location& loc,
-               const location& flavor_loc, ast::flavor_type flavor,
-               ast::rExp cond,
-               const location& body_loc, ast::rExp body) /* const */;
+               const location& flavor_loc, flavor_type flavor,
+               rExp cond,
+               const location& body_loc, rExp body) /* const */;
   };
 
 }
