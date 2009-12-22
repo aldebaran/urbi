b018f8a6c688f4483ccbc6ea53f6cb1e9dc843ec
Akim Demaille <demaille@gostai.com>
2009-12-22 16:30:23 +0100

    remove the on-line tutorial.
    Two problems: it is demanding (exotic Python libraries), and it is empty.
    Today we can probably get rid of Python, urbiscript is richer, but that
    will not save us from the second problem.
    
    	* share/urbi/tutorial.u, share/urbi/tutorial/tutorial.py,
    	* share/urbi/tutorial/tutorial.xml: Remove.
    	* doc/specs/lobby.tex, share/urbi/local.mk, share/urbi/urbi.u: Adjust.

--- share/urbi/local.mk
+++ share/urbi/local.mk
@@ -1,6 +1,5 @@
 # Yes, this is not by the book, but it is so cooler.
 dist_urbi_DATA := $(call ls_files,share/urbi/*.u)
-dist_urbi_DATA += share/urbi/tutorial-content.u
 
 nodist_urbi_DATA =				\
   share/urbi/platform.u
@@ -29,20 +28,6 @@ share/urbi/package-info-urbi-sdk-remote.u: $(top_srcdir)/sdk-remote/.version $(R
 share/urbi/package-info-libport.u: $(top_srcdir)/sdk-remote/libport/.version $(REVISION)
 	$(REVISION_RUN) --prefix='Libport'
 	touch $@
-
-
-## ---------- ##
-## Tutorial.  ##
-## ---------- ##
-
-tutorial_sources =				\
-  share/urbi/tutorial/tutorial.xml		\
-  share/urbi/tutorial/tutorial.py
-EXTRA_DIST += $(tutorial_sources)
-$(srcdir)/share/urbi/tutorial-content.u: $(tutorial_sources)
-	rm -f $@ $@.tmp
-	$(srcdir)/share/urbi/tutorial/tutorial.py $< > $@.tmp
-	mv $@.tmp $@
 
 urbi.stamp: $(dist_urbi_DATA) $(nodist_urbi_DATA)
 	@echo "$$(date)"': $?' >>$@
