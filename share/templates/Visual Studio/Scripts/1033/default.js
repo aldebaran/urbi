3bff835fc46fe28be1565ba706a4d91f62d65abc
Matthieu Nottale <nottale@gostai.com>
2010-01-28 16:05:48 +0100

    Template vcxx: Set urbi-launch-d as the debug run command.
    	* share/templates/Visual Studio/Scripts/1033/default.js: Here.

--- share/templates/Visual Studio/Scripts/1033/default.js
+++ share/templates/Visual Studio/Scripts/1033/default.js
@@ -99,7 +99,8 @@ function AddConfig(proj, strProjectName)
     config.IntermediateDirectory = '$(ConfigurationName)';
     config.OutputDirectory = '$(ConfigurationName)';
     config.ConfigurationType = 2; // dll
-
+    config.DebugSettings.Command = urbiPath + "\\bin\\urbi-launch-d.exe";
+    config.DebugSettings.CommandArguments = '-s "$(targetDir)\\$(targetFileName)" -- -i'
     var CLTool = config.Tools('VCCLCompilerTool');
 
     CLTool.AdditionalIncludeDirectories = urbiPath + "\\include";
