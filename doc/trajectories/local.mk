db7ce29a974c0540e708a7b819555b63356f7f6e
Akim Demaille <demaille@gostai.com>
2010-02-08 17:45:35 +0100

    doc: trajectories.
    The trajectories are requested in *.utraj files, from which a *.dat
    file is made (by sampling).  This file is stored in the repository: we
    don't want to compute it each time, it might get wrong.  From the *.dat
    file, a *.pdf file is generated.
    
    Not perfect yet, LaTeX outputs from GNU Plot would be nicer.
    
    	* doc/specs/trajectory-generator.tex: New.
    	* doc/trajectories/accel.dat: New.
    	* doc/trajectories/accel.utraj: New.
    	* doc/trajectories/cos.dat: New.
    	* doc/trajectories/cos.utraj: New.
    	* doc/trajectories/local.mk: New.
    	* doc/trajectories/plot.u: New.
    	* doc/trajectories/sin.dat: New.
    	* doc/trajectories/sin.utraj: New.
    	* doc/trajectories/smooth-continued.dat: New.
    	* doc/trajectories/smooth-continued.utraj: New.
    	* doc/trajectories/smooth-frozen.dat: New.
    	* doc/trajectories/smooth-frozen.utraj: New.
    	* doc/trajectories/smooth.dat: New.
    	* doc/trajectories/smooth.utraj: New.
    	* doc/trajectories/speed-adaptive.dat: New.
    	* doc/trajectories/speed-adaptive.utraj: New.
    	* doc/trajectories/speed.dat: New.
    	* doc/trajectories/speed.utraj: New.
    	* doc/trajectories/time-adaptive.dat: New.
    	* doc/trajectories/time-adaptive.utraj: New.
    	* doc/trajectories/time.dat: New.
    	* doc/trajectories/time.utraj: New.
    	* doc/Makefile.am, doc/specs/lang.tex, doc/specs/stdlib.tex: Use them.
    	* doc/urbi-sdk.tex: Ditto.
    	* doc/urbi-sandbox.tex: Toy with urbitrajectory.

---
+++ doc/trajectories/local.mk
@@ -0,0 +1,34 @@
+## -------------- ##
+## Trajectories.  ##
+## -------------- ##
+
+URBI = $(top_builddir)/tests/bin/urbi
+GNUPLOT = gnuplot
+$(srcdir)/%.dat: %.utraj
+	$(URBI) -f $(srcdir)/trajectories/plot.u	\
+	     -f '$<'					\
+	     -e 'plot("$@.tmp", sample(y, 40, 0.1s));'	\
+	     -e 'shutdown;'
+	mv $@.tmp $@
+
+%.pdf: %.dat
+	$(GNUPLOT) -e 'set terminal pdf;'			\
+	        -e 'plot "$<" using 1:2 with linespoints'	\
+	        >$@.tmp
+	mv $@.tmp $@
+
+
+# Not only is this true (i.e., we do want these intermediate files to
+# be kept, as we store them in our repository), but it is also
+# mandated by GNU Make, who, otherwise, does not seem to understand
+# how to go from utraj to pdf with an intermediate step in $(srcdir).
+.SECONDARY: $(addprefix $(srcdir)/,$(TRAJECTORIES:.utraj=.dat))
+
+dgb:
+	echo $(TRAJECTORIES)
+	echo $(TRAJECTORIES:.utraj=.pdf)
+
+TRAJECTORIES = $(call ls_files,trajectories/*.utraj)
+
+PDF_IMAGES +=					\
+  $(TRAJECTORIES:.utraj=.pdf)
