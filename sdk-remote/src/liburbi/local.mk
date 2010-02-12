c254b8804a180f9379e4744d71b4998b95323d35
Akim Demaille <demaille@gostai.com>
2010-02-12 15:13:37 +0100

    Style changes.

--- sdk-remote/src/liburbi/local.mk
+++ sdk-remote/src/liburbi/local.mk
@@ -5,7 +5,7 @@
 lib_LTLIBRARIES += liburbi/liburbi@LIBSFX@.la
 # FIXME: Something is fishy here: why do we duplicate libuco code in
 # both liburbi and libuobject?  Libuobject includes liburbi.
-liburbi_liburbi@LIBSFX@_la_SOURCES =			\
+liburbi_liburbi@LIBSFX@_la_SOURCES =		\
   liburbi/compatibility.hh			\
   liburbi/compatibility.hxx			\
   liburbi/kernel-version.cc			\
@@ -17,17 +17,19 @@ liburbi_liburbi@LIBSFX@_la_SOURCES =			\
   liburbi/usyncclient.cc			\
   liburbi/utag.cc
 
-liburbi_liburbi@LIBSFX@_la_CPPFLAGS =			\
+liburbi_liburbi@LIBSFX@_la_CPPFLAGS =		\
   $(AM_CPPFLAGS)				\
   -DBUILDING_URBI_SDK
 # Find version.hh.
 liburbi_liburbi@LIBSFX@_la_CPPFLAGS += -I. -DURBI_ROOT_NOT_DLL
-
-liburbi_liburbi@LIBSFX@_la_LIBADD = $(LIBADD) libuvalue/libuvalue.la $(JPEG_LIBS)
-liburbi_liburbi@LIBSFX@_la_LDFLAGS = -avoid-version -no-undefined
 # We are using Libport.OptionParser which uses Boost.Optional, which is
 # incompatible with strict alias analysis.
 liburbi_liburbi@LIBSFX@_la_CXXFLAGS = -fno-strict-aliasing $(AM_CXXFLAGS)
+liburbi_liburbi@LIBSFX@_la_LIBADD =		\
+  $(LIBADD)					\
+  libuvalue/libuvalue.la			\
+  $(JPEG_LIBS)
+liburbi_liburbi@LIBSFX@_la_LDFLAGS = -avoid-version -no-undefined
 
 ## ---------- ##
 ## umain.cc.  ##
