d71c0ee0e4b83671b67bd1471078da236abbe377
Akim Demaille <demaille@gostai.com>
2010-03-12 15:42:42 +0100

    reheader.
    	* include/urbi/uabstractclient.hh: here.

--- sdk-remote/include/urbi/uabstractclient.hh
+++ sdk-remote/include/urbi/uabstractclient.hh
@@ -1,25 +1,15 @@
-/*! \file urbi/uabstractclient.hh
- ****************************************************************************
+/*
+ * Copyright (C) 2004, 2006-2010, Gostai S.A.S.
  *
- * Definition of the URBI interface class
+ * This software is provided "as is" without warranty of any kind,
+ * either expressed or implied, including but not limited to the
+ * implied warranties of fitness for a particular purpose.
  *
- * Copyright (C) 2004, 2006-2009 Jean-Christophe Baillie.
- * All rights reserved.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- **********************************************************************/
+ * See the LICENSE file for more information.
+ */
+
+/// \file urbi/uabstractclient.hh
+/// \brief Definition of the URBI interface class
 
 #ifndef URBI_UABSTRACTCLIENT_HH
 # define URBI_UABSTRACTCLIENT_HH
