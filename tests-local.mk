63cd3a603846390bbb7f27954a3725eb187293bc
Akim Demaille <demaille@gostai.com>
2010-03-15 14:41:13 +0100

    tests: xfail.
    	* tests: Update to get 2-xfail.chk.
    	* tests-local.mk: XFAIL it.

--- tests-local.mk
+++ tests-local.mk
@@ -35,34 +35,14 @@ k2-check:
 # while running make check.
 ENABLE_HARD_ERRORS = false
 
+# Do not remove this one, it is failing on purpose.
+XFAIL_TESTS +=					\
+  0.x/2-xfail.chk
+
 # test we don't want to care about temporarily
 TFAIL_TESTS +=					\
   1.x/implicit.chk				\
   2.x/non-interruptible.chk
-
-# k1 tests that currently don't pass, but we should.
-# In fact, the above list has been removed to gain some time
-# when running tests. The following tests should be examined
-# and reintroduced in 1.x as needed.
-# TO_CHECK_TESTS =
-# # +begin, +end
-# 1.-/begin-end-report.chk
-# # chan << echo(foo) goes in chan
-# 1.-/channels-tags.chk
-# # +connection
-# 1.-/connection-delete.chk
-# # emit foo
-# 1.-/events.chk
-# 1.-/events-emit-then-return-in-function.chk
-# 1.-/every-emit.chk
-# # []-access to list and hash
-# 1.-/hash-and-list.chk
-# # what should we do with very large literals?
-# 1.-/invalid-number.chk
-# # events
-# 1.-/lazy-test-eval.chk
-# 1.-/loadwav.chk
-# 1.-/object-events.chk
 
 # Uobject tests that we fail because features are not implemented
 XFAIL_TESTS +=					\
