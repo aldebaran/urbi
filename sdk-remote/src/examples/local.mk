b4f4c2c0808bf297d67d1a7b027eef141687cbbe
Akim Demaille <demaille@gostai.com>
2010-03-26 13:27:50 +0100

    build: enjoy Automake 1.11.
    	* src/examples/local.mk: Rely on ".cc" being the default extension.

--- sdk-remote/src/examples/local.mk
+++ sdk-remote/src/examples/local.mk
@@ -8,23 +8,12 @@ bin_PROGRAMS +=					\
   examples/urbi-sendbin				\
   examples/urbi-sendsound
 
-examples_urbi_bandwidth_SOURCES	= examples/urbi-bandwidth.cc
-examples_urbi_mirror_SOURCES	= examples/urbi-mirror.cc
-examples_urbi_ping_SOURCES	= examples/urbi-ping.cc
-examples_urbi_play_SOURCES	= examples/urbi-play.cc
-examples_urbi_record_SOURCES	= examples/urbi-record.cc
-examples_urbi_send_SOURCES	= examples/urbi-send.cc
-examples_urbi_sendbin_SOURCES	= examples/urbi-sendbin.cc
-examples_urbi_sendsound_SOURCES	= examples/urbi-sendsound.cc
-
 if SOUNDCARD
 bin_PROGRAMS += examples/urbi-sound
-examples_urbi_sound_SOURCES = examples/urbi-sound.cc
 endif
 
 if !WIN32
 bin_PROGRAMS += examples/urbi-talkie
-examples_urbi_talkie_SOURCES = examples/urbi-talkie.cc
 endif
 
 
