bfe57555316468362b0ed661929372f230a14d7f
Matthieu Nottale <nottale@gostai.com>
2010-03-30 11:27:05 +0200

    urbi: Call urbi_main directly instead of going through urbi_launch.
    	* src/bin/urbi.cc: Just convert our argv and bounce to
    	urbi_root.urbi_main().

--- src/bin/urbi.cc
+++ src/bin/urbi.cc
@@ -10,7 +10,6 @@
 
 #include <config.h>
 
-#include <libport/cassert>
 #include <libport/cerrno>
 #include <iostream>
 #include <string>
@@ -32,38 +31,20 @@ using libport::strneq;
 int
 main(int argc, char* argv[])
 {
-  UrbiRoot urbi_root(argv[0]);
+  UrbiRoot urbi_root(argv[0],
+# ifdef STATIC_BUILD
+    true
+# else
+    false
+# endif
+    );
 
-  std::vector<std::string> args_urbi_launch;
-  std::vector<std::string> args_libuobject;
-  args_urbi_launch << "--start";
+# ifndef STATIC_BUILD
+  urbi_root.load_plugin();
+# endif
 
-  for (int i = 1; i < argc; ++i)
-  {
-    if (streq(argv[i], "--debug") || streq(argv[i], "-d"))
-      args_urbi_launch << "--debug" << argv[++i];
-    else if (strneq(argv[i], "--debug=", 8))
-      args_urbi_launch << "--debug" << argv[i] + 8;
-    else
-      args_libuobject << argv[i];
-  }
-
-  // Prepare a C version of vector<string> to please urbi_launch.
-  const size_t argc_bounce =
-    args_urbi_launch.size() + args_libuobject.size() + 2;
-  const char** argv_bounce = new const char*[argc_bounce + 1];
-
-  {
-    unsigned i = 0;
-    argv_bounce[i++] = argv[0];//urbi_launch.c_str();
-    for (unsigned j = 0; j < args_urbi_launch.size(); ++j)
-      argv_bounce[i++] = args_urbi_launch[j].c_str();
-    argv_bounce[i++] = "--";
-    for (unsigned j = 0; j < args_libuobject.size(); ++j)
-      argv_bounce[i++] = args_libuobject[j].c_str();
-    argv_bounce[i] = 0;
-    aver(i == argc_bounce);
-  }
-
-  return urbi_root.urbi_launch(argc_bounce, argv_bounce);
+  std::vector<std::string> args;
+  for (int i=0; i< argc; ++i)
+    args.push_back(argv[i]);
+  return urbi_root.urbi_main(args, true, true);
 }
