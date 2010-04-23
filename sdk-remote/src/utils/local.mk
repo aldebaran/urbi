2e90e71f0f4f94ec30e499f0249532a286771646
Akim Demaille <demaille@gostai.com>
2010-04-23 16:36:26 +0200

    remove obsolete files.
    Remove urbi-walk and dependencies.
    
    	* src/utils/move.cc: Remove.
    	* src/utils/move.hh: Remove.
    	* src/utils/moveset/ctrlturnr_20.exe: Remove.
    	* src/utils/moveset/ctrlturnr_30.exe: Remove.
    	* src/utils/moveset/ctrlturnr_40.exe: Remove.
    	* src/utils/moveset/ctrlturnr_50.exe: Remove.
    	* src/utils/moveset/ctrlturnr_60.exe: Remove.
    	* src/utils/moveset/ctrlturnr_70.exe: Remove.
    	* src/utils/moveset/ctrlturnrevr_20.exe: Remove.
    	* src/utils/moveset/ctrlturnrevr_30.exe: Remove.
    	* src/utils/moveset/ctrlturnrevr_40.exe: Remove.
    	* src/utils/moveset/ctrlturnrevr_50.exe: Remove.
    	* src/utils/moveset/ctrlturnrevr_60.exe: Remove.
    	* src/utils/moveset/ctrlturnrevr_70.exe: Remove.
    	* src/utils/moveset/ctrlwalkb_200.exe: Remove.
    	* src/utils/moveset/ctrlwalkb_300.exe: Remove.
    	* src/utils/moveset/ctrlwalkb_400.exe: Remove.
    	* src/utils/moveset/ctrlwalkb_550.exe: Remove.
    	* src/utils/moveset/ctrlwalkf_200.exe: Remove.
    	* src/utils/moveset/ctrlwalkf_300.exe: Remove.
    	* src/utils/moveset/ctrlwalkf_400.exe: Remove.
    	* src/utils/moveset/ctrlwalkf_550.exe: Remove.
    	* src/utils/moveset/moveconfig: Remove.
    	* src/utils/moveset/moveconfig_assym: Remove.
    	* src/utils/urbi-walk.cc: Remove.

--- sdk-remote/src/utils/local.mk
+++ sdk-remote/src/utils/local.mk
@@ -9,8 +9,7 @@
 bin_PROGRAMS +=					\
   utils/urbi-cycle				\
   utils/urbi-reverse				\
-  utils/urbi-scale				\
-  utils/urbi-walk
+  utils/urbi-scale
 
 utils_urbi_cycle_SOURCES =			\
   utils/parse-header.hh				\
@@ -21,11 +20,3 @@ utils_urbi_reverse_SOURCES =			\
 utils_urbi_scale_SOURCES =			\
   utils/parse-header.hh				\
   utils/urbi-scale.cc
-
-utils_urbi_walk_SOURCES =			\
-  utils/urbi-walk.cc				\
-  utils/move.cc					\
-  utils/move.hh
-utils_urbi_walk_LDADD =				\
-  $(JPEG_LIBS)					\
-  liburbi/liburbi$(LIBSFX).la
