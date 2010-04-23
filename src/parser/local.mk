cb84ec50dd0a67af8850fb5a2bfe3f94624d4632
Akim Demaille <demaille@gostai.com>
2010-04-23 15:30:43 +0200

    build: add license banner.
    	* src/Makefile.am, src/ast/local.mk, src/bin/local.mk,
    	* src/binder/local.mk, src/flower/local.mk, src/kernel/local.mk,
    	* src/object/local.mk, src/parser/local.mk, src/rewrite/local.mk,
    	* src/runner/local.mk, src/urbi/local.mk:
    	here.

--- src/parser/local.mk
+++ src/parser/local.mk
@@ -1,3 +1,11 @@
+## Copyright (C) 2008-2010, Gostai S.A.S.
+##
+## This software is provided "as is" without warranty of any kind,
+## either expressed or implied, including but not limited to the
+## implied warranties of fitness for a particular purpose.
+##
+## See the LICENSE file for more information.
+
 dist_libuobject@LIBSFX@_la_SOURCES +=			\
   parser/fwd.hh					\
   parser/is-keyword.hh				\
--- src/urbi/local.mk
+++ src/urbi/local.mk
@@ -1,2 +1,10 @@
+## Copyright (C) 2009-2010, Gostai S.A.S.
+##
+## This software is provided "as is" without warranty of any kind,
+## either expressed or implied, including but not limited to the
+## implied warranties of fitness for a particular purpose.
+##
+## See the LICENSE file for more information.
+
 dist_libuobject@LIBSFX@_la_SOURCES +=			\
   urbi/sdk.cc
