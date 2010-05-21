1b2d624945ebfdd52530730da234f63cd483c031
Akim Demaille <demaille@gostai.com>
2010-05-21 10:58:12 +0200

    build: hard code libjpeg4urbi as the name for the libjpeg library to use.
    	* Makefile.am, configure.ac, sdk/umake.in, src/Makefile.am,
    	* src/liburbi/urbi-root.cc, src/tests/bin/urbi-launch.in,
    	* src/tests/liburbi/local.mk: here.

--- sdk-remote/src/tests/liburbi/local.mk
+++ sdk-remote/src/tests/liburbi/local.mk
@@ -42,7 +42,7 @@ AM_CPPFLAGS += $(BOOST_CPPFLAGS)
 
 AM_LDADD =						\
   $(top_builddir)/src/liburbi/liburbi$(LIBSFX).la	\
-  $(top_builddir)/jpeg/lib$(LIBJPEG_NAME)$(LIBSFX).la	\
+  $(top_builddir)/jpeg/libjpeg4urbi$(LIBSFX).la	\
   $(PTHREAD_LIBS)
 # We should not need to report the -rpath to Boost here, since we
 # don't depend directly from it (it is liburbi.la which does, via
