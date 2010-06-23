1544405b74582129b3d72a50a48d5ff645a3b383
Matthieu Nottale <nottale@gostai.com>
2010-06-23 14:01:48 +0200

    Fix dependencies of 'serialize'.
    	* src/bin/local.mk: Use correct libuobject name.

--- src/bin/local.mk
+++ src/bin/local.mk
@@ -29,7 +29,7 @@ bin_ast_dump_LDADD = libuobject$(LIBSFX).la
 bin_PROGRAMS += bin/serialize
 bin_serialize_SOURCES = bin/serialize.cc ast/serializer.cc
 bin_serialize_CPPFLAGS = $(libuobject_la_CPPFLAGS)
-bin_serialize_LDADD = libuobject.la
+bin_serialize_LDADD = libuobject$(LIBSFX).la
 
 # urbi.
 bin_PROGRAMS += bin/urbi
