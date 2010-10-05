930a57e158d5963c7e63691a853eea3c98b03258
Akim Demaille <demaille@gostai.com>
2010-10-05 16:34:33 +0200

    git-version-gen: update use.
    	* build-aux, sdk-remote: Update.
    	* share/urbi/local.mk: Adjust.
    	* src/Makefile.am: Style change.

--- share/urbi/local.mk
+++ share/urbi/local.mk
@@ -11,24 +11,20 @@ BUILT_SOURCES += $(nodist_urbi_DATA)
 ## PackageInfos.  ##
 ## -------------- ##
 
-REVISION = $(build_aux_dir)/bin/git-version-gen
-REVISIONFLAGS = --directory
-REVISION_RUN = $(REVISION) $(REVISIONFLAGS) --cache=$< --urbiscript=$@
-
 urbi_package_infodir = $(urbidir)/package-info
 nodist_urbi_package_info_DATA =			\
   share/urbi/package-info/urbi-sdk.u		\
   share/urbi/package-info/urbi-sdk-remote.u	\
   share/urbi/package-info/libport.u
 
-share/urbi/package-info/urbi-sdk.u: $(top_srcdir)/.version $(REVISION)
-	$(REVISION_RUN) --prefix='Urbi SDK'
+share/urbi/package-info/urbi-sdk.u: $(top_srcdir)/.version $(VERSIONIFY)
+	$(VERSIONIFY_RUN) --urbiscript=$@ --prefix='Urbi SDK'
 	touch $@
-share/urbi/package-info/urbi-sdk-remote.u: $(top_srcdir)/sdk-remote/.version $(REVISION)
-	$(REVISION_RUN) --prefix='Urbi SDK Remote'
+share/urbi/package-info/urbi-sdk-remote.u: $(top_srcdir)/sdk-remote/.version $(VERSIONIFY)
+	$(VERSIONIFY_RUN) --urbiscript=$@ --prefix='Urbi SDK Remote'
 	touch $@
-share/urbi/package-info/libport.u: $(top_srcdir)/sdk-remote/libport/.version $(REVISION)
-	$(REVISION_RUN) --prefix='Libport'
+share/urbi/package-info/libport.u: $(top_srcdir)/sdk-remote/libport/.version $(VERSIONIFY)
+	$(VERSIONIFY_RUN) --urbiscript=$@ --prefix='Libport'
 	touch $@
 
 urbi.stamp: $(dist_urbi_DATA) $(nodist_urbi_DATA)
