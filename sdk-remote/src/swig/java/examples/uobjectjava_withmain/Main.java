153d66b058eb463df8a58a2c5d09a9dda79d4fd8
Humbert Remi <humbert@gostai.com>
2010-10-08 16:04:42 +0200

    UObject Java, add example uobjectjava with a main.
    	* src/swig/java/Makefile.am: compile.
    	* src/swig/java/examples/uobjectjava_withmain/Main.java: New.
    	* src/swig/java/examples/uobjectjava_withmain/SimpleUObject.java: New.
    	* src/swig/java/liburbijava.ac: configure.
    	* src/swig/java/sample/uobjectjava_withmain.launch.in: New.

---
+++ sdk-remote/src/swig/java/examples/uobjectjava_withmain/Main.java
@@ -0,0 +1,66 @@
+
+package examples.uobjectjava_withmain;
+
+/// Import the liburbi classes in liburbi.main (located in in liburbi.jar).
+import urbi.*;
+import java.util.Map;
+
+/// Main class.
+///  - Load the 'urbijava' library
+///  - Starts all the UObjects
+///  - Call UObject.main
+///
+/// Please reuse this class in your Java UObjects.
+/// you only need to replace the UObjects names in
+///
+///  'UObject.UStart'
+///
+/// by the names of your own UObjects.
+///
+public class Main {
+
+    static {
+	try
+	{
+	    /// Load the c++ native library.
+	    System.loadLibrary("urbijava");
+	}
+	catch (java.lang.UnsatisfiedLinkError e)
+	{
+	    System.out.println ("Loading exception: " + e);
+
+	    // DEBUG INFORMATIONS
+	    System.out.println();
+	    System.out.println("----------- Java.library.path -------------");
+	    String lib_path = System.getProperty("java.library.path");
+	    System.out.println(lib_path);
+
+	    // get the system environment variables
+	    System.out.println();
+	    System.out.println("---------- ENVIRONMENT VARIABLES ----------");
+	    Map<String, String> envMap = System.getenv();
+	    // print the system environment variables
+	    for (String key : envMap.keySet())
+		System.out.println(key + " = " +  envMap.get(key));
+	    System.out.println();
+
+	    System.exit (1);
+	}
+    }
+
+    public static void main(String argv[]) {
+       try
+       {
+           /// Start the 'SimpleUObject' UObject
+           UObject.UStart (SimpleUObject.class);
+
+           /// Call the main in UObject. This main
+           /// will never return.
+           UObject.main (argv);
+       }
+       catch (Exception e)
+       {
+           System.out.println (e);
+       }
+    }
+}
---
+++ sdk-remote/src/swig/java/examples/uobjectjava_withmain/SimpleUObject.java
@@ -0,0 +1,13 @@
+package examples.uobjectjava_withmain;
+
+import urbi.UObject;
+
+public class SimpleUObject extends UObject {
+
+   /// The constructor of your UObject MUST take a string
+   /// as parameter.
+   public SimpleUObject (String s) {
+     super (s); /// Call the constructor of UObject
+   }
+
+}
