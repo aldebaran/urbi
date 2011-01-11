35b8cc058d83c92a75b2c0c19095d71ef1e82478
Matthieu Nottale <nottale@gostai.com>
2011-01-11 13:56:18 +0100

    Update remaining old headers.
    	* sdk-remote/include/urbi/qt_umain.hh,
    	* sdk-remote/include/urbi/uabstractclient.hxx,
    	* sdk-remote/include/urbi/utag.hh: Switch to new standard Gostai header.

--- sdk-remote/include/urbi/qt_umain.hh
+++ sdk-remote/include/urbi/qt_umain.hh
@@ -1,28 +1,15 @@
+/*
+ * Copyright (C) 2004-2011, Gostai S.A.S.
+ *
+ * This software is provided "as is" without warranty of any kind,
+ * either expressed or implied, including but not limited to the
+ * implied warranties of fitness for a particular purpose.
+ *
+ * See the LICENSE file for more information.
+ */
 
-/*! \file urbi/usyncclient.hh
-****************************************************************************
-*
-* Definition of the URBI interface class
-*
-* Copyright (C) 2004, 2006, 2007 Jean-Christophe Baillie.  All rights reserved.
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-**********************************************************************/
-
-#ifndef URBI_UMAIN_HH
-# define URBI_UMAIN_HH
+#ifndef URBI_QT_UMAIN_HH
+# define URBI_QT_UMAIN_HH
 
 # include <urbi/usyncclient.hh>
 # include <urbi/uobject.hh>
@@ -90,4 +77,4 @@
 #  define UMAIN()
 # endif /* !URBI_ENV_REMOTE */
 
-#endif /* !URBI_UMAIN_HH */
+#endif /* !URBI_QT_UMAIN_HH */
