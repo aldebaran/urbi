91bb3fcb22a97fc7ed9c69a2dd1a6abb47138e6b
Quentin Hocquet <hocquet@gostai.com>
2011-01-13 11:55:43 +0100

    Update libport: Intrusive pointers: add pointer implicit conversion.
        * sdk-remote/libport: Here.

--- include/urbi/object/slot.hxx
+++ include/urbi/object/slot.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009, 2010, Gostai S.A.S.
+ * Copyright (C) 2009-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -80,7 +80,7 @@ namespace urbi
     inline
     Slot::operator bool ()
     {
-      return value_.operator bool();
+      return value_;
     }
 
     inline
