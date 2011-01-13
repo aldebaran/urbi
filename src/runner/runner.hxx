762fb31a64ea22861174e72156bf590d3ea8ca8c
Quentin Hocquet <hocquet@gostai.com>
2011-01-13 11:52:17 +0100

    Fix mean implicit conversion bug.
    	An rTag was passed instead of a prio_type: intrusive pointers cast
    	to bools, which cast to ints.
    
    	* src/runner/runner.hxx: Pass the priority as expected.

--- src/runner/runner.hxx
+++ src/runner/runner.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2010, Gostai S.A.S.
+ * Copyright (C) 2007-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -93,7 +93,7 @@ namespace runner
       *finally << boost::bind(&tag_stack_type::pop_back, boost::ref(tag_stack_))
 	       << boost::bind(&Runner::recompute_prio, this,
 			      tag->value_get()->prio_get());
-    recompute_prio(tag);
+    recompute_prio(tag->value_get()->prio_get());
   }
 
   LIBPORT_SPEED_INLINE bool
