656b03636fff146fa0f2cd006287592ea882a408
Akim Demaille <demaille@gostai.com>
2011-01-24 15:05:59 +0100

    remove useless include.
    	* src/kernel/ughostconnection.cc: here.

--- src/kernel/ughostconnection.cc
+++ src/kernel/ughostconnection.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010, Gostai S.A.S.
+ * Copyright (C) 2008-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -13,8 +13,6 @@
 
 #include <libport/cstring>
 #include <libport/windows.hh>
-
-#include <urbi/uvalue.hh> // SYNCLINE_WRAP
 
 #include <kernel/ughostconnection.hh>
 #include <kernel/userver.hh>
