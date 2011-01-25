73b9883bb5dd4ff44324baa1fb36d31cc104b516
Matthieu Nottale <nottale@gostai.com>
2011-01-25 15:37:18 +0100

    UValue: use proto_set.
    	* src/object/uvalue.cc: There.

--- src/object/uvalue.cc
+++ src/object/uvalue.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Gostai S.A.S.
+ * Copyright (C) 2010-2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -29,22 +29,19 @@ namespace urbi
     UValue::UValue()
       : bypassMode_(false)
     {
-      protos_set(new List);
-      proto_add(proto ? rObject(proto) : CxxObject::proto);
+      proto_set(proto ? rObject(proto) : CxxObject::proto);
     }
 
     UValue::UValue(libport::intrusive_ptr<UValue>)
       : bypassMode_(false)
     {
-      protos_set(new List);
-      proto_add(proto ? rObject(proto) : CxxObject::proto);
+      proto_set(proto ? rObject(proto) : CxxObject::proto);
     }
 
     UValue::UValue(const urbi::UValue& v, bool bypass)
       : bypassMode_(false)
     {
-      protos_set(new List);
-      proto_add(proto ? rObject(proto) : CxxObject::proto);
+      proto_set(proto ? rObject(proto) : CxxObject::proto);
       put(v, bypass);
     }
 
