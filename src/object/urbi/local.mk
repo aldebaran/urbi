693f4cca878bbd5a8421233b395b7e1ba8065f8e
Akim Demaille <demaille@gostai.com>
2011-02-16 10:23:14 +0100

    Kernel modules: fix compilation on Windows.
    	* src/object/urbi/local.mk: We need -no-undefined.

--- src/object/urbi/local.mk
+++ src/object/urbi/local.mk
@@ -8,11 +8,17 @@
 
 urbi_uobjects_LTLIBRARIES =
 
+urbi_uobjects_LDFLAGS = $(AM_LDFLAGS) -module -avoid-version
+urbi_uobjects_CPPFLAGS = $(AM_CPPFLAGS) -DBUILDING_URBI_SDK
+if WIN32
+urbi_uobjects_LDFLAGS += -no-undefined
+endif WIN32
+
 # urbi/logger.
 urbi_uobjects_LTLIBRARIES += object/urbi/logger.la
 dist_object_urbi_logger_la_SOURCES = object/urbi/logger.cc object/urbi/logger.hh
-object_urbi_logger_la_CPPFLAGS = $(AM_CPPFLAGS) -DBUILDING_URBI_SDK
-object_urbi_logger_la_LDFLAGS = -module -avoid-version
+object_urbi_logger_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
+object_urbi_logger_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
 
 # urbi/process.
 # Currently too hard to make portable.
@@ -21,15 +27,16 @@ urbi_uobjects_LTLIBRARIES += object/urbi/process.la
 dist_object_urbi_process_la_SOURCES =		\
   object/urbi/process.cc			\
   object/urbi/process.hh
-object_urbi_process_la_CPPFLAGS = $(AM_CPPFLAGS) -DBUILDING_URBI_SDK
-object_urbi_process_la_LDFLAGS = -module -avoid-version
+object_urbi_process_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
+object_urbi_process_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
 endif !WIN32
 
 # urbi/regexp.
 urbi_uobjects_LTLIBRARIES += object/urbi/regexp.la
 dist_object_urbi_regexp_la_SOURCES = object/urbi/regexp.cc object/urbi/regexp.hh
-object_urbi_regexp_la_CPPFLAGS = $(AM_CPPFLAGS) -DBUILDING_URBI_SDK
-object_urbi_regexp_la_LDFLAGS = -module -avoid-version
+object_urbi_regexp_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
+object_urbi_regexp_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
+object_urbi_regexp_la_LIBADD = $(BOOST_REGEX_LIBS)
 
 # urbi/stream.
 urbi_uobjects_LTLIBRARIES += object/urbi/stream.la
@@ -40,6 +47,5 @@ dist_object_urbi_stream_la_SOURCES =		\
   object/urbi/input-stream.hh			\
   object/urbi/output-stream.cc			\
   object/urbi/output-stream.hh
-object_urbi_stream_la_CPPFLAGS = $(AM_CPPFLAGS) -DBUILDING_URBI_SDK
-object_urbi_stream_la_LDFLAGS = -module -avoid-version
-
+object_urbi_stream_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
+object_urbi_stream_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
