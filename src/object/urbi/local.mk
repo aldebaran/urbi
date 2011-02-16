ffb721e562a7272bde3cf68821bec249b831a6f1
Akim Demaille <demaille@gostai.com>
2011-02-16 14:52:33 +0100

    Urbi Modules: link against libuobject to please Visual.
    	* src/object/urbi/local.mk: here.

--- src/object/urbi/local.mk
+++ src/object/urbi/local.mk
@@ -14,6 +14,7 @@ urbi_uobjects_CPPFLAGS = $(AM_CPPFLAGS) -DBUILDING_URBI_SDK
 urbi_uobjects_CXXFLAGS = $(AM_CXXFLAGS) -fno-strict-aliasing
 if WIN32
 urbi_uobjects_LDFLAGS += -no-undefined
+urbi_uobjects_LIBADD = libuobject@LIBSFX@.la
 endif WIN32
 
 # urbi/logger.
@@ -22,6 +23,7 @@ dist_object_urbi_logger_la_SOURCES = object/urbi/logger.cc object/urbi/logger.hh
 object_urbi_logger_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
 object_urbi_logger_la_CXXFLAGS = $(urbi_uobjects_CXXFLAGS)
 object_urbi_logger_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
+object_urbi_logger_la_LIBADD = $(urbi_uobjects_LIBADD)
 
 # urbi/process.
 # Currently too hard to make portable.
@@ -33,6 +35,7 @@ dist_object_urbi_process_la_SOURCES =		\
 object_urbi_process_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
 object_urbi_process_la_CXXFLAGS = $(urbi_uobjects_CXXFLAGS)
 object_urbi_process_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
+object_urbi_process_la_LIBADD = $(urbi_uobjects_LIBADD)
 endif !WIN32
 
 # urbi/regexp.
@@ -41,7 +44,7 @@ dist_object_urbi_regexp_la_SOURCES = object/urbi/regexp.cc object/urbi/regexp.hh
 object_urbi_regexp_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
 object_urbi_regexp_la_CXXFLAGS = $(urbi_uobjects_CXXFLAGS)
 object_urbi_regexp_la_LDFLAGS = $(urbi_uobjects_LDFLAGS) $(BOOST_REGEX_LDFLAGS)
-object_urbi_regexp_la_LIBADD = $(BOOST_REGEX_LIBS)
+object_urbi_regexp_la_LIBADD = $(urbi_uobjects_LIBADD) $(BOOST_REGEX_LIBS)
 
 # urbi/stream.
 urbi_uobjects_LTLIBRARIES += object/urbi/stream.la
@@ -55,3 +58,4 @@ dist_object_urbi_stream_la_SOURCES =		\
 object_urbi_stream_la_CPPFLAGS = $(urbi_uobjects_CPPFLAGS)
 object_urbi_stream_la_CXXFLAGS = $(urbi_uobjects_CXXFLAGS)
 object_urbi_stream_la_LDFLAGS = $(urbi_uobjects_LDFLAGS)
+object_urbi_stream_la_LIBADD = $(urbi_uobjects_LIBADD)
