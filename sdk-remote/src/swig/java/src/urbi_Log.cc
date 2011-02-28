c541fd82cd50921fd39eae3689117d0903c190d2
Akim Demaille <demaille@gostai.com>
2011-02-28 16:33:01 +0100

    Formatting changes.

--- sdk-remote/src/swig/java/src/urbi_Log.cc
+++ sdk-remote/src/swig/java/src/urbi_Log.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Gostai S.A.S.
+ * Copyright (C) 2010, 2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -14,15 +14,23 @@
 GD_INIT();
 
 JNIEXPORT void JNICALL
-Java_urbi_Log_info (JNIEnv *env,jobject, jstring category, jstring msg, jstring functionname, jstring filename, jint linenumber)
+Java_urbi_Log_info (JNIEnv *env, jobject,
+                    jstring category,
+                    jstring msg,
+                    jstring functionname, jstring filename, jint linenumber)
 {
   const char* msg_ = env->GetStringUTFChars(msg, 0);
   const char* functionname_ = env->GetStringUTFChars(functionname, 0);
   const char* filename_ = env->GetStringUTFChars(filename, 0);
   const char*category_ = env->GetStringUTFChars(category, 0);
-  libport::debug::category_type _libport_gd_category = ::libport::debug::add_category(::libport::debug::category_type(category_));
-  if (GD_DEBUGGER->enabled(::libport::Debug::levels::debug, _libport_gd_category))
-    GD_DEBUGGER->debug(msg_, ::libport::Debug::types::info, _libport_gd_category, functionname_, filename_, (int) linenumber);
+  libport::debug::category_type _libport_gd_category =
+    ::libport::debug::add_category(::libport::debug::category_type(category_));
+  if (GD_DEBUGGER->enabled(::libport::Debug::levels::debug,
+                           _libport_gd_category))
+    GD_DEBUGGER->debug(msg_,
+                       ::libport::Debug::types::info,
+                       _libport_gd_category,
+                       functionname_, filename_, (int) linenumber);
   env->ReleaseStringUTFChars(msg, msg_);
   env->ReleaseStringUTFChars(functionname, functionname_);
   env->ReleaseStringUTFChars(filename, filename_);
@@ -30,15 +38,23 @@ Java_urbi_Log_info (JNIEnv *env,jobject, jstring category, jstring msg, jstring 
 }
 
 JNIEXPORT void JNICALL
-Java_urbi_Log_error (JNIEnv *env,jobject, jstring category, jstring msg, jstring functionname, jstring filename, jint linenumber)
+Java_urbi_Log_error (JNIEnv *env, jobject,
+                     jstring category,
+                     jstring msg,
+                     jstring functionname, jstring filename, jint linenumber)
 {
   const char* msg_ = env->GetStringUTFChars(msg, 0);
   const char* functionname_ = env->GetStringUTFChars(functionname, 0);
   const char* filename_ = env->GetStringUTFChars(filename, 0);
   const char*category_ = env->GetStringUTFChars(category, 0);
-  ::libport::debug::category_type _libport_gd_category = ::libport::debug::add_category(::libport::debug::category_type(category_));
-  if (GD_DEBUGGER->enabled(::libport::Debug::levels::log, _libport_gd_category))
-    GD_DEBUGGER->debug(msg_, ::libport::Debug::types::error, _libport_gd_category, functionname_, filename_, (int) linenumber);
+  ::libport::debug::category_type _libport_gd_category =
+      ::libport::debug::add_category(::libport::debug::category_type(category_));
+  if (GD_DEBUGGER->enabled(::libport::Debug::levels::log,
+                           _libport_gd_category))
+    GD_DEBUGGER->debug(msg_,
+                       ::libport::Debug::types::error,
+                       _libport_gd_category,
+                       functionname_, filename_, (int) linenumber);
   env->ReleaseStringUTFChars(msg, msg_);
   env->ReleaseStringUTFChars(functionname, functionname_);
   env->ReleaseStringUTFChars(filename, filename_);
