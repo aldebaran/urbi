3db82cc2e64dce1379174540e0a612a2c254abec
Akim Demaille <demaille@gostai.com>
2011-03-11 11:27:17 +0100

    SDK Remote: fix missing prototype.
    	* sdk-remote/include/urbi/umain.hh: here.
    	This causes failures of umake with picky compilers.

--- sdk-remote/include/urbi/umain.hh
+++ sdk-remote/include/urbi/umain.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009, 2010, Gostai S.A.S.
+ * Copyright (C) 2009, 2010, 2011, Gostai S.A.S.
  *
  * This software is provided "as is" without warranty of any kind,
  * either expressed or implied, including but not limited to the
@@ -33,6 +33,8 @@
     std::vector<std::string> args(argv, argv + argc);           \
     return urbi_root.urbi_main(args, true, true);               \
   }                                                             \
+                                                                \
+  int main_args(const libport::cli_args_type& args);            \
                                                                 \
   int                                                           \
   main_args(const libport::cli_args_type& args)                 \
