e202a34845e4fb6d02ec1f5ca07b7e255746bd61
Akim Demaille <demaille@gostai.com>
2011-06-28 14:16:24 +0200

    doc: factor the grammar bits.
    	* doc/urbi-sdk.tex (\grammar): New.
    	* doc/tables/grammar.tex: Move the contents into...
    	* doc/tables/urbiscript.bnf: here.
    	Extend, fix, and change the conventions.
    	* doc/Makefile.am: Adjust.
    	* doc/specs/lang.tex: Use \grammar.
    	* doc/tables/grammar.tex: Ditto.
    	* doc/tables/notation.tex: Point to the grammar chapter.
    	* doc/urbi-sandbox.tex: Remove experiments.
    	* share/emacs/bnf-mode.el: New.

---
+++ share/emacs/bnf-mode.el
@@ -0,0 +1,16 @@
+;; http://stackoverflow.com/questions/1800199/is-there-a-bnf-mode-for-emacs
+
+(define-generic-mode 'bnf-mode
+  () ;; comment char: inapplicable because # must be at start of line
+  nil ;; keywords
+  '(
+    ("^#.*" . 'font-lock-comment-face) ;; comments at start of line
+    ("^<.*?>" . 'font-lock-function-name-face) ;; LHS nonterminals
+    ("<.*?>" . 'font-lock-builtin-face) ;; other nonterminals
+    ("::=" . 'font-lock-const-face) ;; "goes-to" symbol
+    ("\|" . 'font-lock-warning-face) ;; "OR" symbol
+    ("\{:\\|:\}" . 'font-lock-keyword-face) ;; special pybnf delimiters
+   )
+  '("\\.bnf\\'" "\\.pybnf\\'") ;; filename suffixes
+  nil ;; extra function hooks
+  "Major mode for BNF highlighting.")
