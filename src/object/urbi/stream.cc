1497f02dee9df6daad294a9e1fea07c35db80740
Akim Demaille <demaille@gostai.com>
2011-11-29 10:09:22 +0100

    Urbi.Stream: beware of borken file descriptors.
    	* src/object/urbi/stream.cc (Stream::Stream): Valid the fd before
    	using it.
    	Check dup's success.

--- src/object/urbi/stream.cc
+++ src/object/urbi/stream.cc
@@ -48,9 +48,17 @@ namespace urbi
 
     Stream::Stream(int fd, bool own)
     {
+      // Check that we have a valid fd.
+      if (fcntl(fd, F_GETFL) == -1)
+        FRAISE("cannot create stream: %s", libport::strerror(errno));
+
       int fd2 = fd;
       if (!own)
+      {
         fd2 = dup(fd);
+        if (fd2 == -1)
+          FRAISE("cannot create stream: %s", libport::strerror(errno));
+      }
       newSocket();
       socket_->setNativeFD(fd2);
     }
