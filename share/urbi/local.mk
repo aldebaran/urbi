97b1348404c831bc940f05ae298bc34a831e1574
Akim Demaille <demaille@gostai.com>
2012-01-26 14:51:47 +0100

    maint: fix the installation of ros/gsrapi.u.
    	* share/urbi/local.mk: here.

--- share/urbi/local.mk
+++ share/urbi/local.mk
@@ -7,7 +7,11 @@
 ## See the LICENSE file for more information.
 
 urbidir = $(brandsharedir)/urbi
-dist_urbi_DATA := $(call ls_files,share/urbi/*.u @{share/urbi/ros}/*)
+# Beware of the case where we are a tarball: don't have two different
+# path to share/urbi/ros components.
+dist_urbi_DATA := 				\
+  $(filter-out share/urbi/ros/%, 		\
+    $(call ls_files,share/urbi/*.u))
 
 nodist_urbi_DATA =				\
   share/urbi/package-info.u			\
@@ -19,11 +23,13 @@ share/urbi/platform$(LIBSFX).u: $(top_builddir)/config.status $(top_srcdir)/shar
 	cd $(top_builddir) && $(SHELL) ./config.status $@
 
 
-## -------------- ##
-## PackageInfos.  ##
-## -------------- ##
+urbi.stamp: $(dist_urbi_DATA) $(nodist_urbi_DATA)
 
+## ----------------- ##
+## sub-directories.  ##
+## ----------------- ##
 
+# package-info/.
 urbi_package_infodir = $(urbidir)/package-info
 nodist_urbi_package_info_DATA =			\
   share/urbi/package-info/urbi.u		\
@@ -36,4 +42,6 @@ share/urbi/package-info/libport.u: $(top_srcdir)/sdk-remote/libport/.version $(V
 	$(AM_V_GEN)$(VERSIONIFY_RUN) --urbiscript=$@ --prefix='Libport'
 	$(AM_V_at)touch $@
 
-urbi.stamp: $(dist_urbi_DATA) $(nodist_urbi_DATA)
+# ros/.
+urbi_rosdir = $(urbidir)/ros
+dist_urbi_ros_DATA := $(call ls_files,@{share/urbi/ros}/*)
